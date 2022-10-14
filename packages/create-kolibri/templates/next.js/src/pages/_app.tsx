893c1d5aa569719334b8cd745d73f3119b60d7e2
Martin Oppitz <github@martinoppitz.com>
2022-10-14 06:36:47 +0200

    feat: add next.js template

---
+++ packages/create-kolibri/templates/next.js/src/pages/_app.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import App from 'next/app';
+import { applyPolyfills, defineCustomElements } from '@public-ui/components/dist/loader';
+import { BMF } from '@public-ui/themes';
+import { register } from '@public-ui/core';
+
+class RootApp extends App {
+	componentDidMount() {
+		void applyPolyfills()
+			.then(() => {
+				return register(BMF, []).then(() => {
+					void defineCustomElements(window);
+				});
+			})
+			.catch(console.warn);
+	}
+
+	render() {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		const { Component, pageProps } = this.props;
+		return (
+			<>
+				<Component {...pageProps} />
+			</>
+		);
+	}
+}
+
+export default RootApp;
---
+++ packages/create-kolibri/templates/next.js/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {KolButton} from '@public-ui/react';
+
+const Index = () => {
+	return (
+		<>
+			<kol-button _label="test"></kol-button>
+			<KolButton _label="test"></KolButton>
+		</>
+	);
+};
+
+export default Index;
