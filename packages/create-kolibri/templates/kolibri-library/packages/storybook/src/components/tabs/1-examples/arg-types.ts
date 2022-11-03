be7c0d124eed0b284f30fe48c0503b1d17055f0e
Martin Oppitz <github@martinoppitz.com>
2022-11-03 03:47:45 +0100

    feat: remove stuff from lib template

--- packages/create-kolibri/templates/kolibri-library/packages/storybook/src/components/tabs/1-examples/arg-types.ts
+++
@@ -1,9 +0,0 @@
-export const argTypes = {
-	_tabs: {
-		name: 'Body-Inhalt',
-		control: {
-			type: 'text',
-		},
-		defaultValue: "[{'_label':'Tab 1'},{'_label':'Tab 2'},{'_label':'Tab 3'}]",
-	},
-};
