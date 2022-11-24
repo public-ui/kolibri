7a0f4bc1704ff0db4fb0f26ee4855b8cc917dd03
Martin <6279703+deleonio@users.noreply.github.com>
2022-11-24 13:01:37 +0100

    Update alignment.ts
    Signed-off-by: Martin <6279703+deleonio@users.noreply.github.com>

--- packages/library/src/types/props/alignment.ts
+++ packages/library/src/types/props/alignment.ts
@@ -1,3 +1,3 @@
-export type HorizontalAlignment = 'left' | 'right';
-export type VerticalAlignment = 'top' | 'bottom';
+type HorizontalAlignment = 'left' | 'right';
+type VerticalAlignment = 'top' | 'bottom';
 export type Alignment = HorizontalAlignment | VerticalAlignment;
