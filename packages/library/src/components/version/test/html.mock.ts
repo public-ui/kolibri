abb1de94175a705f37f25088c42d699f6a0611fd
Martin <6279703+deleonio@users.noreply.github.com>
2022-12-07 08:48:34 +0100

    Update html.mock.ts
    Signed-off-by: Martin <6279703+deleonio@users.noreply.github.com>

--- packages/library/src/components/version/test/html.mock.ts
+++ packages/library/src/components/version/test/html.mock.ts
@@ -17,7 +17,7 @@ export const getVersionHtml = (props: Props, additionalAttrs = ''): string => {
 				? ''
 				: getBadgeHtml({
 						_color: '#BEC5C9',
-						_icon: 'infinite',
+						_icon: 'fa-solid fa-infinite',
 						_label: `v${props._version}`,
 				  })
 		}
