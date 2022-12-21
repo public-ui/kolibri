c5d125b3c0f6c9d0479eb7a52f44719eeed359e2
Martin Oppitz <github@martinoppitz.com>
2022-12-21 20:20:04 +0100

    fix: nightwatch

--- packages/create-kolibri/defaults/tests/e2e/axe.js
+++ packages/create-kolibri/defaults/tests/e2e/axe.js
@@ -1,14 +1,5 @@
-module.exports = {
-	'@tags': ['accessibility'],
-	'ensure site is accessible': function (browser) {
-		browser
-			.url('http://localhost:8080')
-			.assert.title('Verwaltung Bibliotheken')
-			.assert.containsText('h1', 'Bibliotheksverwaltung')
-			.axeInject()
-			.axeRun('body', {
-				rules: { 'color-contrast': { enabled: false } },
-			})
-			.end();
-	},
-};
+describe(`Demo's`, () => {
+	it('ecosia.org', (browser) => {
+		browser.url('http://localhost:8080/').waitForElementVisible('body').axeInject().axeRun().end();
+	});
+});
