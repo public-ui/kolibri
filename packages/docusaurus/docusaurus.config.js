b0f33a36e9b03316d98bea1868ce2ec90a86a15f
Martin Oppitz <github@martinoppitz.com>
2023-01-06 13:50:12 +0100

    chore: config seo

--- packages/docusaurus/docusaurus.config.js
+++ packages/docusaurus/docusaurus.config.js
@@ -54,6 +54,12 @@ const config = {
 					// Remove this to remove the "edit this page" links.
 					editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
 				},
+				sitemap: {
+					changefreq: 'hourly',
+					priority: 0.5,
+					ignorePatterns: [],
+					filename: 'sitemap.xml',
+				},
 				theme: {
 					customCss: require.resolve('./src/css/custom.css'),
 				},
