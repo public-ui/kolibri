835b6fdec472666846264279441319ae9b44de2d
Peter Laske <peter.laske@gmx.eu>
2023-01-17 09:32:54 +0100

    fix: replace console warn by devHint
    Refs #25

--- packages/library/src/i18n.ts
+++ packages/library/src/i18n.ts
@@ -2,6 +2,7 @@ import { Generic } from '@a11y-ui/core';
 import { getI18nService } from '@public-ui/core';
 import locale_de from './locales/de.json';
 import locale_en from './locales/en.json';
+import { devHint } from './utils/a11y.tipps';
 
 type ResourcePrefix = 'Kol';
 type ComponentKeys = keyof typeof locale_de;
@@ -22,13 +23,13 @@ type Options = {
 export const translate = (key: `${Lowercase<ResourcePrefix>}-${Lowercase<ComponentKeys>}`, options?: Options) => {
 	const i18n = getI18nService();
 	if (i18n === undefined) {
-		console.warn('I18nService not available! Please call the global register function.');
+		devHint('I18nService not available! Please call the global register function.');
 		return key;
 	}
 
 	const text = i18n.translate(key, options);
 	if (text === key) {
-		console.warn('Locales not initialized! Initialize default locales automatically.');
+		devHint('Locales not initialized! Initialize default locales automatically.');
 
 		translations.forEach((t) =>
 			t((l, t) => {
