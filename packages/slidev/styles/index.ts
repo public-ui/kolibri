a33f6b5123e7cd9069d03fe5444dd93dfc2b4ec1
Martin Oppitz <github@martinoppitz.com>
2023-01-30 06:14:25 +0100

    chore: optimize slidev

--- packages/slidev/styles/index.ts
+++ packages/slidev/styles/index.ts
@@ -1,19 +1,20 @@
-import { register } from "@public-ui/core";
-import { BMF, BZST, DESY, ITZBund, MAPZ } from "@public-ui/themes";
-import "./base.css";
+import { register } from '@public-ui/core';
+import { TH } from '@public-oss/kolibri-themes';
+import { BMF, BZST, DESY, ITZBund, MAPZ, ZOLL } from '@public-ui/themes';
+import './base.css';
 
 register(
-  [BMF, BZST, DESY,  ITZBund, MAPZ],
-  () => {
-    return new Promise((resolve) => resolve());
-  },
-  {
-    theme: {
-      detect: "auto",
-    },
-  }
+	[BMF, BZST, DESY, ITZBund, MAPZ, TH, ZOLL],
+	() => {
+		return new Promise((resolve) => resolve());
+	},
+	{
+		theme: {
+			detect: 'auto',
+		},
+	}
 )
-  .then(() => {
-    document.body.dataset.theme = "bmf";
-  })
-  .catch(() => console.warn);
+	.then(() => {
+		document.body.dataset.theme = 'bmf';
+	})
+	.catch(() => console.warn);
