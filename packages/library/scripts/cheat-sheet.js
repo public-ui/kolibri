98d0452b29033922453bd37775dc6b7902cdeaf0
Martin Oppitz <github@martinoppitz.com>
2023-02-01 09:43:44 +0100

    chore: release 1.3.2

--- packages/library/scripts/cheat-sheet.js
+++ packages/library/scripts/cheat-sheet.js
@@ -13,9 +13,9 @@ let SHEET_CHEAT = `<!DOCTYPE html>
 		<link href="https://fonts.cdnfonts.com/css/roboto" rel="stylesheet" />
 		<link href="https://use.fontawesome.com/releases/v6.2.1/css/all.css" rel="stylesheet" />
 		<script type="module">
-			import { register } from 'https://esm.sh/@public-ui/core@1.3.1';
-			import { defineCustomElements } from 'https://esm.sh/@public-ui/components@1.3.1/dist/loader';
-			import { MAPZ } from 'https://esm.sh/@public-ui/themes@1.3.1';
+			import { register } from 'https://esm.sh/@public-ui/core@1.3.2';
+			import { defineCustomElements } from 'https://esm.sh/@public-ui/components@1.3.2/dist/loader';
+			import { MAPZ } from 'https://esm.sh/@public-ui/themes@1.3.2';
 				register(MAPZ, defineCustomElements)
 						.then(() => {})
 						.catch(console.warn);
@@ -86,9 +86,9 @@ let SHEET_CHEAT = `<!DOCTYPE html>
 			<pre>
 				<code class="language-html">${`<head>
 	<script type="module">
-		import { register } from 'https://esm.sh/@public-ui/core@1.3.1';
-		import { defineCustomElements } from 'https://esm.sh/@public-ui/components@1.3.1/dist/loader';
-		import { MAPZ } from 'https://esm.sh/@public-ui/themes@1.3.1';
+		import { register } from 'https://esm.sh/@public-ui/core@1.3.2';
+		import { defineCustomElements } from 'https://esm.sh/@public-ui/components@1.3.2/dist/loader';
+		import { MAPZ } from 'https://esm.sh/@public-ui/themes@1.3.2';
 		register([MAPZ], [defineCustomElements])
 			.then(() => {})
 			.catch(console.warn);
