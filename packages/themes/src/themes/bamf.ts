3314f03c8d8d99c1072ef354a7dd0c96bc186468
Martin Oppitz <github@martinoppitz.com>
2023-02-24 14:32:06 +0100

    chore: update themes

---
+++ packages/themes/src/themes/bamf.ts
@@ -0,0 +1,7 @@
+import { KoliBri } from '@public-ui/schema';
+
+// Bundesamt für Migration und Flüchtlinge
+export const BAMF = KoliBri.createTheme('bamf', {
+	'KOL-BADGE':
+		':host {display: inline-block;}span {align-items: center;border-radius: 0.3125rem;display: flex;line-height: 1.25rem;gap: 0.5rem;padding: 0.25rem 0.75rem;}',
+});
