997645482add2ecb772aaf6f65d19391b4c27601
Martin Oppitz <github@martinoppitz.com>
2023-03-03 09:52:19 +0100

    chore: optimize styling

--- packages/themes/src/themes/index.ts
+++ packages/themes/src/themes/index.ts
@@ -6,6 +6,8 @@ export * from './bmf';
 export * from './default';
 export * from './desy-v1';
 export * from './desy-v2';
+export * from './ecl-ec';
+export * from './ecl-eu';
 export * from './itzbund';
 export * from './mapz';
 export * from './zoll-v2';
