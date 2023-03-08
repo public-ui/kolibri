4c04fb47b882aefe194b37c9edf4bf30d04b8a00
Martin Oppitz <github@martinoppitz.com>
2023-03-08 09:11:36 +0100

    chore: add jsdoc

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,5 +1,8 @@
 import { KoliBri as KoliBriUnique } from '@public-ui/components';
 
+/**
+ * @deprecated Moved to \@public-ui/components.
+ */
 export const KoliBri = (() => {
 	console.debug(
 		`All schema definitions have been moved to the component package and thus the @public-ui/schema package is deprecated. Now you can import the schema definitions directly from the @public-ui/components package.`
