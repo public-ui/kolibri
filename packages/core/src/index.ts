8cec92601490d6790ad108bf5fdcc136ef762d62
Martin Oppitz <github@martinoppitz.com>
2023-03-09 12:15:41 +0100

    chore: fix core and schema

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,11 +1,10 @@
-import { register as componentRegister } from '@public-ui/components';
+import { register } from '@public-ui/components';
+
+console.debug(
+	`The register functions has been moved to the @public-ui/components package and thus the @public-ui/core package is deprecated. Now you can import the register function directly from the @public-ui/components package.`
+);
 
 /**
  * @deprecated Moved to \@public-ui/components.
  */
-export const register = (() => {
-	console.debug(
-		`The register functions has been moved to the @public-ui/components package and thus the @public-ui/core package is deprecated. Now you can import the register function directly from the @public-ui/components package.`
-	);
-	return componentRegister;
-})();
+export { register };
--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,11 +1,10 @@
-import { KoliBri as KoliBriUnique } from '@public-ui/components';
+import { KoliBri } from '@public-ui/components';
+
+console.debug(
+	`All schema definitions have been moved to the component package and thus the @public-ui/schema package is deprecated. Now you can import the schema definitions directly from the @public-ui/components package.`
+);
 
 /**
  * @deprecated Moved to \@public-ui/components.
  */
-export const KoliBri = (() => {
-	console.debug(
-		`All schema definitions have been moved to the component package and thus the @public-ui/schema package is deprecated. Now you can import the schema definitions directly from the @public-ui/components package.`
-	);
-	return KoliBriUnique;
-})();
+export { KoliBri };
