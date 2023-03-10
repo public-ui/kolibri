17e2bed7022eb7e4b58b2caab34774dd819f8ba3
Peter Laske <peter.laske@gmx.eu>
2023-03-10 08:15:43 +0100

    chore: call register script

--- packages/components/src/global/script.ts
+++ packages/components/src/global/script.ts
@@ -1,15 +1,18 @@
 import { getThemeDetails, setThemeStyle } from '@a11y-ui/core';
 import { setMode } from '@stencil/core';
+import { register } from '../core';
 import { Log } from '../utils/dev.utils';
 
 // ts-prune-ignore-next
-export default (): void => {
+export default async (): Promise<void> => {
 	setMode((elm) => {
 		if (elm.shadowRoot instanceof ShadowRoot) {
 			setThemeStyle(elm, getThemeDetails(elm));
 		}
 		return 'default';
 	});
+
+	await register([], []);
 
 	import('./devtools')
 		.then((devTools) => {
