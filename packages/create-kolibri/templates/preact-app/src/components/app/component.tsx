15d2649222f966b47e8d6d00a5241df0c2ab7bcf
Martin Oppitz <github@martinoppitz.com>
2023-03-20 07:13:55 +0100

    chore: optimize templates

--- packages/create-kolibri/templates/preact-app/src/components/app/component.tsx
+++ packages/create-kolibri/templates/preact-app/src/components/app/component.tsx
@@ -18,22 +18,22 @@ export const AppComponent = (): JSX.Element => {
 			</header>
 			<main className="grid md:grid-cols-2 gap-6 p-12">
 				<KolAlert _type="success" _heading="Dokumentation" _variant="card" _level={2}>
-					<KolLink _href="https://public-ui.github.io/" _fill _target="_blank">
+					<KolLink _href="https://public-ui.github.io/" _label="" _target="_blank">
 						<KolIcon _ariaLabel="" _icon="fa-sharp fa-solid fa-book" /> Dokumentation öffnen
 					</KolLink>
 				</KolAlert>
 				<KolAlert _type="info" _heading="Theming" _variant="card" _level={2}>
-					<KolLink _href="https://public-ui.github.io/?path=/docs/designer--page" _fill _target="_blank">
+					<KolLink _href="https://public-ui.github.io/docs/concepts/styling/designer" _label="" _target="_blank">
 						<KolIcon _ariaLabel="" _icon="fa-solid fa-palette" /> KoliBri-Designer öffnen
 					</KolLink>
 				</KolAlert>
 				<KolAlert _type="warning" _heading="Mitwirken" _variant="card" _level={2}>
-					<KolLink _href="https://github.com/public-ui/kolibri/" _fill _target="_blank">
-						<KolIcon _ariaLabel="" _icon="fa-brands fa-github" /> Github öffnen
+					<KolLink _href="https://github.com/public-ui/kolibri/" _label="" _target="_blank">
+						<KolIcon _ariaLabel="" _icon="fa-brands fa-github" /> GitHub öffnen
 					</KolLink>
 				</KolAlert>
 				<KolAlert _type="error" _heading="Kontakt" _variant="card" _level={2}>
-					<KolLink _href="mailto:kolibri@itzbund.de" _fill _target="_blank">
+					<KolLink _href="mailto:kolibri@itzbund.de" _label="" _target="_blank">
 						<KolIcon _ariaLabel="" _icon="fa-solid fa-envelope" /> E-Mail schreiben
 					</KolLink>
 				</KolAlert>
--- packages/create-kolibri/templates/solid-app/src/components/app/component.tsx
+++ packages/create-kolibri/templates/solid-app/src/components/app/component.tsx
@@ -19,22 +19,22 @@ export const AppComponent: Component = () => {
 			</header>
 			<main class="grid md:grid-cols-2 gap-6 p-12">
 				<KolAlert _type="success" _heading="Dokumentation" _variant="card">
-					<KolLink _href="https://public-ui.github.io/" _fill _target="_blank">
+					<KolLink _href="https://public-ui.github.io/" _label="" _target="_blank">
 						<KolIcon _ariaLabel="" _icon="fa-sharp fa-solid fa-book" /> Dokumentation öffnen
 					</KolLink>
 				</KolAlert>
 				<KolAlert _type="info" _heading="Theming" _variant="card">
-					<KolLink _href="https://public-ui.github.io/?path=/docs/designer--page" _fill _target="_blank">
+					<KolLink _href="https://public-ui.github.io/docs/concepts/styling/designer" _label="" _target="_blank">
 						<KolIcon _ariaLabel="" _icon="fa-solid fa-palette" /> KoliBri-Designer öffnen
 					</KolLink>
 				</KolAlert>
 				<KolAlert _type="warning" _heading="Mitwirken" _variant="card">
-					<KolLink _href="https://github.com/public-ui/kolibri/" _fill _target="_blank">
-						<KolIcon _ariaLabel="" _icon="fa-brands fa-github" /> Github öffnen
+					<KolLink _href="https://github.com/public-ui/kolibri/" _label="" _target="_blank">
+						<KolIcon _ariaLabel="" _icon="fa-brands fa-github" /> GitHub öffnen
 					</KolLink>
 				</KolAlert>
 				<KolAlert _type="error" _heading="Kontakt" _variant="card">
-					<KolLink _href="mailto:kolibri@itzbund.de" _fill _target="_blank">
+					<KolLink _href="mailto:kolibri@itzbund.de" _label="" _target="_blank">
 						<KolIcon _ariaLabel="" _icon="fa-solid fa-envelope" /> E-Mail schreiben
 					</KolLink>
 				</KolAlert>
