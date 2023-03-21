3b1471d08276bb828fecd4d5aa4bd88f695a3195
Wolfram-Zell <wolfram@geezee.de>
2023-03-21 13:15:50 +0100

    append

--- packages/components/src/components/quote/test/html.mock.ts
+++ packages/components/src/components/quote/test/html.mock.ts
@@ -18,7 +18,7 @@ export const getQuoteHtml = (props: Props, slots: Slot = {}): string => {
 	const showExpertSlot = state._quote === '';
 	return `<kol-quote>
   <mock:shadow-root>
-		<figure class="${state._variant === 'block' ? 'block' : 'inline'}">
+		<figure class="${state._variant}">
 			<${state._variant === 'block' ? 'blockquote' : 'q'} cite="${state._href}">
 			${state._quote}
 				<span${showExpertSlot && typeof slots.expert === 'string' ? `` : ` aria-hidden="true" hidden=""`}>
