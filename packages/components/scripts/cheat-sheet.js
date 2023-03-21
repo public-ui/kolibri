44ad4d85b030a709950728a3c57bad7f22313174
Martin Oppitz <github@martinoppitz.com>
2023-03-21 10:09:34 +0100

    chore: release 1.5.0-rc.4

--- packages/components/scripts/cheat-sheet.js
+++ packages/components/scripts/cheat-sheet.js
@@ -13,9 +13,9 @@ let SHEET_CHEAT = `<!DOCTYPE html>
 		<link href="https://fonts.cdnfonts.com/css/roboto" rel="stylesheet" />
 		<link href="https://use.fontawesome.com/releases/v6.2.1/css/all.css" rel="stylesheet" />
 		<script type="module">
-			import { register } from 'https://esm.sh/@public-ui/components@1.5.0-rc.3';
-			import { defineCustomElements } from 'https://esm.sh/@public-ui/components@1.5.0-rc.3/dist/loader';
-			import { ITZBund } from 'https://esm.sh/@public-ui/themes@1.5.0-rc.3';
+			import { register } from 'https://esm.sh/@public-ui/components@1.5.0-rc.4';
+			import { defineCustomElements } from 'https://esm.sh/@public-ui/components@1.5.0-rc.4/dist/loader';
+			import { ITZBund } from 'https://esm.sh/@public-ui/themes@1.5.0-rc.4';
 				register(ITZBund, defineCustomElements)
 						.then(() => {})
 						.catch(console.warn);
@@ -86,9 +86,9 @@ let SHEET_CHEAT = `<!DOCTYPE html>
 			<pre>
 				<code class="language-html">${`<head>
 	<script type="module">
-		import { register } from 'https://esm.sh/@public-ui/components@1.5.0-rc.3';
-		import { defineCustomElements } from 'https://esm.sh/@public-ui/components@1.5.0-rc.3/dist/loader';
-		import { ITZBund } from 'https://esm.sh/@public-ui/themes@1.5.0-rc.3';
+		import { register } from 'https://esm.sh/@public-ui/components@1.5.0-rc.4';
+		import { defineCustomElements } from 'https://esm.sh/@public-ui/components@1.5.0-rc.4/dist/loader';
+		import { ITZBund } from 'https://esm.sh/@public-ui/themes@1.5.0-rc.4';
 		register([ITZBund], [defineCustomElements])
 			.then(() => {})
 			.catch(console.warn);
