04f73b7068c2a1a3c818f06718b45452b96b9c21
Wolfram-Zell <wolfram@geezee.de>
2023-03-22 16:19:20 +0100

    phase one

--- packages/components/src/assets/nav-simulation.js
+++ packages/components/src/assets/nav-simulation.js
@@ -1,4 +1,5 @@
-document.querySelector('#click-nav')._links = [
+/* eslint-disable no-undef */
+document.querySelector('#nav-test')._links = [
 	{
 		_label: '1 Navigationspunkt mit sehr langem Link-Test',
 		_on: {
@@ -6,7 +7,7 @@ document.querySelector('#click-nav')._links = [
 				alert('hallo');
 			},
 		},
-		_icon: 'icofont-woodpecker',
+		_icon: 'codicon codicon-flame',
 	},
 	{
 		_label: '2 Navigationspunkt und ich_bin_ein_echt_langes_zusammengesetztes_Worte_und_versuche_das_Layout_zu_brechen',
@@ -15,35 +16,35 @@ document.querySelector('#click-nav')._links = [
 				alert('hallo');
 			},
 		},
-		_icon: 'icofont-woodpecker',
+		_icon: 'codicon codicon-flame',
 	},
 	{
-		_active: true,
-		_label: '3 Navigationspunkt',
+		_label: '3 Navigationspunkt mit viel Text zu was testen',
 		_href: '#abc',
-		_icon: 'icofont-woodpecker',
+		_icon: 'codicon codicon-flame',
 		_children: [
-			{ _label: '3.1 Navigationspunkt', _href: '#abc', _icon: 'icofont-woodpecker' },
-			{ _label: '3.2 Navigationspunkt', _href: '#abc', _icon: 'icofont-woodpecker', _target: 'asdasd' },
+			{ _label: '3.1 Navigationspunkt', _href: '#abc', _icon: 'codicon codicon-flame' },
+			{ _label: '3.2 Navigationspunkt', _href: '#abc', _icon: 'codicon codicon-flame', _target: 'asdasd' },
 			{
-				_active: true,
+				_icon: 'codicon codicon-flame',
 				_label: '3.3 Navigationspunkt',
 				_href: '#abc',
 				_children: [
-					{ _active: true, _label: '3.3.1 Navigationspunkt (aktiv)', _href: '#abc' },
-					{ _label: '3.3.2 Navigationspunkt', _href: '#abc' },
+					{ _icon: 'codicon codicon-flame', _label: '3.3.1 Navigationspunkt', _href: '#abc' },
+					{ _icon: 'codicon codicon-flame', _label: '3.3.2 Navigationspunkt', _href: '#abc' },
 				],
 			},
 			{
+				_icon: 'codicon codicon-flame',
 				_label: '3.4 Navigationspunkt',
 				_href: '#abc',
 				_children: [
-					{ _label: '3.4.1 Navigationspunkt', _href: '#abc' },
-					{ _label: '3.4.2 Navigationspunkt', _href: '#abc' },
+					{ _icon: 'codicon codicon-flame', _label: '3.4.1 Navigationspunkt', _href: '#abc' },
+					{ _icon: 'codicon codicon-flame', _label: '3.4.2 Navigationspunkt', _href: '#abc' },
 				],
 			},
-			{ _label: '3.5 Navigationspunkt', _href: '#abc' },
+			{ _icon: 'codicon codicon-flame', _label: '3.5 Navigationspunkt', _href: '#abc' },
 		],
 	},
-	{ _label: '4 Navigationspunkt', _href: '#abc' },
+	{ _icon: 'codicon codicon-flame', _label: '4 Navigationspunkt', _href: '#abc' },
 ];
