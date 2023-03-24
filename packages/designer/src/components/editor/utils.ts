5010d79afde9bf2e3b82158e3ebf804bbc2a4f96
Martin Oppitz <github@martinoppitz.com>
2023-03-24 07:32:42 +0100

    chore: ignore eslint

--- packages/designer/src/components/editor/utils.ts
+++ packages/designer/src/components/editor/utils.ts
@@ -1,19 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-floating-promises, @typescript-eslint/no-unsafe-call, @typescript-eslint/restrict-plus-operands */
+
 import { format } from 'prettier';
 import parserCss from 'prettier/esm/parser-postcss.mjs';
 
 export function formatReadableCssJson(code: string) {
 	let css = `{`;
-	const json = JSON.parse(code);
-	for (const key in json) {
-		try {
-			json[key] = format(json[key], { parser: 'css', plugins: [parserCss] });
-		} catch (e) {}
-		css += `
+	try {
+		const json = JSON.parse(code);
+		for (const key in json) {
+			try {
+				json[key] = format(json[key], { parser: 'css', plugins: [parserCss] });
+			} catch (e) {}
+			css += `
 `;
-		css += '"' + key + '": `' + json[key].replace(/(\n\r?)+$/, '') + '`,';
-	}
+			css += '"' + key + '": `' + json[key].replace(/(\n\r?)+$/, '') + '`,';
+		}
+	} catch (e) {}
 	css += `}`;
-	console.log(css);
 	return css;
 }
 
