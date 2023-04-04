e2a0e0d9ffaddc6f7d8a78477d70ba2c70999f2c
Wolfram-Zell <wolfram@geezee.de>
2023-04-04 07:40:45 +0200

    card

--- packages/components/src/components.d.ts
+++ packages/components/src/components.d.ts
@@ -402,7 +402,7 @@ export namespace Components {
     }
     interface KolCard {
         /**
-          * Gibt an, ob die Card einen Footer-Bereich hat.
+          * Macht den Footerbereich der Card sichtbar.
          */
         "_hasFooter"?: boolean;
         /**
@@ -415,7 +415,7 @@ export namespace Components {
          */
         "_headline"?: string;
         /**
-          * Gibt an, welchen H-Level von 1 bis 6 die Überschrift hat.
+          * Setzt den H-Level, von 1 bis 6, der Überschrift.
          */
         "_level"?: HeadingLevel;
     }
@@ -2967,7 +2967,7 @@ declare namespace LocalJSX {
     }
     interface KolCard {
         /**
-          * Gibt an, ob die Card einen Footer-Bereich hat.
+          * Macht den Footerbereich der Card sichtbar.
          */
         "_hasFooter"?: boolean;
         /**
@@ -2980,7 +2980,7 @@ declare namespace LocalJSX {
          */
         "_headline"?: string;
         /**
-          * Gibt an, welchen H-Level von 1 bis 6 die Überschrift hat.
+          * Setzt den H-Level, von 1 bis 6, der Überschrift.
          */
         "_level"?: HeadingLevel;
     }
