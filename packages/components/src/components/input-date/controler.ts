d7c0c0269a935fcfa80781443b5864c47fa9aaae
cyrdam <51902761+cyrdam@users.noreply.github.com>
2023-05-02 17:44:42 +0200

    chore: move validation from component to controler

--- packages/components/src/components/input-date/controler.ts
+++ packages/components/src/components/input-date/controler.ts
@@ -8,12 +8,18 @@ import { InputIconController } from '../@deprecated/input/controller-icon';
 import { Props, Watches } from './types';
 
 export class InputDateController extends InputIconController implements Watches {
+	private readonly DEFAULT_MAX_DATE = new Date(9999, 11, 31, 23, 59, 59);
+
 	/**
 	 * Regex to check whether a string is a date in ISO-8601 format.
 	 * Test the regex here: https://regex101.com/r/ys3ij0/1
 	 */
 	private readonly isoDateRegex =
 		/(^\d{4}-([0]\d|1[0-2])-([0-2]\d|3[01])([T ][0-2]\d:[0-5]\d:[0-5]\d(?:\.\d+)?([+-][0-2]\d:[0-5]\d|Z)?)?$)|(^[0-2]\d:[0-5]\d(:[0-5]\d)?$)/;
+	private readonly isoLocalDateTimeRegex = /^\d{4}-([0]\d|1[0-2])-([0-2]\d|3[01])[T ][0-2]\d:[0-5]\d(:[0-5]\d(?:\.\d+)?)?/;
+	private readonly isoMonthRegex = /^\d{4}-([0]\d|1[0-2])/;
+	private readonly isoTimeRegex = /^[0-2]\d:[0-5]\d(:[0-5]\d(?:\.\d+)?)?/;
+	private readonly isoWeekRegex = /^\d{4}-W(?:[0-4]\d|5[0-3])$/;
 
 	protected readonly component: Generic.Element.Component & Props;
 
@@ -41,16 +47,53 @@ export class InputDateController extends InputIconController implements Watches 
 			return value;
 		}
 		if (typeof value === 'object' && value instanceof Date) {
-			return value.toISOString();
+			return this.valueAsIsoDate(value);
 		}
 		return '';
 	};
+
+	private valueAsIsoDate(value?: Iso8601 | Date | null, defaultValue?: Date): Iso8601 | null | undefined {
+		const v: Iso8601 | Date | undefined = value ?? defaultValue;
+		if (typeof v === 'string') {
+			return v;
+		}
+		if (typeof v === 'object' && v instanceof Date) {
+			const type = this.component._type;
+			switch (type) {
+				case 'date':
+					return `${v.getFullYear()}-${v.getMonth() + 1}-${v.getDate()}`;
+				case 'datetime-local':
+					return `${v.getFullYear()}-${v.getMonth() + 1}-${v.getDate()}T${v.getHours()}:${v.getMinutes()}:${v.getSeconds()}`;
+				case 'month':
+					return `${v.getFullYear()}-${v.getMonth() + 1}`;
+				case 'time':
+					// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/time#using_the_step_attribute
+					if (
+						this.component._step === undefined ||
+						(typeof this.component._step === 'string' && this.component._step === '60') ||
+						(typeof this.component._step === 'number' && this.component._step === 60)
+					) {
+						return `${v.getHours()}:${v.getMinutes()}`;
+					} else {
+						return `${v.getHours()}:${v.getMinutes()}:${v.getSeconds()}`;
+					}
+				case 'week':
+					throw new Error('Auto convert to week is not supported!');
+				default:
+					throw new Error(`No supported type set: ${typeof this.component._type}`);
+			}
+		}
+		if (value === null) {
+			return null;
+		}
+		return undefined;
+	}
 
 	private readonly validateIso8601 = (propName: string, value?: Date | Iso8601 | null, afterPatch?: (v: string) => void) => {
 		return watchValidator(
 			this.component,
 			propName,
-			(value): boolean => value === undefined || value === '' || this.isoDateRegex.test(value),
+			(value): boolean => value === undefined || value === '' || (typeof value === 'string' && this.isoDateRegex.test(value)),
 			new Set(['Date', 'string{ISO-8601}']),
 			this.parseToString(value),
 			{
@@ -74,12 +117,41 @@ export class InputDateController extends InputIconController implements Watches 
 		}
 	}
 
+	private validateDateString(value: Iso8601): boolean {
+		switch (this.component._type) {
+			case 'date':
+				return this.isoDateRegex.test(value);
+			case 'datetime-local':
+				return this.isoLocalDateTimeRegex.test(value);
+			case 'month':
+				return this.isoMonthRegex.test(value);
+			case 'time':
+				return this.isoTimeRegex.test(value);
+			case 'week':
+				return this.isoWeekRegex.test(value);
+			default:
+				throw new Error('_type is not correct');
+		}
+	}
+
 	public validateMax(value?: Date | Iso8601): void {
-		this.validateIso8601('_max', value);
+		watchValidator(
+			this.component,
+			'_max',
+			(value): boolean => value === undefined || (value !== null && this.validateDateString(value)),
+			new Set(['Iso8601', 'Date']),
+			this.valueAsIsoDate(value, this.component._type === 'date' || this.component._type === 'month' || this.component._type === 'datetime-local' ? this.DEFAULT_MAX_DATE : undefined)
+		);
 	}
 
 	public validateMin(value?: Date | Iso8601): void {
-		this.validateIso8601('_min', value);
+		watchValidator(
+			this.component,
+			'_min',
+			(value): boolean => value === undefined || (value !== null && this.validateDateString(value)),
+			new Set(['Iso8601', 'Date']),
+			this.valueAsIsoDate(value)
+		);
 	}
 
 	public validatePlaceholder(value?: string): void {
@@ -114,7 +186,13 @@ export class InputDateController extends InputIconController implements Watches 
 	}
 
 	public validateValue(value?: Date | Iso8601 | null): void {
-		this.validateValueEx(value);
+		watchValidator(
+			this.component,
+			'_value',
+			(value): boolean => value === undefined || value === null || this.validateDateString(value),
+			new Set(['Iso8601', 'Date']),
+			this.valueAsIsoDate(value)
+		);
 	}
 
 	/**
