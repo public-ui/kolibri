74925066abccfdd11707467cfe4326442f69e858
Martin <6279703+deleonio@users.noreply.github.com>
2023-05-02 11:21:16 +0200

    chore: fix typo handnout

--- packages/samples/react/src/samples/handout/routes.ts
+++ packages/samples/react/src/samples/handout/routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '../../shares/types';
 import { HandoutBasic } from './basic';
 
 export const HANDOUT_ROUTES: Routes = {
-	handnout: {
+	handout: {
 		basic: HandoutBasic,
 	},
 };
