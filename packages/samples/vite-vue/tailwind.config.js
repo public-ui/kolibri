da88544fae1583e3c12aa051ba5bfe9e58b0bd65
Wolfram-Zell <wolfram@geezee.de>
2023-05-19 10:56:27 +0200

    Update tailwind.config.js

--- packages/samples/vite-vue/tailwind.config.js
+++ packages/samples/vite-vue/tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
-		extend: {}
+		container: {
+			center: true,
+		},
+		extend: {},
 	},
-	plugins: []
-}
+	plugins: [],
+};
