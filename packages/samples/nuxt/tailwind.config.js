879504f3e2e5e4d19128abad651235614cb17ab5
Wolfram-Zell <wolfram@geezee.de>
2023-05-19 15:45:09 +0200

    init tailwind

---
+++ packages/samples/nuxt/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [],
+	theme: {
+		extend: {
+			container: {
+				center: true,
+				padding: '2rem',
+			},
+		},
+	},
+	plugins: [],
+};
