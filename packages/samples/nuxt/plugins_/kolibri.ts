8465dfbb672dce8f4b7fdb9328b96b1d827e7f45
Wolfram-Zell <wolfram@geezee.de>
2023-05-19 15:45:44 +0200

    added Pinia

---
+++ packages/samples/nuxt/plugins_/kolibri.ts
@@ -0,0 +1,18 @@
+// import { defineNuxtPlugin } from '#app';
+
+// import type { Plugin } from 'vue';
+// import { defineCustomElements } from '@public-ui/components/dist/loader';
+// import { register } from '@public-ui/components';
+// import { ITZBund } from '@public-ui/themes';
+
+// const ComponentLibrary: Plugin = {
+// 	install() {
+// 		register(ITZBund, defineCustomElements)
+// 			.then(() => console.log('Components registered'))
+// 			.catch(console.warn);
+// 	},
+// };
+
+// export default defineNuxtPlugin((nuxtApp) => {
+// 	nuxtApp.vueApp.use(ComponentLibrary);
+// });
---
+++ packages/samples/nuxt/stores/components.ts
@@ -0,0 +1,43 @@
+import { defineStore } from 'pinia';
+import customElementJSON from '@public-ui/components/custom-elements.json';
+
+function componentStoreInit() {
+	console.log('componentStoreInit');
+	const result = {} as Record<string, (typeof customElementJSON.tags)[0]>;
+	customElementJSON.tags.forEach((tag) => {
+		if (!tag.name.includes('-wc')) result[tag.name] = tag;
+	});
+	return result;
+}
+
+export const useComponentStore = defineStore('components', {
+	state() {
+		return {
+			currentTag: '',
+			components: componentStoreInit(),
+		};
+	},
+	getters: {
+		current(state) {
+			return state.components[state.currentTag];
+		},
+		options(state) {
+			return Object.values(state.components).map((c) => ({ label: c.name, value: c.name }));
+		},
+	},
+	actions: {
+		hopComponent(forward: boolean) {
+			const keys = Object.keys(this.components);
+			const currentIndex = keys.indexOf(this.$state.currentTag);
+			let futureIndex = (currentIndex + (forward ? 1 : -1)) % keys.length;
+			if (futureIndex < 0) futureIndex = keys.length + futureIndex;
+			this.$state.currentTag = keys[futureIndex];
+		},
+		nextComponent() {
+			this.hopComponent(true);
+		},
+		prevComponent() {
+			this.hopComponent(false);
+		},
+	},
+});
