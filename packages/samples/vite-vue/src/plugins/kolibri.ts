5bc535fb46d4f83af731f614ffe02e3a9298cb35
Wolfram-Zell <wolfram@geezee.de>
2023-05-19 07:48:57 +0200

    deleted default, updated configs

--- packages/samples/vite-vue/src/plugins/kolibri.ts
+++ packages/samples/vite-vue/src/plugins/kolibri.ts
@@ -1,10 +1,11 @@
 import type { Plugin } from 'vue'
 import { defineCustomElements } from '@public-ui/components/dist/loader'
 import { register } from '@public-ui/components'
-import { ITZBund } from '@public-ui/themes'
+import { MAPZ } from '@public-ui/themes'
+
 export const ComponentLibrary: Plugin = {
 	install() {
-		register(ITZBund, defineCustomElements)
+		register(MAPZ, defineCustomElements)
 			.then(() => console.log('Components registered'))
 			.catch(console.warn)
 	}
