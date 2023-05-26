e36606e55f1445a67c6986e994ad05fd6e3029d5
Martin Oppitz <github@martinoppitz.com>
2023-05-26 17:49:57 +0200

    fix: progress and tests

--- packages/components/src/components/progress/component.tsx
+++ packages/components/src/components/progress/component.tsx
@@ -26,7 +26,7 @@ const createProgressSVG = (state: States): JSX.Element => {
 	switch (state._type) {
 		case 'cycle':
 			return (
-				<svg class={state._type} width="100" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+				<svg class="cycle" width="100" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
 					<circle fill="none" stroke="#efefef" cx="6px" cy="6px" r="5px"></circle>
 					<text aria-hidden="true" font-size="0.1em" x="50%" y="50%" text-anchor="middle" fill="currentColor">
 						{state._label && (
@@ -40,7 +40,7 @@ const createProgressSVG = (state: States): JSX.Element => {
 						</tspan>
 					</text>
 					<circle
-						class="cycle"
+						class="progress"
 						stroke-linecap="round"
 						stroke-dasharray={`${Math.round((state._value / state._max) * 32)}px 32px`}
 						fill="none"
@@ -53,13 +53,13 @@ const createProgressSVG = (state: States): JSX.Element => {
 			);
 		default:
 			return (
-				<div class={state._type}>
+				<div class="bar">
 					{state._label && <div>{state._label}</div>}
 					<div style={{ display: 'flex', gap: '0.3em' }}>
 						<svg width="100" viewBox="0 0 24 2" xmlns="http://www.w3.org/2000/svg">
 							<line stroke-width="2" x1="1" stroke-linecap="round" y1="1" x2="23" y2="1" fill="#efefef" stroke="#efefef"></line>
 							<line
-								class="bar"
+								class="progress"
 								stroke-width="2"
 								x1="1"
 								stroke-linecap="round"
