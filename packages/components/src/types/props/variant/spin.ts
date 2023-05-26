fd5de8df0f8050cc975f10659f363250e48511a7
Martin Oppitz <github@martinoppitz.com>
2023-05-26 15:40:46 +0200

    fix: export spin variant

---
+++ packages/components/src/types/props/variant/spin.ts
@@ -0,0 +1,25 @@
+import { Generic } from '@a11y-ui/core';
+import { watchBoolean } from '../../utils/prop.validators';
+import { watchValidator } from '../../../utils/prop.validators';
+
+/* types */
+/**
+ * Loadingspinner
+ * - https://github.com/vineethtrv/css-loader
+ */
+export type SpinVariant = 'default' | 'none';
+
+/** de
+ * Macht die Fehlermeldung dieses Elements von Screenreadern lesbar.
+ */
+/** en
+ * Makes hints readable for screenreaders.
+ */
+export type PropSpinVariant = {
+	variant: SpinVariant;
+};
+
+/* validator */
+export const validateSpinVariant = (component: Generic.Element.Component, value?: SpinVariant): void => {
+	watchValidator(component, '_variant', (value) => value === 'default' || value === 'none', new Set(['default', 'none']), value);
+};
