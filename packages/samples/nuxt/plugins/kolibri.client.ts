5f13e15afdc2f7ee9d9983fc698a5e1f4c7895e4
Wolfram-Zell <wolfram@geezee.de>
2023-06-02 11:48:59 +0200

    nuxt

--- packages/samples/nuxt/plugins/kolibri.client.ts
+++ packages/samples/nuxt/plugins/kolibri.client.ts
@@ -4,11 +4,15 @@ import { defineCustomElements } from '@public-ui/components/dist/loader';
 import { register } from '@public-ui/components';
 import { ITZBund } from '@public-ui/themes';
 
-// export default defineNuxtPlugin((nuxtApp) => {
-// 	register(ITZBund, defineCustomElements)
-// 		.then(() => console.log('Components registered'))
-// 		.catch(console.warn);
-// });
+import '@public-ui/components/dist/kolibri/kolibri.esm.js';
+
+console.log('test?');
+
+export default defineNuxtPlugin((nuxtApp) => {
+	register(ITZBund, defineCustomElements)
+		.then(() => console.log('Components registered'))
+		.catch(console.warn);
+});
 
 // export default defineNuxtPlugin({
 // 	setup() {
@@ -18,10 +22,10 @@ import { ITZBund } from '@public-ui/themes';
 // 	},
 // });
 
-export default {
-	install() {
-		register(ITZBund, defineCustomElements)
-			.then(() => console.log('Components registered'))
-			.catch(console.warn);
-	},
-};
+// export default {
+// 	install() {
+// 		register(ITZBund, defineCustomElements)
+// 			.then(() => console.log('Components registered'))
+// 			.catch(console.warn);
+// 	},
+// };
