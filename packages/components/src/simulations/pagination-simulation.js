4385141173d5a9dabd22f19f4eebf5128ea77062
Wolfram-Zell <wolfram@geezee.de>
2023-06-15 09:11:18 +0200

    Update pagination-simulation.js

--- packages/components/src/simulations/pagination-simulation.js
+++ packages/components/src/simulations/pagination-simulation.js
@@ -1,8 +1,8 @@
 var buttons = document.querySelectorAll('kol-pagination');
 buttons.forEach((button) => {
-  button._on = {
-    onClick: (event, page) => {
-      alert(`Pagination "${page}" wurde geklickt.`);
-    },
-  };
+	button._on = {
+		onClick: (event, page) => {
+			alert(`Pagination "${page}" wurde geklickt.`);
+		},
+	};
 });
