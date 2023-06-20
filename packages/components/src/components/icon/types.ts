995dac41b200f7103da51bc89ffa05dd81c912e4
Wolfram-Zell <wolfram@geezee.de>
2023-06-20 10:55:03 +0200

    icon part I

--- packages/components/src/components/icon/types.ts
+++ packages/components/src/components/icon/types.ts
@@ -1,16 +1,19 @@
 import { Generic } from '@a11y-ui/core';
-import { AriaLabel } from '../../types/aria-label';
 
 type RequiredProps = {
 	icon: string;
 };
 type OptionalProps = {
+	/**
+	 * @deprecated
+	 */
+	ariaLabel: string;
 	label: string;
 	part: string;
-} & AriaLabel;
+};
 export type KoliBriIconProps = Generic.Element.Members<RequiredProps, OptionalProps>;
 
-type RequiredStates = RequiredProps;
+type RequiredStates = { label: string } & RequiredProps;
 type OptionalStates = OptionalProps;
 
 export type KoliBriIconStates = Generic.Element.Members<RequiredStates, OptionalStates>;
