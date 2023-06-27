707fbbaf7478f7ab01faacca8a3b9cafc48cd58f
Wolfram-Zell <wolfram@geezee.de>
2023-06-27 07:13:52 +0200

    format icon mock

--- packages/components/src/components/icon/test/html.mock.ts
+++ packages/components/src/components/icon/test/html.mock.ts
@@ -10,7 +10,9 @@ export const getIconHtml = (props: KoliBriIconProps, additionalAttrs = ''): stri
 	);
 	return `<kol-icon exportparts="icon"${additionalAttrs}>
   <mock:shadow-root>
-    <i ${typeof props._label === 'string' && props._label.length > 0 ? `aria-label="${props._label}"` : 'aria-hidden="true"'} class="${props._icon}" part="icon" role="img"></i>
-  </mock:shadow-root>
+    <i ${typeof props._label === 'string' && props._label.length > 0 ? `aria-label="${props._label}"` : 'aria-hidden="true"'} class="${
+		props._icon
+	}" part="icon" role="img"></i>
+  </mock:shadow-root> 
 </kol-icon>`;
 };
