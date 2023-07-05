b0c19544d92b9915c303a554d191af97d7055203
Martin Oppitz <github@martinoppitz.com>
2023-07-05 12:36:16 +0200

    chore: update auto gen files

--- packages/adapters/vaadin/KolInputText.java
+++ packages/adapters/vaadin/KolInputText.java
@@ -10,7 +10,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
  */
 
 @Tag("kol-input-text")
-@NpmPackage(value = "@public-ui/components", version = "1.6.0-rc.6")
+@NpmPackage(value = "@public-ui/components", version = "1.6.0-rc.7")
 @JsModule("@public-ui/components/dist/components/kol-input-text")
 public class KolInputText extends Component {
 	/**
