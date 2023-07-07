ac23fc56b544fbb9ed2b4b5da20b6184608b6186
Wolfram-Zell <wolfram@geezee.de>
2023-07-07 09:58:33 +0200

    removed test that were crashing build

--- packages/create-kolibri/templates/react-vite-app/vite.config.ts
+++ packages/create-kolibri/templates/react-vite-app/vite.config.ts
@@ -7,9 +7,4 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: './src/test/setup.ts',
-	},
 });
