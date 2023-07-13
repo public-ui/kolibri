50edb5b1848a76a01974976d5ba6e18c51fb58bc
Martin Oppitz <github@martinoppitz.com>
2023-07-13 06:13:36 +0200

    chore: initial sample

--- packages/components/src/components/icon/test/html.mock.ts
+++ packages/components/src/components/icon/test/html.mock.ts
@@ -1,19 +1,24 @@
 import { mixMembers } from 'stencil-awesome-test';
 
-import { KoliBriIconProps } from '../types';
+import { KoliBriIconProps, KoliBriIconStates } from '../types';
+
+import path from 'path';
+import pug from 'pug';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+const compiledFunction = pug.compileFile(path.join(__dirname, 'icon.pug'));
 
 export const getIconHtml = (props: KoliBriIconProps, additionalAttrs = ''): string => {
-	props = mixMembers(
+	const state = mixMembers<KoliBriIconProps, KoliBriIconStates>(
 		{
 			_icon: 'codicon codicon-home',
 		},
 		props
 	);
-	return `<kol-icon exportparts="icon"${additionalAttrs}>
-  <mock:shadow-root>
-    <i ${typeof props._label === 'string' && props._label.length > 0 ? `aria-label="${props._label}"` : 'aria-hidden="true"'} class="${
-		props._icon
-	}" part="icon" role="img"></i>
-  </mock:shadow-root> 
-</kol-icon>`;
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+	return compiledFunction({
+		additionalAttrs,
+		mode: 'csr',
+		...state,
+	});
 };
