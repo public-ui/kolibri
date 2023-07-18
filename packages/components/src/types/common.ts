e788d2531a84458957437458a4d9641db1910c69
Martin <6279703+deleonio@users.noreply.github.com>
2023-07-18 11:59:17 +0200

    Update common.ts
    Signed-off-by: Martin <6279703+deleonio@users.noreply.github.com>

--- packages/components/src/types/common.ts
+++ packages/components/src/types/common.ts
@@ -1,7 +1,4 @@
 /**
  * Used in web component properties for stringified complex types.
- *
- * ADVICE: Stencil does not watch unknown typed properties. The solution is to
- *         use a stringified complex type.
  */
 export type Stringified<T> = string | T;
