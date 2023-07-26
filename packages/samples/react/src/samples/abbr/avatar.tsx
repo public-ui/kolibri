2b1156c1be9b3a105aed502d33785fd3fa24813c
Martin Oppitz <github@martinoppitz.com>
2023-07-26 12:06:12 +0200

    chore: add avatar

---
+++ packages/samples/react/src/samples/abbr/avatar.tsx
@@ -0,0 +1,8 @@
+import { KolAvatar } from '@public-ui/react';
+import React, { FC } from 'react';
+
+export const AbbrAvatar: FC = () => (
+	<>
+		<KolAvatar _src="https://www.w3schools.com/howto/img_avatar.png" _label="Elke Mustermann" />
+	</>
+);
--- packages/samples/react/src/samples/abbr/routes.ts
+++ packages/samples/react/src/samples/abbr/routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '../../shares/types';
+import { AbbrAvatar } from './avatar';
 
 import { AbbrBasic } from './basic';
 
 export const ABBR_ROUTES: Routes = {
 	abbr: {
 		basic: AbbrBasic,
+		avatar: AbbrAvatar,
 	},
 };
