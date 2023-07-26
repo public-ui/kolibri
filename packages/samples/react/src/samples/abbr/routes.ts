7fe86c9a041345cf63693a13b514a5e4d2445bcf
Stefan Dietz <mail@stefan-dietz.eu>
2023-07-26 15:45:52 +0200

    Polish up avatar examples

--- packages/samples/react/src/samples/abbr/routes.ts
+++ packages/samples/react/src/samples/abbr/routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '../../shares/types';
-import { AbbrAvatar } from './avatar';
 
 import { AbbrBasic } from './basic';
 
 export const ABBR_ROUTES: Routes = {
 	abbr: {
 		basic: AbbrBasic,
-		avatar: AbbrAvatar,
 	},
 };
