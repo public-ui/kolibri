2df7381e007200cd63775285afea6254a32bfd47
Stefan Dietz <mail@stefan-dietz.eu>
2023-07-27 10:26:58 +0200

    Make tooltip ID optional

--- packages/components/src/components/tooltip/types.ts
+++ packages/components/src/components/tooltip/types.ts
@@ -2,13 +2,12 @@ import { Generic } from '@a11y-ui/core';
 
 import { Align } from '../../types/props/align';
 import { PropLabel } from '../../types/props/label';
+import { PropId } from '../../types/props/id';
 
-type RequiredProps = PropLabel & {
-	id: string;
-};
+type RequiredProps = PropLabel;
 type OptionalProps = {
 	align: Align;
-};
+} & PropId;
 export type KoliBriTooltipProps = Generic.Element.Members<RequiredProps, OptionalProps>;
 
 type RequiredStates = RequiredProps & OptionalProps;
