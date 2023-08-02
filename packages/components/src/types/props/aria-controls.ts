2505e4cb92aeb5b10d230171ed6fb272176bac7b
Stefan Dietz <mail@stefan-dietz.eu>
2023-08-02 17:35:56 +0200

    Refactor ariaControls prop

--- packages/components/src/types/props/aria-controls.ts
+++ packages/components/src/types/props/aria-controls.ts
@@ -2,14 +2,21 @@ import { Generic } from '@a11y-ui/core';
 
 import { watchString } from '../../utils/prop.validators';
 
+/* types */
 /**
- * Gibt an, welche Elemente kontrolliert werden. (https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-controls)
+ * Defines which elements are being controlled. (https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-controls)
+ * @deprecated
+ */
+export type AriaControlsPropType = string;
+
+/**
+ * @deprecated
  */
 export type PropAriaControls = {
-	ariaControls: string;
+	ariaControls: AriaControlsPropType;
 };
 
 /* validator */
-export const validateAriaControls = (component: Generic.Element.Component, value?: string): void => {
+export const validateAriaControls = (component: Generic.Element.Component, value?: AriaControlsPropType): void => {
 	watchString(component, '_ariaControls', value);
 };
