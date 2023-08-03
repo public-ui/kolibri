af878ddc7a36ce76ecb32827009e5fe53f4d88c9
Peter Laske <peter.laske@gmx.eu>
2023-08-03 10:49:43 +0200

    feat: add gap between inputs in range component for some themes
    Closes: #4260

--- packages/themes/bmf/src/index.ts
+++ packages/themes/bmf/src/index.ts
@@ -2236,8 +2236,8 @@ export const BMF = KoliBri.createTheme('bmf', {
 		font-size: 0.875em;
 		font-style: italic;
 	}
-	input {
-		border: none;
+	.inputs-wrapper {
+		gap: 1em;
 	}
 	input::placeholder {
 		color: var(--color-grey);
