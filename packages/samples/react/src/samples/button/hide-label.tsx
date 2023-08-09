a3f359ad62e5da6bc8d94d97489d091b7dd6fa8f
Stefan Dietz <mail@stefan-dietz.eu>
2023-08-09 16:35:37 +0200

    Add tooltipAlign examples

--- packages/samples/react/src/samples/button/hide-label.tsx
+++ packages/samples/react/src/samples/button/hide-label.tsx
@@ -14,10 +14,10 @@ const ARGS = {
 export const ButtonIconOnly: FC = () => (
 	<div className="grid gap-14">
 		<div className="flex flex-wrap gap-14">
-			<KolButton _label="Primary" _variant="primary" {...ARGS}></KolButton>
-			<KolButton _label="Secondary" _variant="secondary" {...ARGS}></KolButton>
-			<KolButton _label="Normal" _variant="normal" {...ARGS}></KolButton>
-			<KolButton _label="Danger" _variant="danger" {...ARGS}></KolButton>
+			<KolButton _label="Primary" _variant="primary" _tooltipAlign="top" {...ARGS}></KolButton>
+			<KolButton _label="Secondary" _variant="secondary" _tooltipAlign="right" {...ARGS}></KolButton>
+			<KolButton _label="Normal" _variant="normal" _tooltipAlign="bottom" {...ARGS}></KolButton>
+			<KolButton _label="Danger" _variant="danger" _tooltipAlign="left" {...ARGS}></KolButton>
 			<KolButton _label="Ghost" _variant="ghost" {...ARGS}></KolButton>
 		</div>
 		<div className="flex flex-wrap gap-14">
