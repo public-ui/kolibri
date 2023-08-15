1db18af4d6adf9ec55981c2b4bab114b85badce4
Peter Laske <peter.laske@gmx.eu>
2023-08-15 09:09:11 +0200

    feat: export toaster instance
    Deprecate exported toaster service class to make clear, that the toaster
    instance should be used.
    
    Refs: #4392

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,3 +1,5 @@
+import { ToasterService as ToasterServiceInternal } from './components/toast/toaster';
+
 export { register } from './core';
 export * from './components.d';
 export * from './enums/bund';
@@ -5,5 +7,10 @@ export * from './kolibri';
 export { configKoliBri } from './utils/dev.utils';
 export { Optgroup, Option, SelectOption } from './types/input/types';
 export { KoliBriDevHelper } from './utils/prop.validators';
-export { ToasterService } from './components/toast/toaster';
+export { Toaster } from './components/toast/toaster';
 export { translations } from './i18n';
+
+/**
+ * @deprecated Use Toaster instead
+ */
+export const ToasterService = ToasterServiceInternal;
