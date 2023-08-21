89b13e54fd0aecb72ed3ef1a61dcbdb28f4ae8fb
Stefan Dietz <mail@stefan-dietz.eu>
2023-08-21 11:44:59 +0200

    Refactor table property descriptions

--- packages/components/src/components/table/component.tsx
+++ packages/components/src/components/table/component.tsx
@@ -53,17 +53,17 @@ export class KolTable implements API {
 	@Prop() public _caption?: string;
 
 	/**
-	 * Gibt die Daten an, die für die Erstellung der Tabelle verwendet werden.
+	 * Defines the primary table data.
 	 */
 	@Prop() public _data!: Stringified<KoliBriTableDataType[]>;
 
 	/**
-	 * Hier können die Daten für die Fußzeile der Tabelle übergeben werden.
+	 * Defines the data for the table footer.
 	 */
 	@Prop() public _dataFoot?: Stringified<KoliBriTableDataType[]>;
 
 	/**
-	 * Gibt die horizontalen und vertikalen Header für die Tabelle an.
+	 * Defines the horizontal and vertical table headers.
 	 */
 	@Prop() public _headers!: Stringified<KoliBriTableHeaders>;
 
@@ -73,12 +73,12 @@ export class KolTable implements API {
 	@Prop() public _label?: string;
 
 	/**
-	 * Gibt an, die minimale Breite der Tabelle an.
+	 * Defines the table min-width.
 	 */
 	@Prop() public _minWidth?: string;
 
 	/**
-	 * Gibt an, ob die Daten geteilt in Seiten angezeigt wird.
+	 * Defines whether to show the data distributed over multiple pages.
 	 */
 	@Prop() public _pagination?: boolean | Stringified<KoliBriTablePaginationProps>;
 
