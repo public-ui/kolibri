76e8e0b08ca23aa791231410c18eed151179f022
Martin Oppitz <github@martinoppitz.com>
2023-08-22 11:31:48 +0200

    chore: update auto gen files

--- packages/adapters/vaadin/KolIndentedText.java
+++ packages/adapters/vaadin/KolIndentedText.java
@@ -12,7 +12,7 @@ import java.util.Optional;
  */
 
 @Tag("kol-indented-text")
-@NpmPackage(value = "@public-ui/components", version = "1.6.0-rc.28")
+@NpmPackage(value = "@public-ui/components", version = "1.6.0-rc.29")
 @JsModule("@public-ui/components/dist/components/kol-indented-text")
 public class KolIndentedText extends Component {
 }
