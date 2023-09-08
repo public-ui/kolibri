cd3059fe80b901454ef5a74988857e4cbec7922d
Stefan Dietz <mail@stefan-dietz.eu>
2023-09-08 11:54:21 +0200

    Stableize tests

--- packages/tools/visual-tests/tests/theme-snapshots.spec.ts
+++ packages/tools/visual-tests/tests/theme-snapshots.spec.ts
@@ -1,12 +1,11 @@
 import { test, expect } from '@playwright/test';
-// import { ROUTE_LIST } from '@public-ui/sample-react/src/routes.tsx';
-
-const routes = [`#/accordion/basic`, `#/avatar/basic`];
+// @ts-ignore
+import {routes} from '@public-ui/sample-react/src/routes-test.ts';
 
 routes.forEach((route) => {
 	test(`Matches snapshot for ${route}`, async ({ page }) => {
-		await page.goto(`/${route}`, { waitUntil: 'domcontentloaded' });
-		await page.waitForSelector(`body`);
+		await page.goto(`/#/${route}`, { waitUntil: 'networkidle' });
+		await page.waitForSelector(`kol-button[_aria-label="Weiter zum nächsten Komponenten-Beispiel"]`);
 		await expect(page).toHaveScreenshot({
 			fullPage: true,
 		});
