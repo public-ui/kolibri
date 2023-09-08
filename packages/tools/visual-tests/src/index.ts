36dfe1ff86614bac68cc63eeeb9c267ee380a7bb
Stefan Dietz <mail@stefan-dietz.eu>
2023-09-08 15:41:32 +0200

    Add TypeScript, clean up CL output

---
+++ packages/tools/visual-tests/src/index.ts
@@ -0,0 +1,45 @@
+import child_process from 'node:child_process';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import * as crypto from "crypto";
+import * as fs from "fs";
+
+process.env.KOLIBRI_CWD = process.cwd();
+
+if(!process.env.THEME_MODULE) {
+	throw new Error('Environment variable THEME_MODULE not specified.')
+}
+if(!process.env.TMPDIR) {
+	throw new Error('Environment variable TMPDIR not specified.')
+}
+
+process.env.THEME_MODULE = path.join(process.cwd(), process.env.THEME_MODULE); // Use current working directory (i.e. the theme folder) to complete module path
+const visualsTestModulePath = fileURLToPath(new URL('..', import.meta.url));
+const binaryPath = fileURLToPath(new URL('../node_modules/.bin', import.meta.url));
+const buildPath = path.join(process.env.TMPDIR, `kolibri-visual-testing-build-${crypto.randomUUID()}`);
+process.env.KOLIBRI_VISUAL_TESTS_BUILD_PATH = buildPath;
+
+console.log('Building React Sample App...');
+child_process.execFileSync(path.join(binaryPath, 'kolibri-sample-react-test-build'), [buildPath], {
+	encoding: 'utf-8',
+});
+
+console.log(`React Sample App build finished. Directory:`, buildPath);
+
+const playwright = child_process.spawn(path.join(binaryPath, 'playwright'), ['test', ...process.argv.slice(2)], {
+	cwd: visualsTestModulePath,
+});
+playwright.stdout.on('data', data => {
+	console.log('Plawright: ' + (data as string).toString());
+});
+
+playwright.stderr.on('data', data => {
+	console.log('Plawright stderr: ' + (data as string).toString());
+});
+
+playwright.on('exit', code => {
+	console.log(`Playwright test finished with exit code ${code}.`);
+	console.log('Cleaning up build folder...');
+	fs.rmSync(buildPath, { recursive: true, force: true });
+	process.exit(code ?? 1);
+});
