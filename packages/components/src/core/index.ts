3ed89d8d0dfb6fa0d2f22fd3b7c61a33422c995f
Martin Oppitz <github@martinoppitz.com>
2023-09-11 15:06:06 +0200

    refactor: i18n register

--- packages/components/src/core/index.ts
+++ packages/components/src/core/index.ts
@@ -3,7 +3,7 @@ import { register as coreRegister, STORE } from '@a11y-ui/core';
 
 import { I18nextService, II18nService } from './i18n';
 
-export const register = (
+export const register = async (
 	themes:
 		| Generic.Theming.RegisterPatch<string, string, string>
 		| Generic.Theming.RegisterPatch<string, string, string>[]
@@ -11,17 +11,14 @@ export const register = (
 	loaders: LoaderCallback | LoaderCallback[] | Set<LoaderCallback>,
 	options?: RegisterOptions
 ): Promise<void[]> => {
-	try {
-		if (STORE.I18n === undefined) {
-			Object.defineProperty(STORE, 'I18n', {
-				value: new I18nextService(options?.translation?.name ?? 'de', options?.translations),
-				writable: false,
-			});
-		}
-	} catch (e) {
-		// das Laden der Sprache ist optional
+	if (STORE.I18n === undefined) {
+		const i18n = await I18nextService.getInstance(options?.translation?.name ?? 'de', options?.translations);
+		Object.defineProperty(STORE, 'I18n', {
+			value: i18n,
+			writable: false,
+		});
 	}
-	return coreRegister(themes, loaders, options);
+	return await coreRegister(themes, loaders, options);
 };
 
 export const getI18nService: () => II18nService | undefined = () => STORE?.I18n as II18nService;
