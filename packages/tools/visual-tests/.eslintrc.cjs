6dda4137b0c5417b8efe40c7b5a24f3ce2216faa
Martin Oppitz <github@martinoppitz.com>
2023-09-14 11:50:51 +0200

    fix: eslint

--- packages/tools/visual-tests/.eslintrc.cjs
+++ packages/tools/visual-tests/.eslintrc.cjs
@@ -1,5 +1,18 @@
 module.exports = {
+	env: {
+		node: true,
+	},
 	root: true,
 	extends: ['eslint:recommended'],
+	parser: '@babel/eslint-parser',
+	parserOptions: {
+		requireConfigFile: false,
+		babelOptions: {
+			babelrc: false,
+			configFile: false,
+			// your babel options
+			presets: ['@babel/preset-env'],
+		},
+	},
 	plugins: ['no-loops'],
 };
