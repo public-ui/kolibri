44936ce7dac24c1888ad761cacd565ab369f6312
Stefan Dietz <mail@stefan-dietz.eu>
2023-09-21 19:25:16 +0200

    Touch up input-color samples

--- packages/samples/react/src/components/input-color/basic.tsx
+++ packages/samples/react/src/components/input-color/basic.tsx
@@ -6,24 +6,23 @@ import { FC } from 'react';
 import { ERROR_MSG } from '../../shares/constants';
 
 export const InputColorBasic: FC = () => (
-	<KolForm className="grid gap-4">
-		<KolInputColor
-			_id="farbe"
-			_name="farbe"
-			_value="#ff0000"
-			_error={ERROR_MSG}
-			_label="Farbe"
-			_icon={{
-				left: {
-					icon: 'codicon codicon-arrow-left',
-				},
-				right: {
-					icon: 'codicon codicon-arrow-right',
-				},
-			}}
-		/>
-		<KolInputColor _id="farb1" _label="Farbe" _list="['#000000','#ff0000', '#0000ff','#00ff00']" _error={ERROR_MSG} />
-		<KolInputColor _id="farbe2" _hint="Hilfetext" _value="#ff0000" _label="Farbe (Disabled)" />
-		<KolInputColor _disabled _id="farbe3" _value="#ff0000" _label="Farbe (Disabled)" />
+	<KolForm>
+		<div className="grid gap-4">
+			<KolInputColor
+				_value="#f08080"
+				_label="Farbe"
+				_icon={{
+					left: {
+						icon: 'codicon codicon-arrow-left',
+					},
+					right: {
+						icon: 'codicon codicon-arrow-right',
+					},
+				}}
+			/>
+			<KolInputColor _label="Farbe mit Fehler" _list="['#000000','#f08080', '#0000ff','#00ff00']" _error={ERROR_MSG} _touched />
+			<KolInputColor _hint="Hilfetext" _value="#f08080" _label="Farbe mit Hilfstext" />
+			<KolInputColor _disabled _value="#f08080" _label="Farbe Disabled" />
+		</div>
 	</KolForm>
 );
