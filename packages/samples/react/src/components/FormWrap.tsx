834fea779e15d5d6fd11ab0c9a098991ab84a860
Stefan Dietz <mail@stefan-dietz.eu>
2023-10-13 14:50:43 +0200

    Add support for icon style properties for input components

--- packages/samples/react/src/components/FormWrap.tsx
+++ packages/samples/react/src/components/FormWrap.tsx
@@ -13,6 +13,15 @@ export const FormWrap: FC<FormWrapProps> = (props) => (
 			<FocusElement {...props} />
 			<div className="flex gap-4">
 				<KolButton _label="Submit" _icons="codicon codicon-arrow-right" _type="submit" _variant="primary" />
+				<KolButton
+					_label="Bunte Icons"
+					_icons={{
+						left: { icon: 'codicon codicon-heart-filled', style: { color: '#cc006e' } },
+						right: { icon: 'codicon codicon-squirrel', style: { color: '#b41b1b', fontSize: '24px' } },
+					}}
+					_type="submit"
+					_variant="secondary"
+				/>
 				<KolButton _label="Reset" _type="reset" _variant="tertiary" />
 				<KolButton _label="Help" _type="button" _variant="ghost" />
 			</div>
