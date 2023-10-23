bdf462acf3fceed6c94cb7b99e22be09d33fa3a5
Martin Oppitz <github@martinoppitz.com>
2023-10-23 08:02:27 +0200

    chore: restore compact button

--- packages/tools/kolibri-cli/src/migrate/runner/tasks/v2/nav.ts
+++
@@ -1,3 +0,0 @@
-import { RemovePropertyNameTask } from '../common/RemovePropertyNameTask';
-
-export const NavRemovePropertyHasCompactButton = RemovePropertyNameTask.getInstance('kol-nav', '_has-compact-button', '^1');
