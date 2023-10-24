39468a1121ef048a05785286ee7bf4bb28842774
Stefan Dietz <mail@stefan-dietz.eu>
2023-10-24 11:38:38 +0200

    bzst: Add css tags

--- packages/samples/react/src/App.tsx
+++ packages/samples/react/src/App.tsx
@@ -45,7 +45,8 @@ const getRouteTree = (routes: MyRoutes): ReturnType<typeof Route>[] => {
 						element={
 							<div className="d-grid gap-4">
 								{THEME_OPTIONS.filter(
-									(theme) => ['bmf', 'default', 'ecl-ec', 'ecl-eu', 'itzbund', 'mapz', 'zoll-v2', 'zoll-v3'].indexOf((theme as Option<Theme>).value) >= 0,
+									(theme) =>
+										['bmf', 'default', 'bzst', 'ecl-ec', 'ecl-eu', 'itzbund', 'mapz', 'zoll-v2', 'zoll-v3'].indexOf((theme as Option<Theme>).value) >= 0,
 								).map((theme) => (
 									<div className="d-grid gap-2" key={(theme as Option<Theme>).value} data-theme={(theme as Option<Theme>).value}>
 										<div className="mt-4">
