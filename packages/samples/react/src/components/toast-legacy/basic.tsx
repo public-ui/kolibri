397da0e7888cd0f43fe9359a297c2f8292407bee
Stefan Dietz <mail@stefan-dietz.eu>
2023-10-27 12:36:12 +0200

    Extend toast legacy examples

--- packages/samples/react/src/components/toast-legacy/basic.tsx
+++ packages/samples/react/src/components/toast-legacy/basic.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { FC } from 'react';
-import { KolToast } from '@public-ui/react';
+import { KolButton, KolToast } from '@public-ui/react';
+import { ToasterServiceLegacy } from '@public-ui/components';
 
-export const ToastBasic: FC = () => (
-	<div>
-		<KolToast _label="Foo">Toast content</KolToast>
-		<KolToast _label="Foo" _hasCloser>
-			Toast content
-		</KolToast>
-	</div>
-);
+export const ToastBasic: FC = () => {
+	const toaster = ToasterServiceLegacy.getInstance(document);
+
+	const handleButtonClick = () => {
+		toaster.enqueue({
+			label: `Noch ein Toast`,
+			description: `Beschreibung zum Toast`,
+			type: 'info',
+		});
+	};
+	return (
+		<div>
+			<KolToast _label="Legacy Toast Titel">Toast content</KolToast>
+			<KolToast _label="Zweiter Toast mit Schließen-Button" _hasCloser>
+				Toast content
+			</KolToast>
+
+			<KolButton _label="Weiteren Toast öffnen" _on={{ onClick: handleButtonClick }}></KolButton>
+		</div>
+	);
+};
