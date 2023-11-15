b41ad8d52df757d7edfafda5af909fa83e995c2e
Stefan Dietz <mail@stefan-dietz.eu>
2023-11-15 15:14:51 +0100

    Fix test scenarios

--- packages/samples/react/src/components/table/test-data.ts
+++ packages/samples/react/src/components/table/test-data.ts
@@ -1,66 +1,66 @@
 export type Data = {
 	order: number;
-	date: Date;
+	date: String;
 };
 export const DATA: Data[] = [
 	{
 		order: 0,
-		date: new Date('1981-05-26T21:33:43.612Z'),
+		date: new Date('1981-05-26T21:33:43.612Z').toString(),
 	},
 	{
 		order: 1,
-		date: new Date('1971-04-25T19:44:17.014Z'),
+		date: new Date('1971-04-25T19:44:17.014Z').toString(),
 	},
 	{
 		order: 2,
-		date: new Date('1986-07-10T11:39:29.539Z'),
+		date: new Date('1986-07-10T11:39:29.539Z').toString(),
 	},
 	{
 		order: 3,
-		date: new Date('1976-02-02T10:00:36.346Z'),
+		date: new Date('1976-02-02T10:00:36.346Z').toString(),
 	},
 	{
 		order: 4,
-		date: new Date('1998-07-07T12:50:36.016Z'),
+		date: new Date('1998-07-07T12:50:36.016Z').toString(),
 	},
 	{
 		order: 5,
-		date: new Date('1994-09-05T05:10:44.078Z'),
+		date: new Date('1994-09-05T05:10:44.078Z').toString(),
 	},
 	{
 		order: 6,
-		date: new Date('1986-05-23T13:05:01.874Z'),
+		date: new Date('1986-05-23T13:05:01.874Z').toString(),
 	},
 	{
 		order: 7,
-		date: new Date('1975-06-29T21:51:29.359Z'),
+		date: new Date('1975-06-29T21:51:29.359Z').toString(),
 	},
 	{
 		order: 8,
-		date: new Date('1974-05-15T23:47:02.499Z'),
+		date: new Date('1974-05-15T23:47:02.499Z').toString(),
 	},
 	{
 		order: 9,
-		date: new Date('1990-05-27T10:13:19.641Z'),
+		date: new Date('1990-05-27T10:13:19.641Z').toString(),
 	},
 	{
 		order: 10,
-		date: new Date('1993-10-20T03:54:08.739Z'),
+		date: new Date('1993-10-20T03:54:08.739Z').toString(),
 	},
 	{
 		order: 11,
-		date: new Date('2000-02-25T11:55:43.705Z'),
+		date: new Date('2000-02-25T11:55:43.705Z').toString(),
 	},
 	{
 		order: 12,
-		date: new Date('1988-08-27T09:59:12.251Z'),
+		date: new Date('1988-08-27T09:59:12.251Z').toString(),
 	},
 	{
 		order: 13,
-		date: new Date('1980-10-24T10:41:24.897Z'),
+		date: new Date('1980-10-24T10:41:24.897Z').toString(),
 	},
 	{
 		order: 14,
-		date: new Date('1995-01-25T12:31:27.983Z'),
+		date: new Date('1995-01-25T12:31:27.983Z').toString(),
 	},
 ];
