a61f5467174fffdba918a29259a0ca86cf9d7e04
Martin Oppitz <github@martinoppitz.com>
2023-11-27 17:35:21 +0100

    Release v1.7.6-rc.2

--- packages/components/src/utils/dev.utils.ts
+++ packages/components/src/utils/dev.utils.ts
@@ -134,7 +134,7 @@ export const initKoliBri = (): void => {
 	|  .   '  | .-. | |  | ,--. |  .-.  \\ |  .--' ,--.
 	|  |\\   \\ | '-' | |  | |  | |  '--' / |  |    |  |
 	\`--' \`--´  \`---´  \`--' \`--' \`------´  \`--'    \`--'
-	🚹 The accessible HTML-Standard | 👉 https://public-ui.github.io | 1.7.6-rc.1
+	🚹 The accessible HTML-Standard | 👉 https://public-ui.github.io | 1.7.6-rc.2
 		`,
 			{
 				forceLog: true,
