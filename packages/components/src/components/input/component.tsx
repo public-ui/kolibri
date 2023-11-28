5a5405ef85d4fef2d54974ade3a833050cd753d1
Stefan Dietz <mail@stefan-dietz.eu>
2023-11-28 15:05:35 +0100

    Style input-text and input-date

--- packages/components/src/components/input/component.tsx
+++ packages/components/src/components/input/component.tsx
@@ -57,7 +57,7 @@ export class KolInput implements Props {
 			>
 				<label class="input-label" id={!useTooltopInsteadOfLabel ? `${this._id}-label` : undefined} hidden={useTooltopInsteadOfLabel} htmlFor={this._id}>
 					{/* INFO: span is needed for css styling :after content like a star (*) or optional text ! */}
-					<span>
+					<span class="input-label-span">
 						{/* INFO: label comes with any html tag or as plain text! */}
 						<slot name="label"></slot>
 					</span>
