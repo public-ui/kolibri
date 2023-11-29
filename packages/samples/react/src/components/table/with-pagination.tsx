1fc30f2371d697d62e8c0c1ade27dda34462f9f3
Stefan Dietz <mail@stefan-dietz.eu>
2023-11-29 12:45:19 +0100

    Fix linting errors

--- packages/samples/react/src/components/table/with-pagination.tsx
+++ packages/samples/react/src/components/table/with-pagination.tsx
@@ -2,14 +2,14 @@ import React, { FC } from 'react';
 
 import { KolTable } from '@public-ui/react';
 import { KoliBriTableHeaders, KoliBriTablePaginationProps } from '@public-ui/components';
-import { DATA } from './test-data';
+import { Data, DATA } from './test-data';
 import { DATE_FORMATTER } from './formatter';
 
 const HEADERS: KoliBriTableHeaders = {
 	horizontal: [
 		[
 			{ label: 'Order', key: 'order' },
-			{ label: 'Date', key: 'date', render: (_el, _cell, tupel) => DATE_FORMATTER.format((tupel as Data).date) },
+			{ label: 'Date', key: 'date', render: (_el, _cell, tupel) => DATE_FORMATTER.format((tupel as unknown as Data).date) },
 		],
 	],
 };
