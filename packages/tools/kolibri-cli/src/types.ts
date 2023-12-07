c18611b3cbaf91ad6cc00db03f9987443cc00132
Martin <6279703+deleonio@users.noreply.github.com>
2023-12-07 15:53:41 +0100

    Update types.ts
    Signed-off-by: Martin <6279703+deleonio@users.noreply.github.com>

--- packages/tools/kolibri-cli/src/types.ts
+++ packages/tools/kolibri-cli/src/types.ts
@@ -1,4 +1,4 @@
-export const FILE_EXTENSIONS = ['html', 'js', 'json', 'jsx', 'ts', 'tsx', 'vue'] as const;
+export const FILE_EXTENSIONS = ['html', 'xhtml', 'js', 'json', 'jsx', 'ts', 'tsx', 'vue'] as const;
 export type FileExtension = (typeof FILE_EXTENSIONS)[number];
 
 export const COMPONENT_FILE_EXTENSIONS: FileExtension[] = ['jsx', 'tsx', 'vue'];
