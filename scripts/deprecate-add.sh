ff44781d89fef8397b7ce4edbd4159f57b85a21b
Martin Oppitz <github@martinoppitz.com>
2023-12-12 07:19:12 +0100

    chore: add react standalone to scripts

--- scripts/deprecate-add.sh
+++ scripts/deprecate-add.sh
@@ -9,6 +9,7 @@ npm deprecate @public-ui/hydrate@$1 "Wir empfehlen die Verwendung einer neueren 
 npm deprecate @public-ui/kolibri-cli@$1 "Wir empfehlen die Verwendung einer neueren Version (>=$2)."
 npm deprecate @public-ui/preact@$1 "Wir empfehlen die Verwendung einer neueren Version (>=$2)."
 npm deprecate @public-ui/react@$1 "Wir empfehlen die Verwendung einer neueren Version (>=$2)."
+npm deprecate @public-ui/react-standalone@$1 "Wir empfehlen die Verwendung einer neueren Version (>=$2)."
 npm deprecate @public-ui/solid@$1 "Wir empfehlen die Verwendung einer neueren Version (>=$2)."
 npm deprecate @public-ui/vue@$1 "Wir empfehlen die Verwendung einer neueren Version (>=$2)."
 npm deprecate @public-ui/components@$1 "Wir empfehlen die Verwendung einer neueren Version (>=$2)."
