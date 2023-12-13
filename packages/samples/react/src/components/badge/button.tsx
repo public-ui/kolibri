42deecd80239c250a6bbddd76904e11ca02b2b96
https://gitlab.isoad.isogmbh.de/%rmd% <cyrine.benromdhane@iso-gruppe.com>
2023-12-13 15:23:15 +0100

    Update README and adjust BadgeButton label text

--- packages/samples/react/src/components/badge/button.tsx
+++ packages/samples/react/src/components/badge/button.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import { KolBadge } from '@public-ui/react';
+import { KolBadge, KolHeading } from '@public-ui/react';
 
 import { FC } from 'react';
 
-const PROPS = {
+const createBadgePROPS = (label: String) => ({
 	_smartButton: {
 		_icons: 'codicon codicon-close',
-		_label: 'Entfernen',
+		_label: `Entfernen von ${label}`,
 		_on: {
 			onClick: () => alert('clicked'),
 		},
 	},
-};
+});
 
 export const BadgeButton: FC = () => (
-	<div className="flex gap-2">
-		<KolBadge _label="schwarz" {...PROPS}></KolBadge>
-		<KolBadge _label="türkis" _color="#86ffc6" {...PROPS}></KolBadge>
-		<KolBadge _label="blau" _color="#06539e" {...PROPS}></KolBadge>
-		<KolBadge _label="rot" _color="#ae0000" {...PROPS}></KolBadge>
-		<KolBadge _label="lila" _color="#8b008b" {...PROPS}></KolBadge>
+	<div>
+		<div className="mb-4">
+			<KolHeading _label="Badge" _level={2} />
+		</div>
+		<div className="flex gap-2">
+			<KolBadge _label="schwarz" {...createBadgePROPS('schwarz')}></KolBadge>
+			<KolBadge _label="türkis" _color="#86ffc6" {...createBadgePROPS('türkis')}></KolBadge>
+			<KolBadge _label="blau" _color="#06539e" {...createBadgePROPS('blau')}></KolBadge>
+			<KolBadge _label="rot" _color="#ae0000" {...createBadgePROPS('rot')}></KolBadge>
+			<KolBadge _label="lila" _color="#8b008b" {...createBadgePROPS('lila')}></KolBadge>
+		</div>
 	</div>
 );
