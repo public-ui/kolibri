6607305cc352d93961e7934f09bbcf7f51fe9dc7
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2024-01-04 19:52:56 +0100

    chore: release 2.0.3

--- packages/components/src/utils/dev.utils.ts
+++ packages/components/src/utils/dev.utils.ts
@@ -143,7 +143,7 @@ export const initKoliBri = (): void => {
 	|  .   '  | .-. | |  | ,--. |  .-.  \\ |  .--' ,--.
 	|  |\\   \\ | '-' | |  | |  | |  '--' / |  |    |  |
 	\`--' \`--´  \`---´  \`--' \`--' \`------´  \`--'    \`--'
-	🚹 The accessible HTML-Standard | 👉 https://public-ui.github.io | 2.0.2
+	🚹 The accessible HTML-Standard | 👉 https://public-ui.github.io | 2.0.3
 		`,
 			{
 				forceLog: true,
