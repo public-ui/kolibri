a84ef75fafcf3b93f4e594fb10f0192ff46dcc9b
Stefan Dietz <mail@stefan-dietz.eu>
2024-01-08 16:54:56 +0100

    Fix alert samples

--- packages/components/src/components/alert/component.tsx
+++ packages/components/src/components/alert/component.tsx
@@ -79,7 +79,7 @@ export class KolAlertWc implements API {
 						{typeof this.state._label === 'string' && this.state._label?.length > 0 && (
 							<kol-heading-wc _label={this.state._label} _level={this.state._level}></kol-heading-wc>
 						)}
-						{this._variant === 'msg' && (
+						{this.state._variant === 'msg' && (
 							<div class="content">
 								<slot />
 							</div>
