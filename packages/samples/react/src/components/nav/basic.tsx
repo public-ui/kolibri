d2c03608782c21e91e59f2bf90a5cca2c88a41b0
https://gitlab.isoad.isogmbh.de/%rmd% <cyrine.benromdhane@iso-gruppe.com>
2024-01-09 10:41:33 +0100

    Correct HTML example for Nav

--- packages/samples/react/src/components/nav/basic.tsx
+++ packages/samples/react/src/components/nav/basic.tsx
@@ -4,4 +4,4 @@ import { KolNav } from '@public-ui/react';
 import { FC } from 'react';
 import { LINKS } from './links';
 
-export const NavBasic: FC = () => <KolNav class="block w-fit" _label="Main navigation" _links={JSON.stringify(LINKS)} _hasCompactButton />;
+export const NavBasic: FC = () => <KolNav class="block w-fit" _label="Main navigation" _links={LINKS} _hasCompactButton />;
