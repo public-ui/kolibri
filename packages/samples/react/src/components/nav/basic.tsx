bcd534cd39fd53cc1ec0554d255a70630f262c9d
Stefan Dietz <mail@stefan-dietz.eu>
2024-02-05 09:15:44 +0100

    Implement new prop setHasIconsWhenExpanded

--- packages/samples/react/src/components/nav/basic.tsx
+++ packages/samples/react/src/components/nav/basic.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { KolNav } from '@public-ui/react';
+import { KolInputCheckbox, KolNav } from '@public-ui/react';
 
 import { LINKS } from './links';
 
 import type { FC } from 'react';
-export const NavBasic: FC = () => <KolNav class="block w-fit" _label="Main navigation" _links={LINKS} _hasCompactButton />;
+export const NavBasic: FC = () => {
+	const [hasIconsWhenExpanded, setHasIconsWhenExpanded] = useState(false);
+
+	return (
+		<>
+			<KolInputCheckbox
+				_label="Show icons when expanded"
+				_checked={hasIconsWhenExpanded}
+				_on={{
+					onChange: (_event, value: unknown) => {
+						setHasIconsWhenExpanded(value as boolean);
+					},
+				}}
+			></KolInputCheckbox>
+			<KolNav class="block w-fit" _label="Main navigation" _links={LINKS} _hasCompactButton _hasIconsWhenExpanded={hasIconsWhenExpanded} />
+		</>
+	);
+};
