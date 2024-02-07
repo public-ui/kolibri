eb8834d02e21459c89e3ac1454c23a4d47933c4f
sik <simone.perkins@iso-gruppe.com>
2024-02-07 13:41:56 +0100

    Refactor CSS ecl-eu

--- packages/themes/ecl/src/ecl-ec.ts
+++ packages/themes/ecl/src/ecl-ec.ts
@@ -3,51 +3,21 @@ import { css } from './cssTag';
 
 // Europa Component Library - European Commission | https://ec.europa.eu/component-library/ec/
 export const ECL_EC = KoliBri.createTheme('ecl-ec', {
-	GLOBAL: css`
-		
-	`,
-	'KOL-HEADING': css`
-		
-	`,
-	'KOL-ACCORDION': css`
-		
-	`,
-	'KOL-INDENTED-TEXT': css`
-		
-	`,
-	'KOL-BUTTON': css`
-		
-	`,
-	'KOL-LINK-BUTTON': css`
-		
-	`,
-	'KOL-BADGE': css`
-		
-	`,
-	'KOL-ALERT': css`
-		
-	`,
-	'KOL-AVATAR': css`
-		
-	`,
-	'KOL-TABS': css`
-		
-	`,
-	'KOL-LINK': css`
-		
-	`,
-	'KOL-BUTTON-LINK': css`
-		
-	`,
-	'KOL-BREADCRUMB': css`
-		
-	`,
-	'KOL-DETAILS': css`
-		
-	`,
-	'KOL-PROGRESS': css`
-		
-	`,
+	GLOBAL: css``,
+	'KOL-HEADING': css``,
+	'KOL-ACCORDION': css``,
+	'KOL-INDENTED-TEXT': css``,
+	'KOL-BUTTON': css``,
+	'KOL-LINK-BUTTON': css``,
+	'KOL-BADGE': css``,
+	'KOL-ALERT': css``,
+	'KOL-AVATAR': css``,
+	'KOL-TABS': css``,
+	'KOL-LINK': css``,
+	'KOL-BUTTON-LINK': css``,
+	'KOL-BREADCRUMB': css``,
+	'KOL-DETAILS': css``,
+	'KOL-PROGRESS': css``,
 	'KOL-SPIN': css`
 		.cycle {
 			padding: 0.125rem;
@@ -56,64 +26,26 @@ export const ECL_EC = KoliBri.createTheme('ecl-ec', {
 			}
 		}
 	`,
-	'KOL-PAGINATION': css`
-		
-	`,
-	'KOL-INPUT-CHECKBOX': css`
-		
-	`,
-	'KOL-INPUT-COLOR': css`
-		
-	`,
-	'KOL-CARD': css`
-		
-	`,
+	'KOL-PAGINATION': css``,
+	'KOL-INPUT-CHECKBOX': css``,
+	'KOL-INPUT-COLOR': css``,
+	'KOL-CARD': css``,
 	'KOL-BUTTON-GROUP': ``,
-	'KOL-INPUT-RADIO': css`
-		
-	`,
-	'KOL-INPUT-RANGE': css`
-		
-	`,
-	'KOL-INPUT-TEXT': css`
-		
-	`,
-	'KOL-KOLIBRI': css`
-		
-	`,
-	'KOL-INPUT-PASSWORD': css`
-		
-	`,
-	'KOL-INPUT-NUMBER': css`
-		
-	`,
-	'KOL-INPUT-DATE': css`
-		
-	`,
-	'KOL-INPUT-EMAIL': css`
-		
-	`,
-	'KOL-INPUT-FILE': css`
-		
-	`,
-	'KOL-SELECT': css`
-		
-	`,
-	'KOL-TEXTAREA': css`
-		
-	`,
+	'KOL-INPUT-RADIO': css``,
+	'KOL-INPUT-RANGE': css``,
+	'KOL-INPUT-TEXT': css``,
+	'KOL-KOLIBRI': css``,
+	'KOL-INPUT-PASSWORD': css``,
+	'KOL-INPUT-NUMBER': css``,
+	'KOL-INPUT-DATE': css``,
+	'KOL-INPUT-EMAIL': css``,
+	'KOL-INPUT-FILE': css``,
+	'KOL-SELECT': css``,
+	'KOL-TEXTAREA': css``,
 	'KOL-ICON': ``,
-	'KOL-TABLE': css`
-		
-	`,
-	'KOL-NAV': css`
-		
-	`,
-	'KOL-SKIP-NAV': css`
-		
-	`,
-	'KOL-SPLIT-BUTTON': css`
-		
-	`,
+	'KOL-TABLE': css``,
+	'KOL-NAV': css``,
+	'KOL-SKIP-NAV': css``,
+	'KOL-SPLIT-BUTTON': css``,
 	'KOL-TOAST-CONTAINER': ``,
 });
