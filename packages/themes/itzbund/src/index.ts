c97dff3928ba553e9c7d1390e45fb9bf25988a8b
sik <simone.perkins@iso-gruppe.com>
2024-02-07 14:20:58 +0100

    Edit .ts Dateien

--- packages/themes/itzbund/src/index.ts
+++ packages/themes/itzbund/src/index.ts
@@ -1,39 +1,70 @@
 import { KoliBri } from '@public-ui/schema';
-import { css } from './cssTag';
+import globalCss from './global.scss';
+import accordionCss from './components/accordion.scss';
+import alertCss from './components/alert.scss';
+import badgeCss from './components/badge.scss';
+import breadcrumbCss from './components/breadcrumb.scss';
+import buttonCss from './components/button.scss';
+import buttonGroupCss from './components/button-group.scss';
+import buttonLinkCss from './components/button-link.scss';
+import cardCss from './components/card.scss';
+import headingCss from './components/heading.scss';
+import indentedTextCss from './components/indented-text.scss';
+import inputCheckboxCss from './components/input-checkbox.scss';
+import inputColorCss from './components/input-color.scss';
+import inputDateCss from './components/input-date.scss';
+import inputEmailCss from './components/input-email.scss';
+import inputFileCss from './components/input-file.scss';
+import inputNumberCss from './components/input-number.scss';
+import inputPasswordCss from './components/input-password.scss';
+import inputRadioCss from './components/input-radio.scss';
+import inputTextCss from './components/input-text.scss';
+import linkButtonCss from './components/link-button.scss';
+import linkCss from './components/link.scss';
+import navCss from './components/nav.scss';
+import paginationCss from './components/pagination.scss';
+import progressCss from './components/progress.scss';
+import selectCss from './components/select.scss';
+import skipNavCss from './components/skip-nav.scss';
+import spinCss from './components/spin.scss';
+import splitButtonCss from './components/split-button.scss';
+import tabsCss from './components/tabs.scss';
+import textareaCss from './components/textarea.scss';
+import toastContainerCss from './components/toast-container.scss';
 
 // Informationstechnikzentrum Bund
 export const ITZBund = KoliBri.createTheme('itzbund', {
-	GLOBAL: css``,
-	'KOL-BUTTON': css``,
-	'KOL-BUTTON-GROUP': css``,
-	'KOL-LINK-BUTTON': css``,
-	'KOL-PAGINATION': css``,
-	'KOL-BUTTON-LINK': css``,
-	'KOL-INPUT-TEXT': css``,
-	'KOL-INPUT-PASSWORD': css``,
-	'KOL-INPUT-NUMBER': css``,
-	'KOL-INPUT-DATE': css``,
-	'KOL-INPUT-EMAIL': css``,
-	'KOL-INPUT-FILE': css``,
-	'KOL-TEXTAREA': css``,
-	'KOL-ALERT': css``,
-	'KOL-HEADING': css``,
-	'KOL-BADGE': css``,
-	'KOL-INDENTED-TEXT': css``,
-	'KOL-LINK': css``,
-	'KOL-BREADCRUMB': css``,
-	'KOL-SPIN': css``,
-	'KOL-PROGRESS': css``,
-	'KOL-SELECT': css``,
-	'KOL-INPUT-COLOR': css``,
-	'KOL-ACCORDION': css``,
-	'KOL-TABLE': css``,
-	'KOL-NAV': css``,
-	'KOL-CARD': css``,
-	'KOL-INPUT-CHECKBOX': css``,
-	'KOL-INPUT-RADIO': `/* INPUT */`,
-	'KOL-TOAST-CONTAINER': ``,
-	'KOL-TABS': ``,
-	'KOL-SKIP-NAV': ``,
-	'KOL-SPLIT-BUTTON': ``,
+	GLOBAL: globalCss,
+	'KOL-BUTTON': buttonCss,
+	'KOL-BUTTON-GROUP': buttonGroupCss,
+	'KOL-LINK-BUTTON': linkButtonCss,
+	'KOL-PAGINATION': paginationCss,
+	'KOL-BUTTON-LINK': buttonLinkCss,
+	'KOL-INPUT-TEXT': inputTextCss,
+	'KOL-INPUT-PASSWORD': inputPasswordCss,
+	'KOL-INPUT-NUMBER': inputNumberCss,
+	'KOL-INPUT-DATE': inputDateCss,
+	'KOL-INPUT-EMAIL': inputEmailCss,
+	'KOL-INPUT-FILE': inputFileCss,
+	'KOL-TEXTAREA': textareaCss,
+	'KOL-ALERT': alertCss,
+	'KOL-HEADING': headingCss,
+	'KOL-BADGE': badgeCss,
+	'KOL-INDENTED-TEXT': indentedTextCss,
+	'KOL-LINK': linkCss,
+	'KOL-BREADCRUMB': breadcrumbCss,
+	'KOL-SPIN': spinCss,
+	'KOL-PROGRESS': progressCss,
+	'KOL-SELECT': selectCss,
+	'KOL-INPUT-COLOR': inputColorCss,
+	'KOL-ACCORDION': accordionCss,
+	'KOL-TABLE': tabsCss,
+	'KOL-NAV': navCss,
+	'KOL-CARD': cardCss,
+	'KOL-INPUT-CHECKBOX': inputCheckboxCss,
+	'KOL-INPUT-RADIO': inputRadioCss,
+	'KOL-TOAST-CONTAINER': toastContainerCss,
+	'KOL-TABS': tabsCss,
+	'KOL-SKIP-NAV': skipNavCss,
+	'KOL-SPLIT-BUTTON': splitButtonCss,
 });
