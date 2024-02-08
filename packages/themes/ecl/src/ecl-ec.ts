925ae9aa7520c40057ad7d9df50ecf570aa36ccd
sik <simone.perkins@iso-gruppe.com>
2024-02-08 10:11:40 +0100

    Edit .ts Dateien

--- packages/themes/ecl/src/ecl-ec.ts
+++ packages/themes/ecl/src/ecl-ec.ts
@@ -1,51 +1,81 @@
 import { KoliBri } from '@public-ui/schema';
-import { css } from './cssTag';
+import globalCss from './global-ec.scss';
+import accordionCss from './components/ecl-ec/accordion.scss';
+import alertCss from './components/ecl-ec/alert.scss';
+import avatarCss from './components/ecl-ec/avatar.scss';
+import badgeCss from './components/ecl-ec/badge.scss';
+import breadcrumbCss from './components/ecl-ec/breadcrumb.scss';
+import buttonCss from './components/ecl-ec/button.scss';
+import buttonGroupCss from './components/ecl-ec/button-group.scss';
+import buttonLinkCss from './components/ecl-ec/button-link.scss';
+import cardCss from './components/ecl-ec/card.scss';
+import detailsCss from './components/ecl-ec/details.scss';
+import headingCss from './components/ecl-ec/heading.scss';
+import iconCss from './components/ecl-ec/icon.scss';
+import indentedTextCss from './components/ecl-ec/indented-text.scss';
+import inputCheckboxCss from './components/ecl-ec/input-checkbox.scss';
+import inputColorCss from './components/ecl-ec/input-color.scss';
+import inputDateCss from './components/ecl-ec/input-date.scss';
+import inputEmailCss from './components/ecl-ec/input-email.scss';
+import inputFileCss from './components/ecl-ec/input-file.scss';
+import inputNumberCss from './components/ecl-ec/input-number.scss';
+import inputPasswordCss from './components/ecl-ec/input-password.scss';
+import inputRadioCss from './components/ecl-ec/input-radio.scss';
+import inputRangeCss from './components/ecl-ec/input-range.scss';
+import inputTextCss from './components/ecl-ec/input-text.scss';
+import linkButtonCss from './components/ecl-ec/link-button.scss';
+import kolibriCss from './components/ecl-ec/kolibri.scss';
+import linkCss from './components/ecl-ec/link.scss';
+import navCss from './components/ecl-ec/nav.scss';
+import paginationCss from './components/ecl-ec/pagination.scss';
+import progressCss from './components/ecl-ec/progress.scss';
+import selectCss from './components/ecl-ec/select.scss';
+import skipNavCss from './components/ecl-ec/skip-nav.scss';
+import spinCss from './components/ecl-ec/spin.scss';
+import splitButtonCss from './components/ecl-ec/split-button.scss';
+import tableCss from './components/ecl-ec/table.scss';
+import tabsCss from './components/ecl-ec/tabs.scss';
+import textareaCss from './components/ecl-ec/textarea.scss';
+import toastContainerCss from './components/ecl-ec/toast-container.scss';
 
 // Europa Component Library - European Commission | https://ec.europa.eu/component-library/ec/
 export const ECL_EC = KoliBri.createTheme('ecl-ec', {
-	GLOBAL: css``,
-	'KOL-HEADING': css``,
-	'KOL-ACCORDION': css``,
-	'KOL-INDENTED-TEXT': css``,
-	'KOL-BUTTON': css``,
-	'KOL-LINK-BUTTON': css``,
-	'KOL-BADGE': css``,
-	'KOL-ALERT': css``,
-	'KOL-AVATAR': css``,
-	'KOL-TABS': css``,
-	'KOL-LINK': css``,
-	'KOL-BUTTON-LINK': css``,
-	'KOL-BREADCRUMB': css``,
-	'KOL-DETAILS': css``,
-	'KOL-PROGRESS': css``,
-	'KOL-SPIN': css`
-		.cycle {
-			padding: 0.125rem;
-			& span {
-				background-color: var(--color-blue-75);
-			}
-		}
-	`,
-	'KOL-PAGINATION': css``,
-	'KOL-INPUT-CHECKBOX': css``,
-	'KOL-INPUT-COLOR': css``,
-	'KOL-CARD': css``,
-	'KOL-BUTTON-GROUP': ``,
-	'KOL-INPUT-RADIO': css``,
-	'KOL-INPUT-RANGE': css``,
-	'KOL-INPUT-TEXT': css``,
-	'KOL-KOLIBRI': css``,
-	'KOL-INPUT-PASSWORD': css``,
-	'KOL-INPUT-NUMBER': css``,
-	'KOL-INPUT-DATE': css``,
-	'KOL-INPUT-EMAIL': css``,
-	'KOL-INPUT-FILE': css``,
-	'KOL-SELECT': css``,
-	'KOL-TEXTAREA': css``,
-	'KOL-ICON': ``,
-	'KOL-TABLE': css``,
-	'KOL-NAV': css``,
-	'KOL-SKIP-NAV': css``,
-	'KOL-SPLIT-BUTTON': css``,
-	'KOL-TOAST-CONTAINER': ``,
+	GLOBAL: globalCss,
+	'KOL-HEADING': headingCss,
+	'KOL-ACCORDION': accordionCss,
+	'KOL-INDENTED-TEXT': indentedTextCss,
+	'KOL-BUTTON': buttonCss,
+	'KOL-LINK-BUTTON': linkButtonCss,
+	'KOL-BADGE': badgeCss,
+	'KOL-ALERT': alertCss,
+	'KOL-AVATAR': avatarCss,
+	'KOL-TABS': tabsCss,
+	'KOL-LINK': linkCss,
+	'KOL-BUTTON-LINK': buttonLinkCss,
+	'KOL-BREADCRUMB': breadcrumbCss,
+	'KOL-DETAILS': detailsCss,
+	'KOL-PROGRESS': progressCss,
+	'KOL-SPIN': spinCss,
+	'KOL-PAGINATION': paginationCss,
+	'KOL-INPUT-CHECKBOX': inputCheckboxCss,
+	'KOL-INPUT-COLOR': inputColorCss,
+	'KOL-CARD': cardCss,
+	'KOL-BUTTON-GROUP': buttonGroupCss,
+	'KOL-INPUT-RADIO': inputRadioCss,
+	'KOL-INPUT-RANGE': inputRangeCss,
+	'KOL-INPUT-TEXT': inputTextCss,
+	'KOL-KOLIBRI': kolibriCss,
+	'KOL-INPUT-PASSWORD': inputPasswordCss,
+	'KOL-INPUT-NUMBER': inputNumberCss,
+	'KOL-INPUT-DATE': inputDateCss,
+	'KOL-INPUT-EMAIL': inputEmailCss,
+	'KOL-INPUT-FILE': inputFileCss,
+	'KOL-SELECT': selectCss,
+	'KOL-TEXTAREA': textareaCss,
+	'KOL-ICON': iconCss,
+	'KOL-TABLE': tableCss,
+	'KOL-NAV': navCss,
+	'KOL-SKIP-NAV': skipNavCss,
+	'KOL-SPLIT-BUTTON': splitButtonCss,
+	'KOL-TOAST-CONTAINER': toastContainerCss,
 });
--- packages/themes/ecl/src/ecl-eu.ts
+++ packages/themes/ecl/src/ecl-eu.ts
@@ -1,117 +1,78 @@
 import { KoliBri } from '@public-ui/schema';
-import { css } from './cssTag';
+import globalCss from './global-ec.scss';
+import accordionCss from './components/ecl-eu/accordion.scss';
+import alertCss from './components/ecl-eu/alert.scss';
+import avatarCss from './components/ecl-eu/avatar.scss';
+import badgeCss from './components/ecl-eu/badge.scss';
+import breadcrumbCss from './components/ecl-eu/breadcrumb.scss';
+import buttonCss from './components/ecl-eu/button.scss';
+import buttonGroupCss from './components/ecl-eu/button-group.scss';
+import buttonLinkCss from './components/ecl-eu/button-link.scss';
+import cardCss from './components/ecl-eu/card.scss';
+import detailsCss from './components/ecl-eu/details.scss';
+import headingCss from './components/ecl-eu/heading.scss';
+import indentedTextCss from './components/ecl-eu/indented-text.scss';
+import inputCheckboxCss from './components/ecl-eu/input-checkbox.scss';
+import inputColorCss from './components/ecl-eu/input-color.scss';
+import inputDateCss from './components/ecl-eu/input-date.scss';
+import inputEmailCss from './components/ecl-eu/input-email.scss';
+import inputFileCss from './components/ecl-eu/input-file.scss';
+import inputNumberCss from './components/ecl-eu/input-number.scss';
+import inputPasswordCss from './components/ecl-eu/input-password.scss';
+import inputRadioCss from './components/ecl-eu/input-radio.scss';
+import inputRangeCss from './components/ecl-eu/input-range.scss';
+import inputTextCss from './components/ecl-eu/input-text.scss';
+import linkButtonCss from './components/ecl-eu/link-button.scss';
+import kolibriCss from './components/ecl-eu/kolibri.scss';
+import linkCss from './components/ecl-eu/link.scss';
+import navCss from './components/ecl-eu/nav.scss';
+import paginationCss from './components/ecl-eu/pagination.scss';
+import progressCss from './components/ecl-eu/progress.scss';
+import selectCss from './components/ecl-eu/select.scss';
+import skipNavCss from './components/ecl-eu/skip-nav.scss';
+import spinCss from './components/ecl-eu/spin.scss';
+import splitButtonCss from './components/ecl-eu/split-button.scss';
+import tabsCss from './components/ecl-eu/tabs.scss';
+import textareaCss from './components/ecl-eu/textarea.scss';
+import toastContainerCss from './components/ecl-eu/toast-container.scss';
 
 // Europa Component Library - European Union | https://ec.europa.eu/component-library/eu/
 export const ECL_EU = KoliBri.createTheme('ecl-eu', {
-	GLOBAL: css`
-		
-	`,
-	'KOL-HEADING': css`
-		
-	`,
-	'KOL-ACCORDION': css`
-		
-	`,
-	'KOL-INDENTED-TEXT': css`
-		
-	`,
-	'KOL-BUTTON': css`
-		
-	`,
-	'KOL-LINK-BUTTON': css`
-		
-	`,
-	'KOL-BUTTON-GROUP': css`
-		
-	`,
-	'KOL-PAGINATION': css`
-		
-	`,
-	'KOL-PROGRESS': css`
-
-	`,
-	'KOL-NAV': css`
-		
-	`,
-	'KOL-BADGE': css`
-		
-	`,
-	'KOL-ALERT': css`
-		
-	`,
-	'KOL-CARD': css`
-		
-	`,
-	'KOL-INPUT-CHECKBOX': css`
-		
-	`,
-	'KOL-INPUT-COLOR': css`
-		
-	`,
-	'KOL-INPUT-FILE': css`
-		
-	`,
-	'KOL-INPUT-EMAIL': css`
-		
-	`,
-	'KOL-INPUT-NUMBER': css`
-		
-	`,
-	'KOL-INPUT-DATE': css`
-		
-	`,
-	'KOL-INPUT-PASSWORD': css`
-		
-	`,
-	'KOL-INPUT-TEXT': css`
-		
-	`,
-	'KOL-KOLIBRI': css`
-		
-	`,
-	'KOL-SELECT': css`
-		
-	`,
-	'KOL-TEXTAREA': css`
-		
-	`,
-	'KOL-TABLE': css`
-		
-	`,
-	'KOL-AVATAR': css`
-		
-	`,
-	'KOL-TABS': css`
-		
-	`,
-	'KOL-LINK': css`
-		
-	`,
-	'KOL-BUTTON-LINK': css`
-		
-	`,
-	'KOL-BREADCRUMB': css`
-		
-	`,
-	'KOL-DETAILS': css`
-		
-	`,
-	'KOL-SPIN': css`
-	
-	`,
-	'KOL-INPUT-RADIO': css`
-		
-	`,
-	'KOL-INPUT-RANGE': css`
-		
-	`,
-	'KOL-SKIP-NAV': css`
-		
-	`,
-	'KOL-SPLIT-BUTTON': css`
-		
-	`,
-	'KOL-TOAST-CONTAINER': css`
-		
-	`,
+	GLOBAL: globalCss,
+	'KOL-HEADING': headingCss,
+	'KOL-ACCORDION': accordionCss,
+	'KOL-INDENTED-TEXT': indentedTextCss,
+	'KOL-BUTTON': buttonCss,
+	'KOL-LINK-BUTTON': linkButtonCss,
+	'KOL-BUTTON-GROUP': buttonGroupCss,
+	'KOL-PAGINATION': paginationCss,
+	'KOL-PROGRESS': progressCss,
+	'KOL-NAV': navCss,
+	'KOL-BADGE': badgeCss,
+	'KOL-ALERT': alertCss,
+	'KOL-CARD': cardCss,
+	'KOL-INPUT-CHECKBOX': inputCheckboxCss,
+	'KOL-INPUT-COLOR': inputColorCss,
+	'KOL-INPUT-FILE': inputFileCss,
+	'KOL-INPUT-EMAIL': inputEmailCss,
+	'KOL-INPUT-NUMBER': inputNumberCss,
+	'KOL-INPUT-DATE': inputDateCss,
+	'KOL-INPUT-PASSWORD': inputPasswordCss,
+	'KOL-INPUT-TEXT': inputTextCss,
+	'KOL-KOLIBRI': kolibriCss,
+	'KOL-SELECT': selectCss,
+	'KOL-TEXTAREA': textareaCss,
+	'KOL-TABLE': tabsCss,
+	'KOL-AVATAR': avatarCss,
+	'KOL-TABS': tabsCss,
+	'KOL-LINK': linkCss,
+	'KOL-BUTTON-LINK': buttonLinkCss,
+	'KOL-BREADCRUMB': breadcrumbCss,
+	'KOL-DETAILS': detailsCss,
+	'KOL-SPIN': spinCss,
+	'KOL-INPUT-RADIO': inputRadioCss,
+	'KOL-INPUT-RANGE': inputRangeCss,
+	'KOL-SKIP-NAV': skipNavCss,
+	'KOL-SPLIT-BUTTON': splitButtonCss,
+	'KOL-TOAST-CONTAINER': toastContainerCss,
 });
