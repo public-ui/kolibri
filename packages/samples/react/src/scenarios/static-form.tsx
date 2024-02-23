bd2dab1d93effbce0304d565f88741524b1cc79b
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2024-02-23 08:24:00 +0100

    fix: value handling for inputs

--- packages/samples/react/src/scenarios/static-form.tsx
+++ packages/samples/react/src/scenarios/static-form.tsx
@@ -15,34 +15,39 @@ import {
 } from '@public-ui/react';
 import type { FC } from 'react';
 import React from 'react';
+import { SampleDescription } from '../components/SampleDescription';
 
 export const StaticForm: FC = () => {
 	return (
 		<>
-			<p>This sample shows how you can use KoliBri in static form context, too.</p>
-			<ol>
-				<li>
-					First you have to enable the <code>experimental mode</code>:{' '}
-					<code className="bg-gray-200">&lt;meta name=&quot;kolibri&quot; content=&quot;dev-mode=false;experimental-mode=true;&quot; /&gt;</code>
-				</li>
-				<li>
-					Currently you have to use a native <code>form</code> element: <code className="bg-gray-200">&lt;form method=&quot;GET&quot;&gt;...&lt;/form&gt;</code>
-				</li>
-				<li>
-					It is important in static usage to give every input a <code>name</code> attribute:{' '}
-					<code className="bg-gray-200">&lt;KolInputColor _name=&quot;color&quot; _label=&quot;Color&quot; /&gt;</code>
-				</li>
-				<li>
-					Last of all one button should have the type <code>submit</code>:{' '}
-					<code className="bg-gray-200">&lt;KolButton _label=&quot;Submit&quot; _type=&quot;submit&quot; _variant=&quot;primary&quot; /&gt;</code>
-				</li>
-			</ol>
+			<SampleDescription>
+				<p>This sample shows how you can use KoliBri in static form context, too.</p>
+				<ol>
+					<li>
+						First you have to enable the <code>experimental mode</code>:{' '}
+						<code className="bg-gray-200">&lt;meta name=&quot;kolibri&quot; content=&quot;dev-mode=false;experimental-mode=true;&quot; /&gt;</code>
+					</li>
+					<li>
+						Currently you have to use a native <code>form</code> element:{' '}
+						<code className="bg-gray-200">&lt;form method=&quot;GET&quot;&gt;...&lt;/form&gt;</code>
+					</li>
+					<li>
+						It is important in static usage to give every input a <code>name</code> attribute:{' '}
+						<code className="bg-gray-200">&lt;KolInputColor _name=&quot;color&quot; _label=&quot;Color&quot; /&gt;</code>
+					</li>
+					<li>
+						Last of all one button should have the type <code>submit</code>:{' '}
+						<code className="bg-gray-200">&lt;KolButton _label=&quot;Submit&quot; _type=&quot;submit&quot; _variant=&quot;primary&quot; /&gt;</code>
+					</li>
+				</ol>
+			</SampleDescription>
 			<form className="grid gap-4" method="get">
 				<KolInputCheckbox _name="checkbox" _label="Checkbox" />
 				<KolInputColor _name="color" _label="Color" />
 				<KolInputDate _name="date" _label="Date" />
 				<KolInputEmail _name="email" _label="Email" />
 				<KolInputFile _name="file" _label="File" />
+				<KolInputFile _name="file" _label="Files (multiple)" _multiple />
 				<KolInputNumber _name="number" _label="Number" />
 				<KolInputPassword _name="password" _label="Password" />
 				<KolInputRadio
@@ -63,7 +68,17 @@ export const StaticForm: FC = () => {
 						{ label: 'Option B', value: 'B' },
 					]}
 				/>
-				<KolTextarea _name="textarea" _label="Textarea" />
+				<KolSelect
+					_name="select"
+					_label="Select (multiple)"
+					_multiple
+					_options={[
+						{ label: 'Option A', value: 'A' },
+						{ label: 'Option B', value: 'B' },
+					]}
+					_rows={2}
+				/>
+				<KolTextarea _name="textarea" _label="Textarea" _rows={5} />
 				<div className="flex flex-wrap gap-4">
 					<KolButton _label="Submit" _type="submit" _variant="primary" />
 					<KolButton _label="Reset" _type="reset" />
