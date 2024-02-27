4abb6b82693a3660bc1d1702dbe4b0b711608096
Cyrine Ben Romdhane <135605630+anicyne@users.noreply.github.com>
2024-02-27 08:09:22 +0100

    Remove initialization route for code example alert

--- packages/samples/react/src/App.tsx
+++ packages/samples/react/src/App.tsx
@@ -136,7 +136,6 @@ export const App: FC = () => {
 					{!hideMenus && isDraftTheme(theme) && <KolBadge className="mb-3" _label="DRAFT" _color="#db5461" />}
 					<Routes>
 						{ROUTE_TREE}
-						<Route path="/" element={<KolAlert _type="info">This code example has not been migrated yet - it&#39;s coming soon!</KolAlert>} />
 						<Route path="*" element={<Navigate to={ROUTE_LIST[0]} replace />} />
 						<Route path="back-page" element={<BackPage />} />
 					</Routes>
