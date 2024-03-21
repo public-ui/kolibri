ee40c4a84dd720394464af60a8b1dfdb6372b21e
Stefan Dietz <mail@stefan-dietz.eu>
2024-03-21 15:31:51 +0100

    Progress bar: Allow dynamic SVG width, refactor, make <text> part of the SVG

--- packages/components/src/components/progress/component.tsx
+++ packages/components/src/components/progress/component.tsx
@@ -5,79 +5,101 @@ import { Component, h, Host, Prop, State, Watch } from '@stencil/core';
 import type { JSX } from '@stencil/core';
 const VALID_VARIANTS = Object.keys(KoliBriProgressVariantEnum);
 
-// https://css-tricks.com/html5-progress-element/
-const createProgressSVG = (state: ProgressStates): JSX.Element => {
+const CycleSvg = ({ state }: { state: ProgressStates }) => {
 	const fullCircle = 342;
 	const textPositionTop = '43%';
 	const textPositionBottom = '57%';
+	const valueY = state._label ? textPositionBottom : '50%';
 
-	let labelY = textPositionTop;
-	let valueY = state._label ? textPositionBottom : '50%';
+	return (
+		<svg class="cycle" width="100" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">
+			<circle class="background" cx="60" cy="60" r="54.5" fill="currentColor" stroke="currentColor" stroke-width="8"></circle>
+			<circle class="whitespace" cx="60" cy="60" r="59" fill="currentColor" stroke="currentColor" stroke-width="3"></circle>
+			<circle class="border" cx="60" cy="60" r="59" fill="currentColor" stroke="currentColor" stroke-width="1"></circle>
+			<circle class="whitespace" cx="60" cy="60" r="51" fill="currentColor" stroke="currentColor" stroke-width="1"></circle>
+			<circle class="border" cx="60" cy="60" r="50" fill="currentColor" stroke="currentColor" stroke-width="1"></circle>
+			<circle
+				class="progress"
+				fill="currentColor"
+				stroke="currentColor"
+				stroke-linecap="round"
+				stroke-dasharray={`${Math.round((state._value / state._max) * fullCircle)}px ${fullCircle}px`}
+				stroke-width="6"
+				cx="60"
+				cy="60"
+				r="54.5"
+			></circle>
+			{state._label && (
+				<text aria-hidden="true" x="50%" y={textPositionTop} text-anchor="middle" fill="currentColor">
+					{state._label}
+				</text>
+			)}
+			<text aria-hidden="true" x="50%" y={valueY} text-anchor="middle" fill="currentColor">
+				{state._value}
+				{state._unit}
+			</text>
+		</svg>
+	);
+};
+
+const BarSvg = ({ state }: { state: ProgressStates }) => {
+	const textLabelPadding = 'var(--kolibri-text-label-padding, 45px)';
+	const percentage = 100 * (state._value / state._max);
+
+	return (
+		<div class="bar">
+			{state._label && <div>{state._label}</div>}
+			<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="12" overflow="visible">
+				<rect
+					class="background"
+					x="1"
+					y="1"
+					height="10"
+					rx="5"
+					fill="currentColor"
+					stroke="currentColor"
+					stroke-width="3"
+					style={{ width: `calc(100% - 2px - ${textLabelPadding})` }}
+				></rect>
+				<rect
+					class="border"
+					x="1"
+					y="1"
+					height="10"
+					rx="5"
+					fill="currentColor"
+					stroke="currentColor"
+					stroke-width="1"
+					style={{ width: `calc(100% - 2px - ${textLabelPadding})` }}
+				></rect>
+				<rect
+					class="progress"
+					x="2.5"
+					y="2.5"
+					height="7"
+					rx="3.5"
+					fill="currentColor"
+					stroke="currentColor"
+					stroke-width="3"
+					style={{ width: `calc(${percentage}% - 5px - (${textLabelPadding} / 100 * ${percentage}))` }}
+				></rect>
+				<text aria-hidden="true" text-anchor="end" dominant-baseline="middle" fill="currentColor" x="100%" y="50%">
+					{state._value}
+					{state._unit}
+				</text>
+			</svg>
+		</div>
+	);
+};
+
+const createProgressSVG = (state: ProgressStates): JSX.Element => {
 	switch (state._variant) {
-		case 'cycle-value-label':
-			if (state._label) {
-				labelY = textPositionBottom;
-				valueY = textPositionTop;
-			}
-		// eslint-disable-next-line no-fallthrough
 		case 'cycle':
-		case 'cycle-label-value':
-			return (
-				<svg class="cycle" width="100" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">
-					<circle class="background" cx="60" cy="60" r="54.5" fill="currentColor" stroke="currentColor" stroke-width="8"></circle>
-					<circle class="whitespace" cx="60" cy="60" r="59" fill="currentColor" stroke="currentColor" stroke-width="3"></circle>
-					<circle class="border" cx="60" cy="60" r="59" fill="currentColor" stroke="currentColor" stroke-width="1"></circle>
-					<circle class="whitespace" cx="60" cy="60" r="51" fill="currentColor" stroke="currentColor" stroke-width="1"></circle>
-					<circle class="border" cx="60" cy="60" r="50" fill="currentColor" stroke="currentColor" stroke-width="1"></circle>
-					<circle
-						class="progress"
-						fill="currentColor"
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-dasharray={`${Math.round((state._value / state._max) * fullCircle)}px ${fullCircle}px`}
-						stroke-width="6"
-						cx="60"
-						cy="60"
-						r="54.5"
-					></circle>
-					{state._label && (
-						<text aria-hidden="true" x="50%" y={labelY} text-anchor="middle" fill="currentColor">
-							{state._label}
-						</text>
-					)}
-					<text aria-hidden="true" x="50%" y={valueY} text-anchor="middle" fill="currentColor">
-						{state._value}
-						{state._unit}
-					</text>
-				</svg>
-			);
+			return <CycleSvg state={state} />;
+		case 'bar':
+			return <BarSvg state={state} />;
 		default:
-			return (
-				<div class="bar">
-					{state._label && <div>{state._label}</div>}
-					<div style={{ display: 'flex', gap: '0.3em' }}>
-						<svg width="100" viewBox="0 0 102 8" xmlns="http://www.w3.org/2000/svg">
-							<rect class="background" x="1" y="1" height="10" rx="5" fill="currentColor" stroke="currentColor" stroke-width="3" width="100"></rect>
-							<rect
-								class="progress"
-								x="2.5"
-								y="2.5"
-								height="7"
-								rx="3.5"
-								fill="currentColor"
-								stroke="currentColor"
-								stroke-width="3"
-								width={95 * (state._value / state._max)}
-							></rect>
-							<rect class="border" x="1" y="1" height="10" rx="5" fill="currentColor" stroke="currentColor" stroke-width="1" width="100"></rect>
-						</svg>
-						<text aria-hidden="true" text-anchor="middle" dominant-baseline="central" fill="currentColor">
-							{state._value}
-							{state._unit}
-						</text>
-					</div>
-				</div>
-			);
+			throw new Error(`Progress variant ${state._variant} not implemented.`);
 	}
 };
 
@@ -96,6 +118,8 @@ export class KolProcess implements ProgressAPI {
 		return (
 			<Host class="kol-progress">
 				{createProgressSVG(this.state)}
+
+				{/* https://css-tricks.com/html5-progress-element/ */}
 				<progress aria-busy={this.state._value < this.state._max ? 'true' : 'false'} max={this.state._max} value={this.state._value}></progress>
 				<span aria-live="polite" aria-relevant="removals text" class="visually-hidden">
 					{this.state._liveValue} von {this.state._max} {this.state._unit}
