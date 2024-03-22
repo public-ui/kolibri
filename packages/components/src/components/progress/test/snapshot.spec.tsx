d40b76f439b3a6dcf59df264b39363e13c41a9f2
Stefan Dietz <mail@stefan-dietz.eu>
2024-03-22 12:28:34 +0100

    Adjust progress bar tests

--- packages/components/src/components/progress/test/snapshot.spec.tsx
+++ packages/components/src/components/progress/test/snapshot.spec.tsx
@@ -26,25 +26,23 @@ describe('test Progress', () => {
 		});
 		expect(page.root).toEqualHtml(
 			`<kol-progress class="kol-progress">
-         <mock:shadow-root>
-            <div class="bar">
-             <div style="display: flex; gap: 0.3em;">
-               <svg width="100" viewBox="0 0 102 8" xmlns="http://www.w3.org/2000/svg">
-                <rect class="background" x="1" y="1" height="10" rx="5" fill="currentColor" stroke="currentColor" stroke-width="3" width="100"></rect>
-                  <rect class="progress" fill="currentColor" height="7" rx="3.5" stroke="currentColor" stroke-width="3" width="0" x="2.5" y="2.5"></rect>
-             <rect class="border" x="1" y="1" height="10" rx="5" fill="currentColor" stroke="currentColor" stroke-width="1" width="100"></rect>
-           </svg>
-             <text aria-hidden="true" dominant-baseline="central" fill="currentColor" text-anchor="middle">
-                         0%
-                        </text>
-             </div>
-           </div>
-           <progress aria-busy="true" max="100" value="0"></progress>
-           <span ${DEFAULT_HTML_SPAN_PROPS} class="visually-hidden">
-             0 von 100 %
-           </span>
-         </mock:shadow-root>
-       </kol-progress>`,
+	<mock:shadow-root>
+		<div class="bar">
+			<svg height="12" overflow="visible" width="100%" xmlns="http://www.w3.org/2000/svg">
+				<rect class="background" fill="currentColor" height="10" rx="5" stroke="currentColor" stroke-width="3" x="1" y="1" style="width: calc(100% - 2px - var(--kolibri-text-label-padding, 45px));"></rect>
+				<rect class="border" fill="currentColor" height="10" rx="5" stroke="currentColor" stroke-width="1" x="1" y="1" style="width: calc(100% - 2px - var(--kolibri-text-label-padding, 45px));"></rect>
+				<rect class="progress" fill="currentColor" height="7" rx="3.5" stroke="currentColor" stroke-width="3" x="2.5" y="2.5" style="width: calc(0% - 5px - (var(--kolibri-text-label-padding, 45px) / 100 * 0));"></rect>
+				<text aria-hidden="true" dominant-baseline="middle" fill="currentColor" text-anchor="end" x="100%" y="50%">
+					0%
+				</text>
+				</svg>
+			</div>
+			<progress aria-busy="true" max="100" value="0"></progress>
+			<span ${DEFAULT_HTML_SPAN_PROPS} class="visually-hidden">
+			0 von 100 %
+			</span>
+		</mock:shadow-root>
+	</kol-progress>`,
 		);
 	});
 
@@ -56,25 +54,23 @@ describe('test Progress', () => {
 		});
 		expect(page.root).toEqualHtml(
 			`<kol-progress class="kol-progress">
-         <mock:shadow-root>
-            <div class="bar">
-             <div style="display: flex; gap: 0.3em;">
-               <svg width="100" viewBox="0 0 102 8" xmlns="http://www.w3.org/2000/svg">
-                <rect class="background" x="1" y="1" height="10" rx="5" fill="currentColor" stroke="currentColor" stroke-width="3" width="100"></rect>
-                  <rect class="progress" fill="currentColor" height="7" rx="3.5" stroke="currentColor" stroke-width="3" width="38.452380952380956" x="2.5" y="2.5"></rect>
-             <rect class="border" x="1" y="1" height="10" rx="5" fill="currentColor" stroke="currentColor" stroke-width="1" width="100"></rect>
-           </svg>
-             <text aria-hidden="true" dominant-baseline="central" fill="currentColor" text-anchor="middle">
-                          ${DEFAULT_PROPS._value}%
-                        </text>
-             </div>
-           </div>
-           <progress aria-busy="true" max="${DEFAULT_PROPS._max}" value="${DEFAULT_PROPS._value}"></progress>
-           <span ${DEFAULT_HTML_SPAN_PROPS} class="visually-hidden">
-             0 von ${DEFAULT_PROPS._max} %
-           </span>
-         </mock:shadow-root>
-       </kol-progress>`,
+		<mock:shadow-root>
+			<div class="bar">
+				<svg height="12" overflow="visible" width="100%" xmlns="http://www.w3.org/2000/svg">
+					<rect class="background" fill="currentColor" height="10" rx="5" stroke="currentColor" stroke-width="3" x="1" y="1" style="width: calc(100% - 2px - var(--kolibri-text-label-padding, 45px));"></rect>
+					<rect class="border" fill="currentColor" height="10" rx="5" stroke="currentColor" stroke-width="1" x="1" y="1" style="width: calc(100% - 2px - var(--kolibri-text-label-padding, 45px));"></rect>
+					<rect class="progress" fill="currentColor" height="7" rx="3.5" stroke="currentColor" stroke-width="3" x="2.5" y="2.5" style="width: calc(40.476190476190474% - 5px - (var(--kolibri-text-label-padding, 45px) / 100 * 40.476190476190474));"></rect>
+					<text aria-hidden="true" dominant-baseline="middle" fill="currentColor" text-anchor="end" x="100%" y="50%">
+					${DEFAULT_PROPS._value}%
+					</text>
+				</svg>
+			</div>
+			<progress aria-busy="true" max="${DEFAULT_PROPS._max}" value="${DEFAULT_PROPS._value}"></progress>
+			<span ${DEFAULT_HTML_SPAN_PROPS} class="visually-hidden">
+			0 von ${DEFAULT_PROPS._max} %
+			</span>
+		</mock:shadow-root>
+	</kol-progress>`,
 		);
 	});
 
@@ -85,35 +81,23 @@ describe('test Progress', () => {
 		});
 		expect(page.root).toEqualHtml(
 			` <kol-progress class="kol-progress">
-         <mock:shadow-root>
-            <div class="bar">
-             <div style="display: flex; gap: 0.3em;">
-               <svg width="100" viewBox="0 0 102 8" xmlns="http://www.w3.org/2000/svg">
-                <rect class="background" x="1" y="1" height="10" rx="5" fill="currentColor" stroke="currentColor" stroke-width="3" width="100"></rect>
-                 <rect
-								   class="progress"
-                  x="2.5"
-               y="2.5"
-               height="7"
-               rx="3.5"
-               fill="currentColor"
-               stroke="currentColor"
-               stroke-width="3"
-               width="95"
-             ></rect>
-             <rect class="border" x="1" y="1" height="10" rx="5" fill="currentColor" stroke="currentColor" stroke-width="1" width="100"></rect>
-           </svg>
-             <text aria-hidden="true" dominant-baseline="central" fill="currentColor" text-anchor="middle">
-                          42%
-                        </text>
-             </div>
-           </div>
-           <progress aria-busy="false" max="${DEFAULT_PROPS._max}" value="${DEFAULT_PROPS._max}"></progress>
-           <span ${DEFAULT_HTML_SPAN_PROPS} class="visually-hidden">
-             0 von ${DEFAULT_PROPS._max} %
-           </span>
-         </mock:shadow-root>
-       </kol-progress>`,
+		<mock:shadow-root>
+			<div class="bar">
+				<svg height="12" overflow="visible" width="100%" xmlns="http://www.w3.org/2000/svg">
+					<rect class="background" fill="currentColor" height="10" rx="5" stroke="currentColor" stroke-width="3" x="1" y="1" style="width: calc(100% - 2px - var(--kolibri-text-label-padding, 45px));"></rect>
+					<rect class="border" fill="currentColor" height="10" rx="5" stroke="currentColor" stroke-width="1" x="1" y="1" style="width: calc(100% - 2px - var(--kolibri-text-label-padding, 45px));"></rect>
+					<rect class="progress" fill="currentColor" height="7" rx="3.5" stroke="currentColor" stroke-width="3" x="2.5" y="2.5" style="width: calc(100% - 5px - (var(--kolibri-text-label-padding, 45px) / 100 * 100));"></rect>
+					<text aria-hidden="true" dominant-baseline="middle" fill="currentColor" text-anchor="end" x="100%" y="50%">
+						42%
+					</text>
+				</svg>
+			</div>
+			<progress aria-busy="false" max="${DEFAULT_PROPS._max}" value="${DEFAULT_PROPS._max}"></progress>
+			<span ${DEFAULT_HTML_SPAN_PROPS} class="visually-hidden">
+			0 von ${DEFAULT_PROPS._max} %
+			</span>
+		</mock:shadow-root>
+	</kol-progress>`,
 		);
 	});
 
@@ -124,7 +108,7 @@ describe('test Progress', () => {
 		});
 		expect(page.root).toEqualHtml(
 			`<kol-progress class="kol-progress">
-         <mock:shadow-root>
+	       <mock:shadow-root>
            <svg class="cycle" viewBox="0 0 120 120" width="100" xmlns="http://www.w3.org/2000/svg">
              <circle class="background" cx="60" cy="60" r="54.5" fill="currentColor" stroke="currentColor" stroke-width="8"></circle>
              <circle class="whitespace" cx="60" cy="60" r="59" fill="currentColor" stroke="currentColor" stroke-width="3"></circle>
