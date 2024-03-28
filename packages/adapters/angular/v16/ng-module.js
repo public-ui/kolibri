e591c52b5cf3c5c8fccd864e6fc0aa4ebc467772
Stefan Dietz <mail@stefan-dietz.eu>
2024-03-28 11:48:40 +0100

    Add missing CommonModule import to Angular modules

--- packages/adapters/angular/v16/ng-module.js
+++ packages/adapters/angular/v16/ng-module.js
@@ -9,16 +9,17 @@ fs.writeFileSync(
 	`
 /* AutoGen NgModule */
 
+import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { ${componentListStr} } from './components';
 import { ReplaceTagDirective } from './angular-component-lib/ReplaceTagDirective';
 export { setTagNameTransformer } from './angular-component-lib/tagNameTransformer';
 
-
 @NgModule({
   declarations: [ReplaceTagDirective, ${componentListStr}],
   exports: [${componentListStr}],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [CommonModule],
 })
 export class KoliBriModule {}
 export { ${componentListStr} }
