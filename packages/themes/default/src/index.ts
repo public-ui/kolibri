aeaa05d16d7c0fce84f0f75f3e468c952a0a8a1b
Stefan Dietz <mail@stefan-dietz.eu>
2024-04-09 14:22:06 +0200

    Remove table styling

--- packages/themes/default/src/index.ts
+++ packages/themes/default/src/index.ts
@@ -34,7 +34,6 @@ import progressCss from './components/progress.scss';
 import selectCss from './components/select.scss';
 import skipNavCss from './components/skip-nav.scss';
 import splitButtonCss from './components/split-button.scss';
-import tableCss from './components/table.scss';
 import tableStatefulCss from './components/table-stateful.scss';
 import tableStatelessCss from './components/table-stateless.scss';
 import tabsCss from './components/tabs.scss';
@@ -79,7 +78,6 @@ export const DEFAULT = KoliBri.createTheme('default', {
 	'KOL-SELECT': selectCss,
 	'KOL-SKIP-NAV': skipNavCss,
 	'KOL-SPLIT-BUTTON': splitButtonCss,
-	'KOL-TABLE': tableCss,
 	'KOL-TABLE-STATEFUL': tableStatefulCss,
 	'KOL-TABLE-STATELESS': tableStatelessCss,
 	'KOL-TABS': tabsCss,
