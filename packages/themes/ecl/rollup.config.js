aeac1644e7316da2458151406be5be9de41500c4
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2024-04-26 16:52:26 +0200

    chore: move code and reconfigure package.json
    Refs: #6352

---
+++ packages/themes/ecl/rollup.config.js
@@ -0,0 +1,26 @@
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+
+export default {
+	input: 'src/index.ts',
+	output: [
+		{
+			file: 'dist/index.cjs',
+			format: 'cjs',
+			sourcemap: true,
+		},
+		{
+			file: 'dist/index.mjs',
+			format: 'es',
+			sourcemap: true,
+		},
+	],
+	external: ['@public-ui/components'],
+	plugins: [
+		typescript(),
+		postcss({
+			plugins: [],
+			inject: false,
+		}),
+	],
+};
