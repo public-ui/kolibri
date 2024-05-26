180b2c22ff7d92aacfe0b813fd15919fe2bf328b
Stefan Dietz <mail@stefan-dietz.eu>
2024-05-26 10:36:44 +0200

    Add real Accordion tests
    Refs: #6161

--- packages/components/e2e/accordion-set-content.e2e.ts
+++ packages/components/e2e/accordion-set-content.e2e.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { test } from '@stencil/playwright';
 
-test.describe('my-component', () => {
+test.skip('Accordion (set content test)', () => {
 	test('should render the correct name', async ({ page }) => {
 		await page.setContent('<kol-accordion _label="My Accordion" _level={1}>My Accordion content</kol-accordion>');
 
--- packages/components/e2e/accordion.e2e.ts
+++ packages/components/e2e/accordion.e2e.ts
@@ -1,9 +1,31 @@
 import { expect } from '@playwright/test';
 import { test } from '@stencil/playwright';
 
-test('accordion', async ({ page }) => {
-	await page.goto('/#/accordion/basic');
-	await expect(page.getByText('Inhalt Accordion Tab 1')).not.toBeVisible();
-	await page.getByRole('button', { name: 'Überschrift Accordion Tab 1' }).click();
-	await expect(page.getByText('Inhalt Accordion Tab 1')).toBeVisible();
+test.describe('Accordion', () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto('/#/accordion/basic');
+	});
+
+	test('should render the accordion title', async ({ page }) => {
+		expect(await page.locator('kol-accordion').first().getByRole('button')).toHaveText('Überschrift Accordion Tab 1');
+	});
+
+	test('should render the accordion content after the title has been clicked', async ({ page }) => {
+		await expect(page.getByText('Inhalt Accordion Tab 1')).not.toBeVisible();
+		await page.getByRole('button', { name: 'Überschrift Accordion Tab 1' }).click();
+		await expect(page.getByText('Inhalt Accordion Tab 1')).toBeVisible();
+	});
+
+	test('should hide the accordion content after the title has been clicked again', async ({ page }) => {
+		await page.getByRole('button', { name: 'Überschrift Accordion Tab 1' }).click();
+		await expect(page.getByText('Inhalt Accordion Tab 1')).toBeVisible();
+		await page.getByRole('button', { name: 'Überschrift Accordion Tab 1' }).click();
+		await expect(page.getByText('Inhalt Accordion Tab 1')).not.toBeVisible();
+	});
+
+	test("should not hide the accordion content after the title has been clicked if it's disabled", async ({ page }) => {
+		await expect(page.getByText('Inhalt Accordion Tab 2')).toBeVisible();
+		await page.getByRole('button', { name: 'Überschrift Accordion Tab 2 (deaktiviert)' }).click();
+		await expect(page.getByText('Inhalt Accordion Tab 2')).toBeVisible();
+	});
 });
