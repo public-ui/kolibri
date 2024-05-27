8725737fbc8e6014fce0f2132dc94c21a2ee8027
Stefan Dietz <mail@stefan-dietz.eu>
2024-05-27 10:45:03 +0200

    Add some Combobox tests
    Refs: #6161

---
+++ packages/components/e2e/combobox.e2e.ts
@@ -0,0 +1,28 @@
+import { expect } from '@playwright/test';
+import { test } from '@stencil/playwright';
+
+test.describe('Combobox', () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto('/#/combobox/basic');
+	});
+
+	test('should show a value initially', async ({ page }) => {
+		const input = await page.locator('kol-combobox').first().getByLabel('Label');
+		expect(await input.inputValue()).toBe('Deutschland');
+	});
+
+	test('should show all options when clicking the text input', async ({ page }) => {
+		const combobox = await page.locator('kol-combobox').first();
+		const input = combobox.getByLabel('Label');
+		await input.click();
+		expect(await combobox.getByRole('listbox')).toBeVisible();
+		expect(await combobox.getByRole('listbox').locator('li').count()).toBe(193);
+	});
+
+	test('should show all options when clicking the arrow icon', async ({ page }) => {
+		const combobox = await page.locator('kol-combobox').first();
+		await combobox.locator('.combobox__icon').click();
+		expect(await combobox.getByRole('listbox')).toBeVisible();
+		expect(await combobox.getByRole('listbox').locator('li').count()).toBe(193);
+	});
+})π;
