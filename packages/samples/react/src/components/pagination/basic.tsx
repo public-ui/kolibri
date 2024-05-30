3bb3cb6170d7b3510e6386030ec808d02de00f1c
Peter Laske <37439758+laske185@users.noreply.github.com>
2024-05-30 09:59:22 +0200

    feat: extend pagination samples with many pages per pagination
    Refs: #6482

--- packages/samples/react/src/components/pagination/basic.tsx
+++ packages/samples/react/src/components/pagination/basic.tsx
@@ -13,8 +13,8 @@ export const PaginationBasic: FC = () => (
 			</p>
 		</SampleDescription>
 		<div className="grid gap-4">
-			<KolPagination _max={10} _page={6} _siblingCount={0} _label="Test Paginierung 1" _on={{}} />
-			<KolPagination _max={6} _page={6} _siblingCount={0} _boundaryCount={2} _label="Test Paginierung 2" _on={{}} />
+			<KolPagination _max={316514} _page={15475} _siblingCount={3} _label="Test Paginierung 1" _on={{}} />
+			<KolPagination _max={14} _page={14} _siblingCount={1} _boundaryCount={2} _label="Test Paginierung 2" _on={{}} />
 			<KolPagination _max={4} _page={1} _siblingCount={0} _boundaryCount={2} _label="Test Paginierung 3" _on={{}} />
 			<KolPagination _max={4} _page={6} _siblingCount={0} _boundaryCount={2} _hasButtons={false} _label="Test Paginierung 4" _on={{}} />
 			<KolPagination
