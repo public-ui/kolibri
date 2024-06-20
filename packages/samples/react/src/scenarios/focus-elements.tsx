f98e2e8ffbca62b2c5af8909845dc12841dec0db
Stefan Dietz <mail@stefan-dietz.eu>
2024-06-20 11:09:30 +0200

    Handle no component or invalid component selected nicely
    Refs: #6346

--- packages/samples/react/src/scenarios/focus-elements.tsx
+++ packages/samples/react/src/scenarios/focus-elements.tsx
@@ -1,5 +1,6 @@
 import {
 	KolAccordion,
+	KolAlert,
 	KolButton,
 	KolButtonLink,
 	KolDetails,
@@ -19,73 +20,107 @@ import {
 	KolTextarea,
 } from '@public-ui/react';
 import type { FC, ForwardRefRenderFunction } from 'react';
+import { useMemo } from 'react';
 import React, { forwardRef, useLayoutEffect, useRef } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
-const focusElements = new Map<string, ForwardRefRenderFunction<HTMLElement, any>>();
-focusElements.set('inputCheckbox', (props, ref) => <KolInputCheckbox _name="checkbox" _label="Checkbox" ref={ref} />);
-focusElements.set('inputColor', (props, ref) => <KolInputColor _name="color" _label="Color" ref={ref} />);
-focusElements.set('inputDate', (props, ref) => <KolInputDate _name="date" _label="Date" ref={ref} />);
-focusElements.set('inputEmail', (props, ref) => <KolInputEmail _name="email" _label="Email" ref={ref} />);
-focusElements.set('inputFile', (props, ref) => <KolInputFile _name="file" _label="File" ref={ref} />);
-focusElements.set('inputFileMultiple', (props, ref) => <KolInputFile _name="file" _label="Files (multiple)" _multiple ref={ref} />);
-focusElements.set('inputNumber', (props, ref) => <KolInputNumber _name="number" _label="Number" ref={ref} />);
-focusElements.set('inputPassword', (props, ref) => <KolInputPassword _name="password" _label="Password" ref={ref} />);
-focusElements.set('inputRadio', (props, ref) => (
-	<KolInputRadio
-		_name="radio"
-		_label="Radio"
-		_options={[
-			{ label: 'Option A', value: 'A' },
-			{ label: 'Option B', value: 'B' },
-		]}
-		_value="A"
-		ref={ref}
-	/>
-));
-focusElements.set('inputRange', (props, ref) => <KolInputRange _name="range" _label="Range" ref={ref} />);
-focusElements.set('inputText', (props, ref) => <KolInputText _name="text" _label="Text" ref={ref} />);
-focusElements.set('select', (props, ref) => (
-	<KolSelect
-		_name="select"
-		_label="Select"
-		_options={[
-			{ label: 'Option A', value: 'A' },
-			{ label: 'Option B', value: 'B' },
-		]}
-		ref={ref}
-	/>
-));
-focusElements.set('selectMultiple', (props, ref) => (
-	<KolSelect
-		_name="select"
-		_label="Select (multiple)"
-		_multiple
-		_options={[
-			{ label: 'Option A', value: 'A' },
-			{ label: 'Option B', value: 'B' },
-		]}
-		_rows={2}
-		ref={ref}
-	/>
-));
-focusElements.set('textarea', (props, ref) => <KolTextarea _name="textarea" _label="Textarea" _rows={5} ref={ref} />);
+const getFocusElements = () => {
+	const focusElements = new Map<string, ForwardRefRenderFunction<HTMLElement, any>>();
+	focusElements.set('inputCheckbox', (_, ref) => <KolInputCheckbox _name="checkbox" _label="Checkbox" ref={ref} />);
+	focusElements.set('inputColor', (_, ref) => <KolInputColor _name="color" _label="Color" ref={ref} />);
+	focusElements.set('inputDate', (_, ref) => <KolInputDate _name="date" _label="Date" ref={ref} />);
+	focusElements.set('inputEmail', (_, ref) => <KolInputEmail _name="email" _label="Email" ref={ref} />);
+	focusElements.set('inputFile', (_, ref) => <KolInputFile _name="file" _label="File" ref={ref} />);
+	focusElements.set('inputFileMultiple', (_, ref) => <KolInputFile _name="file" _label="Files (multiple)" _multiple ref={ref} />);
+	focusElements.set('inputNumber', (_, ref) => <KolInputNumber _name="number" _label="Number" ref={ref} />);
+	focusElements.set('inputPassword', (_, ref) => <KolInputPassword _name="password" _label="Password" ref={ref} />);
+	focusElements.set('inputRadio', (_, ref) => (
+		<KolInputRadio
+			_name="radio"
+			_label="Radio"
+			_options={[
+				{ label: 'Option A', value: 'A' },
+				{ label: 'Option B', value: 'B' },
+			]}
+			_value="A"
+			ref={ref}
+		/>
+	));
+	focusElements.set('inputRange', (_, ref) => <KolInputRange _name="range" _label="Range" ref={ref} />);
+	focusElements.set('inputText', (_, ref) => <KolInputText _name="text" _label="Text" ref={ref} />);
+	focusElements.set('select', (_, ref) => (
+		<KolSelect
+			_name="select"
+			_label="Select"
+			_options={[
+				{ label: 'Option A', value: 'A' },
+				{ label: 'Option B', value: 'B' },
+			]}
+			ref={ref}
+		/>
+	));
+	focusElements.set('selectMultiple', (_, ref) => (
+		<KolSelect
+			_name="select"
+			_label="Select (multiple)"
+			_multiple
+			_options={[
+				{ label: 'Option A', value: 'A' },
+				{ label: 'Option B', value: 'B' },
+			]}
+			_rows={2}
+			ref={ref}
+		/>
+	));
+	focusElements.set('textarea', (_, ref) => <KolTextarea _name="textarea" _label="Textarea" _rows={5} ref={ref} />);
+	focusElements.set('accordion', (_, ref) => <KolAccordion _label="Accordion here" ref={ref} />);
+	focusElements.set('button', (_, ref) => <KolButton _label="Button here" ref={ref}></KolButton>);
+	focusElements.set('buttonLink', (_, ref) => <KolButtonLink _label="ButtonLink here" ref={ref}></KolButtonLink>);
+	focusElements.set('details', (_, ref) => (
+		<KolDetails _label="Details here" ref={ref}>
+			detailed details
+		</KolDetails>
+	));
+	focusElements.set('link', (_, ref) => <KolLink _label="Link here" _href="#" ref={ref}></KolLink>);
+	focusElements.set('linkButton', (_, ref) => <KolLinkButton _label="LinkButton here" _href="#" ref={ref}></KolLinkButton>);
 
-focusElements.set('accordion', (props, ref) => <KolAccordion _label="Accordion here" ref={ref} />);
-focusElements.set('button', (props, ref) => <KolButton _label="Button here" ref={ref}></KolButton>);
-focusElements.set('buttonLink', (props, ref) => <KolButtonLink _label="ButtonLink here" ref={ref}></KolButtonLink>);
-focusElements.set('details', (props, ref) => (
-	<KolDetails _label="Details here" ref={ref}>
-		detailed details
-	</KolDetails>
-));
-focusElements.set('link', (props, ref) => <KolLink _label="Link here" _href="#" ref={ref}></KolLink>);
-focusElements.set('linkButton', (props, ref) => <KolLinkButton _label="LinkButton here" _href="#" ref={ref}></KolLinkButton>);
+	return focusElements;
+};
+
+type FallbackProps = {
+	invalidComponent?: boolean;
+};
+const Fallback = (props: FallbackProps) => {
+	const focusElements = useMemo(() => getFocusElements(), []);
+	const componentNames = focusElements
+		.keys()
+		.toArray()
+		.map((key) => key);
+
+	return (
+		<div>
+			{props.invalidComponent && (
+				<KolAlert _type="error" _variant="card">
+					Component not found.
+				</KolAlert>
+			)}
+
+			<h4>Focus Test Cases</h4>
+			<ul>
+				{componentNames.map((componentName) => (
+					<li key={componentName}>
+						<KolLink _label={componentName} _href={`#/scenarios/focus-elements?component=${componentName}`}></KolLink>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 
 export const FocusElements: FC = () => {
 	const ref = useRef(null);
-
-	const [searchParams, setSearchParams] = useSearchParams();
+	const focusElements = useMemo(() => getFocusElements(), []);
+	const [searchParams] = useSearchParams();
 	const componentName = searchParams.get('component');
 
 	useLayoutEffect(() => {
@@ -97,12 +132,12 @@ export const FocusElements: FC = () => {
 	if (componentName) {
 		const Component = focusElements.get(componentName);
 		if (!Component) {
-			throw new Error('Component not found:', componentName);
+			return <Fallback invalidComponent />;
 		}
 		const Element = forwardRef(Component);
 
 		return <Element ref={ref} />;
 	} else {
-		return <>Select component</>;
+		return <Fallback />;
 	}
 };
