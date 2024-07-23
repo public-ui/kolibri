5bd4a8582e17e1f2ca0cd416e1fda787acbc037c
Stefan Dietz <mail@stefan-dietz.eu>
2024-07-23 20:16:22 +0200

    Add English description and DRAFT badge
    Refs: #3694

--- packages/samples/react/src/components/single-select/basic.tsx
+++ packages/samples/react/src/components/single-select/basic.tsx
@@ -1,18 +1,22 @@
-import type { FC } from 'react';
 import React from 'react';
+import type { FC } from 'react';
 import { FormWrap } from '../FormWrap';
-import { SingleSelectVariants } from './partials/variants';
+import { HideMenusContext } from '../../shares/HideMenusContext';
 import { SampleDescription } from '../SampleDescription';
-export const SingleSelectBasic: FC = () => (
-	<>
-		<SampleDescription>
-			<p>
-				Die SingleSelect-Komponente erlaubt Benutzern, eine einzelne Option aus einer Dropdown-Liste auszuwählen und unterstützt eine Suchfunktion für eine
-				einfache Navigation.
-			</p>
-		</SampleDescription>
+import { SingleSelectVariants } from './partials/variants';
+import { useContext } from 'react';
+import { KolBadge } from '@public-ui/react';
+
+export const SingleSelectBasic: FC = () => {
+	const hideMenus = useContext(HideMenusContext);
+
+	return (
 		<>
+			<SampleDescription>
+				<p>SingleSelect-Komponente provides a select field for a single value, supported by a search field.</p>
+			</SampleDescription>
+			{!hideMenus && <KolBadge className="block mb-3" _label="Component is a DRAFT - Don't use in production yet." _color="#db5461" />}
 			<FormWrap RefComponent={SingleSelectVariants} />
 		</>
-	</>
-);
+	);
+};
