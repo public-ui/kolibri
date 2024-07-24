799d3f53931c3dc47cbbca0d43f488c148a63b97
Stefan Dietz <mail@stefan-dietz.eu>
2024-07-24 20:25:11 +0200

    (#6171) Remove extra semicolon

--- packages/samples/react/src/components/button/basic.tsx
+++ packages/samples/react/src/components/button/basic.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import type { FC } from 'react';
+import React from 'react';
 import { FocusElement } from '../FocusInput';
 import { ButtonVariants } from './partials/variants';
 import { SampleDescription } from '../SampleDescription';
@@ -10,6 +11,6 @@ export const ButtonBasic: FC = () => (
 				KolButton shows a button-element. This sample demonstrates the basic usage with its different styling variants, icons, disabled state and hidden labels.
 			</p>
 		</SampleDescription>
-		<FocusElement RefComponent={ButtonVariants} />;
+		<FocusElement RefComponent={ButtonVariants} />
 	</>
 );
