51aee2276e220a7252f7aa96c7e3406fdd7d0823
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2024-07-27 08:00:38 +0200

    fix: react sample bootstrap
    Refs: #6609

--- packages/samples/react/src/react.main.tsx
+++ packages/samples/react/src/react.main.tsx
@@ -53,7 +53,7 @@ void (async () => {
 					name: 'en',
 				},
 				transformTagName: ENABLE_TAG_NAME_TRANSFORMER ? tagNameTransformer : undefined,
-				environment: process.env.NODE_ENV === 'production' ? 'PROD' : 'DEV',
+				environment: process.env.NODE_ENV === 'development' ? 'development' : 'production',
 			},
 		);
 	} catch (error) {
