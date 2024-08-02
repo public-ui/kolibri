9c70bc33252592e2a726236e68cd15f7073dd5ff
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2024-08-02 06:18:23 +0200

    (#6652) refact: the hash code

--- packages/samples/react/src/components/SampleDescription.tsx
+++ packages/samples/react/src/components/SampleDescription.tsx
@@ -5,18 +5,25 @@ import { KolIndentedText, KolLink } from '@public-ui/react';
 
 import { HideMenusContext } from '../shares/HideMenusContext';
 
+const getLocationPaths = () => {
+	return location.hash.split('/').slice(1);
+};
+
 export const SampleDescription: FC<PropsWithChildren> = (props) => {
 	const hideMenus = useContext(HideMenusContext);
+
 	const docLink = useMemo(() => {
-		const arr = location.href.split('/');
-		const componentName = arr[arr.length - 2];
-		return componentName === 'scenarios'
+		const paths = getLocationPaths();
+		return paths[0] === 'scenarios'
 			? null // Scenarios are not a component and hence have no documentation.
-			: `${PUBLIC_DOC_COMPONENT_URL}/${componentName}`;
-	}, [location.href]);
+			: `${PUBLIC_DOC_COMPONENT_URL}/${paths[0]}`;
+	}, [location.hash]);
+
 	const codeLink = useMemo(() => {
-		const link = location.hash.slice(1);
-		return `${PUBLIC_CODE_COMPONENT_URL}${link}.tsx`;
+		const paths = getLocationPaths();
+		return paths[0] === 'scenarios'
+			? null // Scenarios are not a component and hence have no documentation.
+			: `${PUBLIC_CODE_COMPONENT_URL}/${paths[0]}/${paths[1]}.tsx`;
 	}, [location.hash]);
 
 	return hideMenus ? null : (
