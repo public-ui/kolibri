9db53d70c1c1b4e39a7e56b141927eea9b74f618
Cyrine Ben Romdhane <135605630+anicyne@users.noreply.github.com>
2024-08-05 10:28:21 +0200

    Translate input-radio labels and options to English.
    Refs: #6455

--- packages/samples/react/src/shares/constants.ts
+++ packages/samples/react/src/shares/constants.ts
@@ -1,3 +1,3 @@
 export const PUBLIC_DOC_COMPONENT_URL = 'https://public-ui.github.io/docs/components';
-export const ERROR_MSG = 'Ich bin eine Fehlermeldung!';
-export const HINT_MSG = 'Ich bin ein Hinweis.';
+export const ERROR_MSG = 'I am an error message!';
+export const HINT_MSG = 'I am a hint.';
