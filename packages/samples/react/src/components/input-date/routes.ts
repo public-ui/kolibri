06118703759a4d84f31ad30eef9e2e8933b6ddf5
Stefan Dietz <mail@stefan-dietz.eu>
2024-08-07 14:22:16 +0200

    InputDate: Add reset method
    Refs: #6684

--- packages/samples/react/src/components/input-date/routes.ts
+++ packages/samples/react/src/components/input-date/routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '../../shares/types';
 import { InputDateBasic } from './basic';
+import { InputDateReset } from './reset';
 
 export const INPUT_DATE_ROUTES: Routes = {
 	'input-date': {
 		basic: InputDateBasic,
+		reset: InputDateReset,
 	},
 };
