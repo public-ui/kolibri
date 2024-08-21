9cb7ba72411201e490148fa3db96bf6576e3b843
Makko <marco.leithold@iso-gruppe.com>
2024-08-21 08:13:11 +0200

    Migrations-Tool: KolTable zu KolTableStateful umschreiben
    Refs: #6325

---
+++ packages/tools/kolibri-cli/src/migrate/runner/tasks/v2/table.ts
@@ -0,0 +1,3 @@
+import { RenameTagNameTask } from '../common/RenameTagNameTask';
+
+export const RenameTagNameKolTableToKolTableStateful = RenameTagNameTask.getInstance('kol-table', 'kol-table-stateful', '^2');
