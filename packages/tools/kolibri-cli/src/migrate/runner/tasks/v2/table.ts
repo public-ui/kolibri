efc3e80ecf024978831be92598b088a6e9fba6c9
Makko <marco.leithold@iso-gruppe.com>
2024-08-21 08:48:47 +0200

    range version
    Refs: #6325

--- packages/tools/kolibri-cli/src/migrate/runner/tasks/v2/table.ts
+++ packages/tools/kolibri-cli/src/migrate/runner/tasks/v2/table.ts
@@ -1,3 +1,3 @@
 import { RenameTagNameTask } from '../common/RenameTagNameTask';
 
-export const RenameTagNameKolTableToKolTableStateful = RenameTagNameTask.getInstance('kol-table', 'kol-table-stateful', '^2');
+export const RenameTagNameKolTableToKolTableStateful = RenameTagNameTask.getInstance('kol-table', 'kol-table-stateful', '>=2.1.7 <3');
