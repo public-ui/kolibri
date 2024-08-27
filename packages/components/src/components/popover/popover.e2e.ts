7213f37d8adab2dfc112acd944016a7ea97151bc
Stefan Dietz <mail@stefan-dietz.eu>
2024-08-27 09:45:23 +0200

    Refactory e2e tests
    Refs: #6725

--- packages/components/src/components/popover/popover.e2e.ts
+++ packages/components/src/components/popover/popover.e2e.ts
@@ -3,24 +3,24 @@ import { test } from '@stencil/playwright';
 
 test.describe('kol-popover', () => {
 	test('should display popover when _show is true and hide when _show is false', async ({ page }) => {
-		await page.setContent(` <button id="trigger">Trigger Button</button> <kol-popover-wc _align="top" >Dropdown-Inhalt</kol-popover-wc> `);
-		const popover = page.locator('kol-popover-wc');
-		await expect(popover.locator('.popover')).not.toBeVisible();
-		await popover.evaluate(() => {
+		await page.setContent(`<kol-popover-wc _align="top">Dropdown-Inhalt</kol-popover-wc>`);
+		const popoverComponent = page.locator('kol-popover-wc');
+		const popoverElement = popoverComponent.locator('.popover');
+
+		await expect(popoverElement).not.toBeVisible();
+		await popoverComponent.evaluate(() => {
 			const popover = document.querySelector('kol-popover-wc');
 
 			if (popover) {
 				popover._show = true;
 			}
 		});
+		await expect(popoverElement).toBeVisible();
 
-		await expect(popover.locator('.popover')).toBeVisible();
-		const alignAttr = await popover.evaluate((el) => el.getAttribute('_align'));
-		expect(alignAttr).toBe('top');
 		await page.evaluate(() => {
 			const popover = document.querySelector('kol-popover-wc');
 			if (popover) popover._show = false;
 		});
-		await expect(popover.locator('.popover')).not.toBeVisible();
+		await expect(popoverElement).not.toBeVisible();
 	});
 });
--- packages/components/src/components/split-button/split-button.e2e.ts
+++ packages/components/src/components/split-button/split-button.e2e.ts
@@ -2,24 +2,31 @@ import { expect } from '@playwright/test';
 import { test } from '@stencil/playwright';
 
 test.describe('kol-split-button', () => {
-	test('should display popover when the split button is clicked', async ({ page }) => {
-		await page.setContent(` <kol-split-button _label="Sample Button">Dropdown-Inhalt</kol-split-button> `);
+	test('should display toggle popover when the primary button is clicked', async ({ page }) => {
+		await page.setContent(` <kol-split-button _label="Sample Button">Dropdown contents</kol-split-button> `);
 		const splitButton = page.locator('kol-split-button ');
 
 		const mainButton = splitButton.locator('.main-button');
+		const popover = splitButton.locator('kol-popover-wc .popover');
+
+		await expect(popover).not.toBeVisible();
+		await mainButton.click();
+		await expect(popover).toBeVisible();
+		await mainButton.click();
+		await expect(popover).not.toBeVisible();
+	});
+
+	test('should display toggle popover when the secondary button is clicked', async ({ page }) => {
+		await page.setContent(` <kol-split-button _label="Sample Button">Dropdown contents</kol-split-button> `);
+		const splitButton = page.locator('kol-split-button ');
+
 		const secondaryButton = splitButton.locator('.secondary-button');
-		const popover = splitButton.locator('kol-popover-wc');
+		const popover = splitButton.locator('kol-popover-wc .popover');
 
-		await expect(popover.locator('.popover')).not.toBeVisible();
-		await secondaryButton.click({ force: true });
-		await expect(popover.locator('.popover')).toBeVisible();
-		await secondaryButton.click({ force: true });
-		await expect(popover.locator('.popover')).not.toBeVisible();
-
-		await expect(popover.locator('.popover')).not.toBeVisible();
-		await mainButton.click({ force: true });
-		await expect(popover.locator('.popover')).toBeVisible();
-		await mainButton.click({ force: true });
-		await expect(popover.locator('.popover')).not.toBeVisible();
+		await expect(popover).not.toBeVisible();
+		await secondaryButton.click();
+		await expect(popover).toBeVisible();
+		await secondaryButton.click();
+		await expect(popover).not.toBeVisible();
 	});
 });
