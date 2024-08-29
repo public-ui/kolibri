da7d567f60424d46dca146f6da2c51d532c2519a
Stefan Dietz <mail@stefan-dietz.eu>
2024-08-29 09:37:35 +0200

    Fix data-label-align attribute in checkbox tests
    Refs: #6083

--- packages/components/src/components/input-checkbox/test/html.mock.ts
+++ packages/components/src/components/input-checkbox/test/html.mock.ts
@@ -34,7 +34,7 @@ export const getInputCheckboxHtml = (props: InputCheckboxProps): string => {
 					${state._hideLabel ? `_hideLabel=""` : ''}
 					${state._touched ? `_touched=""` : ''}
 					${state._required ? `_required=""` : ''}
-					${state._labelAlign ? `_labelAlign="${state._labelAlign}"` : ''}
+					data-label-align="${state._labelAlign || 'right'}"
 					_hint=""
 					_id="${state._id}"
 					_label="${state._label ? `${state._label}` : ''}"
