c5fb9d836d106667b2d89d5dcebed78b8ea226a0
Makko <marco.leithold@iso-gruppe.com>
2024-09-18 17:19:46 +0200

    all rem values converted in unocss
    Refs: #6674

--- packages/samples/react/unocss.config.ts
+++ packages/samples/react/unocss.config.ts
@@ -156,6 +156,64 @@ export default defineConfig({
 				'9xl': [ convertRem(8), '1' ],
 				'kol-test': ['625%', '1']
 			  },
+			  borderRadius: {
+				DEFAULT: convertRem(0.25),
+				none: '0',
+				sm: convertRem(0.125),
+				md: convertRem(0.375),
+				lg: convertRem(0.5),
+				xl: convertRem(0.75),
+				'2xl': convertRem(1),
+				'3xl': convertRem(1.5),
+				full: '9999px'
+			  },
+			  containers: {
+				xs: `(min-width: ${convertRem(20)})`,
+				sm: `(min-width: ${convertRem(24)})`,
+				md: `(min-width: ${convertRem(28)})`,
+				lg: `(min-width: ${convertRem(32)})`,
+				xl: `(min-width: ${convertRem(36)})`,
+				'2xl': `(min-width: ${convertRem(42)})`,
+				'3xl': `(min-width: ${convertRem(48)})`,
+				'4xl': `(min-width: ${convertRem(56)})`,
+				'5xl': `(min-width: ${convertRem(64)})`,
+				'6xl': `(min-width: ${convertRem(72)})`,
+				'7xl': `(min-width: ${convertRem(80)})`,
+				prose: '(min-width: 65ch)'
+			  },
+			  spacing: {
+				DEFAULT: convertRem(1),
+				none: '0',
+				xs: convertRem(0.75),
+				sm: convertRem(0.875),
+				lg: convertRem(1.125),
+				xl: convertRem(1.25),
+				'2xl': convertRem(1.5),
+				'3xl': convertRem(1.875),
+				'4xl': convertRem(2.25),
+				'5xl': convertRem(3),
+				'6xl': convertRem(3.75),
+				'7xl': convertRem(4.5),
+				'8xl': convertRem(6),
+				'9xl': convertRem(8)
+			  },
+			  textIndent: {
+				DEFAULT: convertRem(1.5),
+				xs: convertRem(0.5),
+				sm: convertRem(1),
+				md: convertRem(1.5),
+				lg: convertRem(2),
+				xl: convertRem(2.5),
+				'2xl': convertRem(3),
+				'3xl': convertRem(4)
+			  },
+			  textStrokeWidth: {
+				DEFAULT: convertRem(1.5),
+				none: '0',
+				sm: 'thin',
+				md: 'medium',
+				lg: 'thick'
+			  },
 		}
 	},
 	presets: [presetUno()],
