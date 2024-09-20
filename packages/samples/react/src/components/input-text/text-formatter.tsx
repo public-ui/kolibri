6c9055ce1290a53b32f3b33e1e77b2f04f5a87ea
Makko <marco.leithold@iso-gruppe.com>
2024-09-20 12:08:40 +0200

    Sample App: Currency Format Example with react-number-format
    Refs: #6850

--- packages/samples/react/src/components/input-text/text-formatter.tsx
+++ packages/samples/react/src/components/input-text/text-formatter.tsx
@@ -3,6 +3,8 @@ import { Formik, Field, type FieldProps } from 'formik';
 import { KolForm, KolHeading, KolInputText } from '@public-ui/react';
 import { SampleDescription } from '../SampleDescription';
 
+import { NumericFormat, type NumberFormatValues } from 'react-number-format';
+
 const NON_ALPHANUM = /[^a-zA-Z0-9]/g;
 const EVERY_FOUR_CHARS = /(.{4})(?!$)/g;
 
@@ -23,15 +25,24 @@ class IbanFormatter {
 	}
 }
 
-type FormValues = {
+type IbanExampleFormValues = {
 	iban: string;
+};
+
+type CurrencyExampleFormValues = {
+	currency: number;
 };
 
 export function InputTextFormatterDemo() {
 	const handleSubmit = async () => {};
 	const formatter = new IbanFormatter();
-	const initialValues: FormValues = {
+
+	const initialIbanExampleValues: IbanExampleFormValues = {
 		iban: 'DE89370400440532013000',
+	};
+
+	const initialCurrencyExampleValues: CurrencyExampleFormValues = {
+		currency: 1000000,
 	};
 
 	return (
@@ -43,14 +54,14 @@ export function InputTextFormatterDemo() {
 				</p>
 			</SampleDescription>
 			<section className="w-full flex flex-col">
-				<Formik<FormValues> initialValues={initialValues} onSubmit={handleSubmit}>
+				<Formik<IbanExampleFormValues> initialValues={initialIbanExampleValues} onSubmit={handleSubmit}>
 					{(form) => (
 						<>
 							<div className="p-2">
 								<KolHeading _label="Formatted Form Field" _level={2} />
 								<KolForm>
 									<Field name="iban">
-										{({ field }: FieldProps<FormValues['iban']>) => (
+										{({ field }: FieldProps<IbanExampleFormValues['iban']>) => (
 											<div className="block mt-2">
 												<KolInputText
 													onBlur={() => {
@@ -74,6 +85,46 @@ export function InputTextFormatterDemo() {
 															}
 														},
 													}}
+												/>
+											</div>
+										)}
+									</Field>
+								</KolForm>
+							</div>
+							<div className="p-2">
+								<KolHeading _label="Model" _level={2} />
+								<pre className="text-base">{JSON.stringify(form.values, null, 2)}</pre>
+							</div>
+						</>
+					)}
+				</Formik>
+			</section>
+
+			<section className="w-full flex flex-col">
+				<Formik<CurrencyExampleFormValues> initialValues={initialCurrencyExampleValues} onSubmit={handleSubmit}>
+					{(form) => (
+						<>
+							<div className="p-2">
+								<KolHeading _label="Formatted Form Field (with react-number-format)" _level={2} />
+								<KolForm>
+									<Field name="currency">
+										{({ field }: FieldProps<CurrencyExampleFormValues['currency']>) => (
+											<div className="block mt-2">
+												<NumericFormat
+													customInput={({ value, onBlur, onChange, onFocus, ...other }: any) => {
+														console.log('OTHER: ', other);
+														return <KolInputText _label="Currency" _value={value} _on={{ onBlur, onChange, onFocus }} />;
+													}}
+													displayType="input"
+													value={typeof field.value === 'number' ? Number(field.value).toFixed(2) : undefined}
+													onBlur={() => {
+														void form.setFieldTouched('currency', true);
+													}}
+													onValueChange={(value: NumberFormatValues) => {
+														void form.setFieldValue('currency', value.floatValue, true);
+													}}
+													suffix={'€'}
+													thousandSeparator={true}
 												/>
 											</div>
 										)}
