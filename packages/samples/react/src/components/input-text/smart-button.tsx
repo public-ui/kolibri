22de1d154b00a0591b82a1428bfc19b6b8553f9b
alex_ <alexander.schmidt@codeagle.de>
2024-09-20 13:38:46 +0200

    changed alert to Toast
    Refs: #6783

--- packages/samples/react/src/components/input-text/smart-button.tsx
+++ packages/samples/react/src/components/input-text/smart-button.tsx
@@ -2,14 +2,24 @@ import * as React from 'react';
 import { SampleDescription } from '../SampleDescription';
 import type { FC } from 'react';
 import type { ButtonProps } from '@public-ui/components/src';
+import { ToasterService } from '@public-ui/components';
 import { KolInputText } from '@public-ui/react';
 
 export const InputTextSmartButton: FC = () => {
+	const toaster = ToasterService.getInstance(document);
+
 	const smartButton: ButtonProps = {
 		_icons: 'codicon codicon-info',
 		_hideLabel: true,
 		_label: 'Alert',
-		_on: { onClick: (_event, _value) => alert('Smart-Button gedrückt!') },
+		_on: {
+			onClick: (_event, _value) => {
+				void toaster.enqueue({
+					description: 'Smart-Button Clicked',
+					label: `Toaster`,
+				});
+			},
+		},
 	};
 
 	return (
