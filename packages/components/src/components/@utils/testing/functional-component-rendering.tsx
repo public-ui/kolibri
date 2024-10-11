bdde5628905b5c685bc2b6abc44e612e61c98393
Makko <marco.leithold@iso-gruppe.com>
2024-10-11 07:04:41 +0200

    fix
    Refs: #6909

--- packages/components/src/components/@utils/testing/functional-component-rendering.tsx
+++ packages/components/src/components/@utils/testing/functional-component-rendering.tsx
@@ -1,11 +1,9 @@
 import type { FunctionalComponent } from '@stencil/core';
-import { Component } from '@stencil/core';
+
 import { newSpecPage, type SpecPage } from '@stencil/core/testing';
+import { TestComponent } from './test-component';
 
- function renderFunctionalComponentToSpecPage<T extends object>(componentConstructor: () => FunctionalComponent<T>): Promise<SpecPage> {
-	@Component({ tag: 'test-component' })
-	class TestComponent {}
-
+function renderFunctionalComponentToSpecPage<T extends object>(componentConstructor: () => FunctionalComponent<T>): Promise<SpecPage> {
 	return newSpecPage({
 		components: [TestComponent],
 		template: componentConstructor,
