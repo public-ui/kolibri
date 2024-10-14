d53aab7a6a26d5128ae9e1384697b1acd31983c8
Makko <marco.leithold@iso-gruppe.com>
2024-10-14 09:02:12 +0200

    revert
    Refs: #6909

--- packages/components/src/components/@shared/alert-fc/components/heading-content.component.tsx
+++ packages/components/src/components/@shared/alert-fc/components/heading-content.component.tsx
@@ -2,13 +2,13 @@ import { h } from '@stencil/core';
 import type { FunctionalComponent as FC } from '@stencil/core';
 import { DEFAULT_PROPS, type KolAlertFcProps } from '../_types';
 import Content from './content.component';
-import KolHeadingFc from '../../heading-fc';
+import { KolHeadingWcTag } from '../../../../core/component-names';
 
 const HeadingContent: FC<KolAlertFcProps> = ({ _label, _level = DEFAULT_PROPS._level, _variant = DEFAULT_PROPS._variant }, children) => {
 	const content: unknown[] = [];
 
 	if (_label) {
-		content.push(<KolHeadingFc _label={_label} _level={_level} />);
+		content.push(<KolHeadingWcTag _label={_label} _level={_level} />);
 	}
 
 	if (_variant === 'msg') {
--- packages/components/src/components/@shared/index.ts
+++ packages/components/src/components/@shared/index.ts
@@ -1,2 +1,1 @@
 export { default as KolAlertFc } from './alert-fc';
-export { default as KolHeadingFc } from './heading-fc';
