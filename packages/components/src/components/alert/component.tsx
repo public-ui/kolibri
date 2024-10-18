338f487ce17572b23aa32d021941cb5010731b24
alex_ <alexander.schmidt@codeagle.de>
2024-10-18 10:17:53 +0200

    added build label for Icon
    Refs: #6631

--- packages/components/src/components/alert/component.tsx
+++ packages/components/src/components/alert/component.tsx
@@ -8,13 +8,7 @@ import { KolIconTag, KolHeadingWcTag, KolButtonWcTag } from '../../core/componen
 
 import type { AlertAPI, AlertStates, AlertType, AlertVariant, HasCloserPropType, HeadingLevel, KoliBriAlertEventCallbacks, LabelPropType } from '../../schema';
 const Icon = (props: { ariaLabel: string; icon: string; label?: string }) => {
-	return (
-		<KolIconTag
-			class="heading-icon"
-			_label={typeof props.label === 'string' && props.label.length > 0 ? props.ariaLabel + ': ' + props.label : props.ariaLabel}
-			_icons={props.icon}
-		/>
-	);
+	return <KolIconTag class="heading-icon" _label={buildLabel(props.ariaLabel, props.label)} _icons={props.icon} />;
 };
 
 const AlertIcon = (props: { label?: string; type?: AlertType }) => {
@@ -32,6 +26,9 @@ const AlertIcon = (props: { label?: string; type?: AlertType }) => {
 	}
 };
 
+const buildLabel = (ariaLabel: string, label: string | undefined) => {
+	return [ariaLabel, label].filter((f) => f).join(': ');
+};
 /**
  * @internal
  * @slot - Der Inhalt der Meldung.
