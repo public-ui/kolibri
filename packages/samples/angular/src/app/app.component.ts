f133eaf1a7951520dcd753616e30f02e21033e2a
Stefan Dietz <mail@stefan-dietz.eu>
2024-10-23 14:40:28 +0200

    Add basic Angular example (w/o ngModel)
    Refs: #6343

--- packages/samples/angular/src/app/app.component.ts
+++ packages/samples/angular/src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { KoliBriModule } from '@public-ui/angular-v18';
+import { KoliBriModule, KolInputDate } from '@public-ui/angular-v18';
+import { FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
-	imports: [RouterOutlet, KoliBriModule],
+	imports: [RouterOutlet, KoliBriModule, FormsModule],
 })
 export class AppComponent {
-	title = 'angular';
+	dateModel = new Date();
+
+	public handleInput(evt: Event) {
+		const value = (evt.target as HTMLKolInputDateElement).value;
+		console.log('input', evt, value);
+		if (value instanceof Date) {
+			this.dateModel = value;
+		}
+	}
+
+	public handleChange(evt: Event) {
+		console.log('change', evt, (evt.target as HTMLKolInputDateElement).value);
+	}
 }
