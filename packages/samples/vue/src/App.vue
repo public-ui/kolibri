b712555dac9b9a11ff5aed3e467dbba1601a186e
Stefan Dietz <mail@stefan-dietz.eu>
2024-10-24 16:47:47 +0200

    Add basic Vue scenario
    Refs: #6343

--- packages/samples/vue/src/App.vue
+++ packages/samples/vue/src/App.vue
@@ -1,7 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { KolButton, KolInputDate } from '@public-ui/vue';
+
+const date = defineModel('date');
+date.value = new Date();
+
+const handleDateInput = (event) => {
+	date.value = event.target._value;
+};
+
+const increaseDate = () => {
+	date.value = new Date(date.value.getTime() + 1000 * 60 * 60 * 24);
+};
+</script>
 
 <template>
-	<p>KoliBri Vue Sample</p>
+	<main>
+		<KolInputDate _label="Date Input" :_value="date" @input="handleDateInput" />
+		<KolButton _label="increase" :_on="{ onClick: increaseDate }" />
+		<pre>
+value: {{ JSON.stringify(date) }} ({{ typeof date }})
+			</pre
+		>
+	</main>
 </template>
 
 <style>
--- packages/samples/vue/src/main.ts
+++ packages/samples/vue/src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from 'vue';
+import { DEFAULT } from '@public-ui/themes';
+import { defineCustomElements } from '@public-ui/components/dist/loader';
+import { register } from '@public-ui/components';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+register(DEFAULT, defineCustomElements)
+	.then(() => {
+		createApp(App).mount('#app');
+	})
+	.catch((error) => {
+		/* Handle errors */
+	});
