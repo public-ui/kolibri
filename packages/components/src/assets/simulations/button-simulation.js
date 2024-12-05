6fc40e41c3fce8112464d04c88db8754babb047c
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2024-12-05 20:34:18 +0100

    feat: add new codicons and update existing codicon mappings

--- packages/components/src/assets/simulations/button-simulation.js
+++
@@ -1,8 +0,0 @@
-var buttons = document.querySelectorAll('kol-button.not-used');
-buttons.forEach((button) => {
-	button._on = {
-		onClick: () => {
-			alert(`Button "${button._label}" wurde geklickt.`);
-		},
-	};
-});
