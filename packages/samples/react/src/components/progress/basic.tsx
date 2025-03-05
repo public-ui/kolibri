0af8311325104590890622d4e107966d4ef1777e
Peter Laske <37439758+laske185@users.noreply.github.com>
2025-03-05 09:24:09 +0100

    fix: value length when unit is percentage
    Refs: #7397
    
    # Conflicts:
    #	packages/components/src/components/progress/shadow.tsx

--- packages/samples/react/src/components/progress/basic.tsx
+++ packages/samples/react/src/components/progress/basic.tsx
@@ -13,15 +13,15 @@ export const ProgressBasic: FC = () => (
 
 		<div className="grid gap-4 grid-cols-1 sm:grid-cols-2">
 			<fieldset title="Percentages" className="flex flex-col gap-4">
-				<KolProgress _variant="bar" _max={100} _value={0}></KolProgress>
-				<KolProgress _variant="bar" _max={100} _value={17}></KolProgress>
-				<KolProgress _variant="bar" _max={100} _value={100}></KolProgress>
-				<KolProgress _variant="cycle" _max={100} _value={85}></KolProgress>
+				<KolProgress _variant="bar" _max={7} _value={0}></KolProgress>
+				<KolProgress _variant="bar" _max={7} _value={2}></KolProgress>
+				<KolProgress _variant="bar" _max={7} _value={7}></KolProgress>
+				<KolProgress _variant="cycle" _max={7} _value={6}></KolProgress>
 			</fieldset>
 			<fieldset title="Custom units" className="flex flex-col gap-4">
 				<KolProgress _label="Distance" _variant="bar" _max={65434} _value={7236} _unit="m"></KolProgress>
 				<KolProgress _label="12 Tasks to do" _variant="bar" _max={12} _value={5} _unit="tasks completed"></KolProgress>
-				<KolProgress _label="Max 150 kg" _variant="cycle" _max={150} _value={134} _unit="kg"></KolProgress>
+				<KolProgress _label="Max 150 kg" _variant="cycle" _max={150} _value={42} _unit="kg"></KolProgress>
 			</fieldset>
 		</div>
 	</>
