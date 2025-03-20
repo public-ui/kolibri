ac335cca69ddb85efde9107ce64ee3896531db5b
Stefan Dietz <mail@stefan-dietz.eu>
2025-03-20 10:42:17 +0100

    Fix typo
    Refs: #7450

--- packages/themes/default/src/index.ts
+++ packages/themes/default/src/index.ts
@@ -9,7 +9,7 @@ import buttonCss from './components/button.scss';
 import buttonGroupCss from './components/button-group.scss';
 import buttonLinkCss from './components/button-link.scss';
 import cardCss from './components/card.scss';
-import ComboboxCss from './components/combobox.scss';
+import comboboxCss from './components/combobox.scss';
 import detailsCss from './components/details.scss';
 import drawerCss from './components/drawer.scss';
 import formCss from './components/form.scss';
@@ -58,7 +58,7 @@ export const DEFAULT = KoliBri.createTheme('default', {
 	'KOL-BUTTON-GROUP': buttonGroupCss,
 	'KOL-BUTTON-LINK': buttonLinkCss,
 	'KOL-CARD': cardCss,
-	'KOL-COMBOBOX': ComboboxCss,
+	'KOL-COMBOBOX': comboboxCss,
 	'KOL-DETAILS': detailsCss,
 	'KOL-DRAWER': drawerCss,
 	'KOL-FORM': formCss,
