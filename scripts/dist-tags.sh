acffe6d39e3be622e821129cfefe80fac54e2e5f
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-04-01 13:43:56 +0200

    feat: add deprecation and dist-tag support for @public-ui/theme-bwst in scripts
    Refs: #7540

--- scripts/dist-tags.sh
+++ scripts/dist-tags.sh
@@ -12,5 +12,6 @@ npm dist-tag $1 @public-ui/vue@$2 $3
 npm dist-tag $1 @public-ui/components@$2 $3
 npm dist-tag $1 @public-ui/sample-react@$2 $3
 npm dist-tag $1 @public-ui/themes@$2 $3
+npm dist-tag $1 @public-ui/theme-bwst@$2 $3
 npm dist-tag $1 @public-ui/theme-default@$2 $3
 npm dist-tag $1 @public-ui/visual-tests@$2 $3
