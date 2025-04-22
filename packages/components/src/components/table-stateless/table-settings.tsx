c833365d9bf78d8c377b15fce0097b66d6807bb2
Stefan Dietz <mail@stefan-dietz.eu>
2025-04-22 20:41:38 +0200

    Provide translations
    Refs: #1834

--- packages/components/src/components/table-stateless/table-settings.tsx
+++ packages/components/src/components/table-stateless/table-settings.tsx
@@ -13,24 +13,23 @@ import { KolPopoverButtonWcTag, KolInputNumberTag, KolInputCheckboxTag, KolButto
 export class KolTableSettings {
 	public render(): JSX.Element {
 		return (
-			<KolPopoverButtonWcTag class="kol-table-settings" _icons="codicon codicon-settings-gear" _label={translate('kol-table-configuration')} _hideLabel>
+			<KolPopoverButtonWcTag class="kol-table-settings" _icons="codicon codicon-settings-gear" _label={translate('kol-table-settings')} _hideLabel>
 				<div class="kol-table-settings__content">
-					<h2 class="kol-table-settings__title">Table Configuration</h2>
+					<h2 class="kol-table-settings__title">{translate('kol-table-settings')}</h2>
 					<div class="kol-table-settings__columns">
-						{/* Left Column */}
 						<div class="kol-table-settings__column">
-							<KolInputCheckboxTag _checked _label="Show column Left Column" _hideLabel />
+							<KolInputCheckboxTag _checked _label={translate('kol-table-settings-show-column', { placeholders: { column: 'Left Column' } })} _hideLabel />
 							<span>Left Column</span>
 
-							<KolInputNumberTag _hideLabel _value={10} _label="Left Column Width" />
-							<KolButtonWcTag _icons="codicon codicon-arrow-up" _label="Move column up" _hideLabel _variant="ghost" />
-							<KolButtonWcTag _icons="codicon codicon-arrow-down" _label="Move column down" _hideLabel _variant="ghost" />
+							<KolInputNumberTag _hideLabel _value={10} _label={translate('kol-table-settings-column-width', { placeholders: { column: 'Left Column' } })} />
+							<KolButtonWcTag _icons="codicon codicon-arrow-up" _label={translate('kol-table-settings-move-up')} _hideLabel _variant="ghost" />
+							<KolButtonWcTag _icons="codicon codicon-arrow-down" _label={translate('kol-table-settings-move-down')} _hideLabel _variant="ghost" />
 						</div>
 					</div>
 
 					<div class="kol-table-settings__actions">
-						<KolButtonWcTag _label="Cancel" _variant="secondary" />
-						<KolButtonWcTag _label="Apply" _variant="primary" />
+						<KolButtonWcTag _label={translate('kol-table-settings-cancel')} _variant="secondary" />
+						<KolButtonWcTag _label={translate('kol-table-settings-apply')} _variant="primary" />
 					</div>
 				</div>
 			</KolPopoverButtonWcTag>
