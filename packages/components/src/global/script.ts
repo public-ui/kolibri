d5dad25b9084a461f833a47372b0ec3c24bc5d73
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-05-07 13:05:57 +0200

    refactor: remove unused theme management code from script.ts
    Refs: #7655

--- packages/components/src/global/script.ts
+++ packages/components/src/global/script.ts
@@ -1,22 +1,4 @@
-import { getThemeDetails, setThemeStyle } from 'adopted-style-sheets';
-
 import { Log } from '../schema';
-import { setMode } from '@stencil/core';
-
-setMode((elm) => {
-	try {
-		if (elm.shadowRoot instanceof ShadowRoot) {
-			setThemeStyle(elm, getThemeDetails(elm));
-		}
-	} catch (error) {
-		/**
-		 * Try is needed for SSR.
-		 * - no HTMLElement is available
-		 * - no ShadowRoot is available
-		 */
-	}
-	return 'default';
-});
 
 import('./devtools')
 	.then((devTools) => {
