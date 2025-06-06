5ab349bbc7032bef9717cf5eb6bc0b641348b1d7
Stefan Dietz <mail@stefan-dietz.eu>
2025-06-06 13:50:58 +0200

    Remove redundant tests
    Refs: #7735

--- packages/components/src/components/heading/test/snapshot.spec.tsx
+++ packages/components/src/components/heading/test/snapshot.spec.tsx
@@ -7,9 +7,5 @@ import { KolHeading } from '../shadow';
 executeSnapshotTests<HeadingProps>(
 	KolHeadingTag,
 	[KolHeading],
-	[
-		{ _label: 'Headline' },
-		...[0, 1, 2, 3, 4, 5, 6].map((_level) => ({ _label: 'Headline', _level }) as HeadingProps),
-		...['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'strong'].map((_variant) => ({ _label: 'Headline', _variant }) as HeadingProps),
-	],
+	[{ _label: 'Headline' }, ...[0, 1, 2, 3, 4, 5, 6].map((_level) => ({ _label: 'Headline', _level }) as HeadingProps)],
 );
