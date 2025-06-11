246c832168345f0595071383361bb869c91eb46f
Stefan Dietz <mail@stefan-dietz.eu>
2025-06-11 13:43:03 +0200

    Update component test and snapshots
    Refs: #7825

--- packages/components/src/components/toolbar/test/snapshot.spec.tsx
+++ packages/components/src/components/toolbar/test/snapshot.spec.tsx
@@ -4,21 +4,28 @@ import { executeSnapshotTests } from '../../../utils/testing';
 
 import { KolToolbar } from '../shadow';
 
+const ITEMS = [
+	{
+		_label: 'Button',
+	},
+	{
+		_href: '#',
+		_label: 'Link',
+	},
+];
+
 executeSnapshotTests<ToolbarProps>(
 	KolToolbarTag,
 	[KolToolbar],
 	[
 		{
-			_label: 'Text',
-			_items: [
-				{
-					_label: 'Button',
-				},
-				{
-					_href: '#',
-					_label: 'Link',
-				},
-			],
+			_label: 'Label horizontal',
+			_items: ITEMS,
+		},
+		{
+			_label: 'Label vertical',
+			_items: ITEMS,
+			_orientation: 'vertical',
 		},
 	],
 );
