9ed1a6971ba64960f46fcd82a3bf00de92c3edf4
Peter Laske <37439758+laske185@users.noreply.github.com>
2025-06-18 15:07:15 +0200

    feat(samples): add KolSpin label demo

---
+++ packages/samples/react/src/components/spin/label.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { KolSpin } from '@public-ui/react';
+
+import type { FC } from 'react';
+import { SampleDescription } from '../SampleDescription';
+
+export const SpinLabel: FC = () => (
+	<>
+		<SampleDescription>
+			<p>This sample shows KolSpin with an accessible label.</p>
+		</SampleDescription>
+
+		<KolSpin _show _label="Loading" />
+	</>
+);
