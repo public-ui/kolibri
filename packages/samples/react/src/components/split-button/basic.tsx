d2c0ce91ef23bb8836bce4ebd278531d7e0f6306
Martin <6279703+deleonio@users.noreply.github.com>
2025-07-17 05:27:48 +0200

    feat(samples-react): improve split-button example (#7805)

--- packages/samples/react/src/components/split-button/basic.tsx
+++ packages/samples/react/src/components/split-button/basic.tsx
@@ -1,38 +1,33 @@
 import React from 'react';
 
-import { ToasterService } from '@public-ui/components';
-import { KolSplitButton } from '@public-ui/react';
+import { KolSplitButton, KolToolbar } from '@public-ui/react';
 import { SampleDescription } from '../SampleDescription';
+import { useToasterService } from '../../hooks/useToasterService';
 
 import type { FC } from 'react';
 
 export const SplitButtonBasic: FC = () => {
-	const toaster = ToasterService.getInstance(document);
-	const handleButtonClick = () => {
-		void toaster.enqueue({
-			description: 'The Button has been clicked.',
-			label: `Button Clicked`,
-			type: 'info',
-		});
+	const { buttonWithTextClickEventHandler } = useToasterService();
+
+	const TOOLBAR_ITEMS = [
+		{ _label: 'Speichern', _on: { onClick: buttonWithTextClickEventHandler } },
+		{ _label: 'Verschieben', _on: { onClick: buttonWithTextClickEventHandler } },
+		{ _label: 'Löschen', _on: { onClick: buttonWithTextClickEventHandler } },
+	];
+
+	const handlePrimaryClick = {
+		onClick: buttonWithTextClickEventHandler,
 	};
 
 	return (
 		<>
 			<SampleDescription>
-				<p>
-					SplitButton renders a button with an additional context-menu, that can be opened by clicking the arrow icon. The first sample shows the button with an
-					assigned action. In the second sample no action is assigned and clicking the button opens the context menu instead.
-				</p>
+				<p>SplitButton renders a button with an additional context-menu that opens a list of actions.</p>
 			</SampleDescription>
 
-			<div className="flex gap-4">
-				<KolSplitButton _label="Only the arrow opens" _on={{ onClick: handleButtonClick }}>
-					Dropdown contents
-				</KolSplitButton>
-				<KolSplitButton _label="Button without visible label" _hideLabel _icons="codicon codicon-git-pull-request">
-					Dropdown contents
-				</KolSplitButton>
-			</div>
+			<KolSplitButton _label="Bearbeiten" _on={handlePrimaryClick}>
+				<KolToolbar _label="Aktionen" _items={TOOLBAR_ITEMS} _orientation="vertical" />
+			</KolSplitButton>
 		</>
 	);
 };
--- packages/samples/react/src/components/split-button/routes.ts
+++ packages/samples/react/src/components/split-button/routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '../../shares/types';
 import { SplitButtonBasic } from './basic';
-import { SplitButtonWithContext } from './with-context';
 
 export const SPLIT_BUTTON_ROUTES: Routes = {
 	'split-button': {
 		basic: SplitButtonBasic,
-		'with-context': SplitButtonWithContext,
 	},
 };
