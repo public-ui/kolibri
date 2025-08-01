61ab100478d965ca04bf59dac895c22b3ca440c6
Martin <6279703+deleonio@users.noreply.github.com>
2025-08-01 21:41:29 +0200

    fix: type-safe controller prop assignment

--- packages/components/src/components/_skeleton/internal/functional-components/base-controller.ts
+++ packages/components/src/components/_skeleton/internal/functional-components/base-controller.ts
@@ -3,7 +3,7 @@ import type { ComponentInterface } from './generic-types';
 export abstract class BaseController<RenderProps, Host extends ComponentInterface<RenderProps> = ComponentInterface<RenderProps>> {
 	public constructor(protected readonly component: Host) {}
 
-	public setRenderPropsOrStates<K extends keyof RenderProps>(prop: K, value: RenderProps[K]): void {
-		(this.component as ComponentInterface<RenderProps>)[prop] = value;
+	public setRenderPropsOrStates<K extends keyof RenderProps>(prop: K, value: Host[K]): void {
+		this.component[prop] = value;
 	}
 }
