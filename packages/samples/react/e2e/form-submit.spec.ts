8d00070c45e08d32fb72346aa939c6919f0e9513
Oliver Schulczynski <Oliver.Schulczynski@iso-gruppe.com>
2025-08-04 13:13:15 +0200

    7645 - Correction of the e2e expect formulation
    Refs: #7645

--- packages/samples/react/e2e/form-submit.spec.ts
+++ packages/samples/react/e2e/form-submit.spec.ts
@@ -8,24 +8,24 @@ test.describe('form-submit', () => {
 	test('Submit form with checkbox checked and with submit button (via Enter)', async ({ page }) => {
 		await page.getByLabel('Checkbox1').check();
 		await page.getByLabel('Checkbox1').press('Enter');
-		expect(await page.locator('pre').innerText()).toContain('"Form mit Button"');
+		expect(await page.locator('pre').innerText()).toContain('"Form with Submit-Button"');
 	});
 
 	test('Submit form with radio checked and with submit button (via Enter)', async ({ page }) => {
 		await page.getByLabel('Radio1').check();
 		await page.getByLabel('Radio1').press('Enter');
-		expect(await page.locator('pre').innerText()).toContain('"Form mit Button"');
+		expect(await page.locator('pre').innerText()).toContain('"Form with Submit-Button"');
 	});
 
 	test('Submit form with checkbox checked and without submit button (via Enter)', async ({ page }) => {
 		await page.getByLabel('Checkbox2').check();
 		await page.getByLabel('Checkbox2').press('Enter');
-		expect(await page.locator('pre').innerText()).toContain('"Form ohne Button"');
+		expect(await page.locator('pre').innerText()).toContain('"Form without Submit-Button"');
 	});
 
 	test('Submit form with radio checked and without submit button (via Enter)', async ({ page }) => {
 		await page.getByLabel('Radio2').check();
 		await page.getByLabel('Radio2').press('Enter');
-		expect(await page.locator('pre').innerText()).toContain('"Form ohne Button"');
+		expect(await page.locator('pre').innerText()).toContain('"Form without Submit-Button"');
 	});
 });
