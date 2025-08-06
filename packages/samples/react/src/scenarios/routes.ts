cb48ae2edddce69eeead33a9acf99ff8fe288e67
Oliver Schulczynski <Oliver.Schulczynski@iso-gruppe.com>
2025-08-06 12:27:55 +0200

    7645 - change e2e Test form-submit to static-form and finally remove scenario form-submit
    Refs: #7645

--- packages/samples/react/src/scenarios/routes.ts
+++ packages/samples/react/src/scenarios/routes.ts
@@ -3,7 +3,6 @@ import { ChangeTabindex } from './change-tabindex';
 import { CustomTooltipWidth } from './custom-tooltip-width';
 import { DisabledInteractiveElements } from './disabled-interactive-elements';
 import { FocusElements } from './focus-elements';
-import { FormSubmit } from './form-submit';
 import { InputGroupWithError } from './input-group-with-error';
 import { InputsGetValue } from './inputs-get-value';
 import { PerformanceTest } from './performance-test';
@@ -18,7 +17,6 @@ export const SCENARIO_ROUTES: Routes = {
 		'custom-tooltip-width': CustomTooltipWidth,
 		'disabled-interactive-scenario': DisabledInteractiveElements,
 		'focus-elements': FocusElements,
-		'form-submit': FormSubmit,
 		'input-group-with-error': InputGroupWithError,
 		'inputs-get-value': InputsGetValue,
 		'performance-test': PerformanceTest,
