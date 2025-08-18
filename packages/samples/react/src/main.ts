5ead247621bdc2200da2de33e801ede3626fec16
Stefan Dietz <mail@stefan-dietz.eu>
2025-08-18 20:44:22 +0200

    Sample App: Allow to load optional THEME_CSS
    Refs: #8245

--- packages/samples/react/src/main.ts
+++ packages/samples/react/src/main.ts
@@ -1,5 +1,11 @@
 import 'uno.css'; // https://github.com/antfu/unocss
 // App style
 import './style.scss';
+
+/* Optional theme CSS (e.g. font-face declarations) */
+if (process.env.THEME_CSS) {
+	import(/* @vite-ignore */ process.env.THEME_CSS);
+}
+
 // App
 import './react.main';
--- packages/themes/default/serve.sh
+++ packages/themes/default/serve.sh
@@ -2,6 +2,7 @@
 
 export THEME_MODULE="`pwd`/dist"
 export THEME_EXPORT=$1
+export THEME_CSS="`pwd`/assets/sample-app.css"
 cd ../../../node_modules/@public-ui/sample-react/
-echo "`pwd`"
+
 npm start
