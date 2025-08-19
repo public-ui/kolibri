648bf7f9b0dcb617bf1403ddcc9b6c48689f0d2e
Stefan Dietz <mail@stefan-dietz.eu>
2025-08-19 09:52:39 +0200

    Revert default theme changes
    Refs: #8245

--- packages/themes/default/serve.sh
+++ packages/themes/default/serve.sh
@@ -2,7 +2,6 @@
 
 export THEME_MODULE="`pwd`/dist"
 export THEME_EXPORT=$1
-export THEME_CSS="`pwd`/assets/sample-app.css"
 cd ../../../node_modules/@public-ui/sample-react/
-
+echo "`pwd`"
 npm start
