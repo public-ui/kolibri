7a14b9c4d22a1c2f665be07fd3742504f44115b8
Oliver Schulczynski <Oliver.Schulczynski@iso-gruppe.com>
2025-09-04 11:25:38 +0200

    8305 (card) style close button in default theme like ghost button
    Refs: #8305

--- packages/components/src/components/card/component.tsx
+++ packages/components/src/components/card/component.tsx
@@ -62,7 +62,6 @@ export class KolCardWc implements CardAPI {
 								},
 							}}
 							_label={this.translateClose}
-							_buttonVariant="ghost"
 							_on={this.on}
 							_tooltipAlign="left"
 						/>
