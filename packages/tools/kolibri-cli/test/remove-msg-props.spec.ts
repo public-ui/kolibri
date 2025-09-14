8360c2d47fff73c620e3e92dcbdca49b09efef84
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-09-14 06:38:04 +0200

    test: indent remove-msg-props spec

--- packages/tools/kolibri-cli/test/remove-msg-props.spec.ts
+++ packages/tools/kolibri-cli/test/remove-msg-props.spec.ts
@@ -15,13 +15,13 @@ describe('RemoveMsgPropsTask', () => {
 		const task = RemoveMsgPropsTask.getInstance('^4');
 		task.run(tmpDir);
 
-const tsxContent = fs.readFileSync(tsxPath, 'utf8');
-const htmlContent = fs.readFileSync(htmlPath, 'utf8');
-assert.ok(!tsxContent.includes('_label'));
-assert.ok(!tsxContent.includes('_variant'));
-assert.match(tsxContent, /_msg=\{\{\s*_type:\s*'error',\s*_description:\s*'Oops'\s*\}\}/);
-assert.ok(!htmlContent.includes('_label'));
-assert.ok(!htmlContent.includes('_variant'));
-assert.match(htmlContent, /_msg='\{"_type":"error","_description":"Oops"\}'/);
-});
+		const tsxContent = fs.readFileSync(tsxPath, 'utf8');
+		const htmlContent = fs.readFileSync(htmlPath, 'utf8');
+		assert.ok(!tsxContent.includes('_label'));
+		assert.ok(!tsxContent.includes('_variant'));
+		assert.match(tsxContent, /_msg=\{\{\s*_type:\s*'error',\s*_description:\s*'Oops'\s*\}\}/);
+		assert.ok(!htmlContent.includes('_label'));
+		assert.ok(!htmlContent.includes('_variant'));
+		assert.match(htmlContent, /_msg='\{"_type":"error","_description":"Oops"\}'/);
+	});
 });
