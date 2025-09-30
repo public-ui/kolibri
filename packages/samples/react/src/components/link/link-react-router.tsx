b98fc91264d5a3687e788aeb66e763df3eff4d8a
donald donchi fofack <donchidonald2000@gmail.com>
2025-09-30 12:39:02 +0200

    feat(samples): update code after review
    Refs: #8523

--- packages/samples/react/src/components/link/link-react-router.tsx
+++ packages/samples/react/src/components/link/link-react-router.tsx
@@ -1,35 +1,30 @@
 import type { FC } from 'react';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
-import { KolLink, KolHeading } from '@public-ui/react-v19';
+import { KolLink } from '@public-ui/react-v19';
 import { SampleDescription } from '../SampleDescription';
-
-const ROUTE = '/back-page';
 
 export const LinkReactRouter: FC = () => {
 	const navigate = useNavigate();
 
-	const handleClick = (event: Event) => {
-		event.preventDefault();
-		navigate(ROUTE);
+	const on = {
+		onClick: (event: Event) => {
+			event.preventDefault();
+			navigate('/back-page');
+		},
 	};
 
 	return (
 		<>
 			<SampleDescription>
 				<p>
-					This sample shows how <code>KolLink</code> works with <code>react-router</code>. Primary click uses <code>preventDefault()</code> and{' '}
-					<code>navigate(...)</code>, while right click / &#34;open in new tab&#34; still follows <code>_href</code>. Make sure <code>_href</code> and{' '}
-					<code>navigate(...)</code> point to the same path.
+					This sample shows how KolLink works with react-router. Primary click uses preventDefault() and navigate(...), while right click open in new tab still
+					follows _href. Make sure _href and navigate(...) point to the same path.
 				</p>
 			</SampleDescription>
 
 			<div className="grid gap-4">
-				<KolHeading _level={2} _label="React Router Navigation" />
-				<KolLink _href={ROUTE} _label="Go to overview" _variant="standalone" _on={{ onClick: handleClick }} />
-
-				<KolLink _hideLabel _icons="codicon codicon-home" _href={ROUTE} _label="Go to overview (icon)" _on={{ onClick: handleClick }} />
+				<KolLink _href="/back-page" _label="Go to overview" _variant="standalone" _on={on} />
 			</div>
 		</>
 	);
