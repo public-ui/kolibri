c5fc4256f4c8befd850312deb0ad52e94b860171
BF150 <franziska.bartsch@iso-gruppe.com>
2025-10-08 16:14:20 +0200

    fixed selection problems$ $ Refs: #8211

--- packages/samples/react/src/components/input-text/text-formatter.tsx
+++ packages/samples/react/src/components/input-text/text-formatter.tsx
@@ -20,19 +20,10 @@ class IbanFormatter {
 	public parse(value: string): string {
 		return this.electronicFormat(value);
 	}
-	public format(value: string, ref?: HTMLKolInputTextElement | null): string {
-		if (ref) {
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-			ref.selectionStart().then((value) => {
-				// schon zu spät selectionstart ist schon am ende des strings
-				console.log(value);
-
-				if (value) {
-					setTimeout(() => {
-						ref?.setSelectionStart(value);
-					}, 1);
-				}
-			});
+	public format(value: string, ref?: HTMLKolInputTextElement | null, selectionStart?: number | null): string {
+		if (ref && selectionStart) {
+			if (selectionStart % 5 === 0) selectionStart++;
+			ref?.setSelectionStart(selectionStart);
 		}
 		return this.printFormat(value);
 	}
@@ -51,7 +42,8 @@ export function InputTextFormatterDemo() {
 	const formatter = new IbanFormatter();
 
 	const textInput1 = React.useRef<HTMLKolInputTextElement>(null);
-	const textInput2 = React.useRef<HTMLKolInputTextElement>(null);
+	let textInput1SelectionStart: number | null | undefined;
+	let textInput1InputLength: number | undefined;
 
 	const initialIbanExampleValues: IbanExampleFormValues = {
 		iban: 'DE89370400440532013000',
@@ -86,7 +78,7 @@ export function InputTextFormatterDemo() {
 													}}
 													id="field-iban"
 													_label="IBAN"
-													_value={formatter.format(field.value ?? '', textInput1.current)}
+													_value={formatter.format(field.value ?? '', textInput1.current, textInput1SelectionStart)}
 													_msg={{
 														_type: 'error',
 														_description: form.errors.iban || '',
@@ -96,6 +88,10 @@ export function InputTextFormatterDemo() {
 													_on={{
 														onInput: (event, value: unknown) => {
 															if (event.target) {
+																textInput1.current?.selectionStart().then((start) => {
+																	textInput1SelectionStart = start;
+																	textInput1InputLength = (value as string).length;
+																});
 																const parsed_value = formatter.parse((value as string) ?? '');
 																void form.setFieldValue('iban', parsed_value, true);
 															}
@@ -128,7 +124,7 @@ export function InputTextFormatterDemo() {
 											<div className="block mt-2">
 												<NumericFormat
 													customInput={({ type, value, onBlur, onChange, onFocus }: any) => {
-														return <KolInputText _label="Currency" _type={type} _value={value} _on={{ onBlur, onChange, onFocus }} ref={textInput2} />;
+														return <KolInputText _label="Currency" _type={type} _value={value} _on={{ onBlur, onChange, onFocus }} />;
 													}}
 													displayType="input"
 													value={typeof field.value === 'number' ? field.value.toFixed(2) : undefined}
