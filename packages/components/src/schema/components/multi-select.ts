34f1d1aea0a65790b01ca1d1360e2395f3700788
Hasan Yurtsever <yurtseverhasan84@gmail.com>
2025-10-13 14:06:57 +0200

    feat: add multi-select schema$ $ Refs: #7313

---
+++ packages/components/src/schema/components/multi-select.ts
@@ -0,0 +1,69 @@
+import type { Generic } from 'adopted-style-sheets';
+
+import type {
+	MsgPropType,
+	PropAccessKey,
+	PropDisabled,
+	PropHideLabel,
+	PropHideMsg,
+	PropHint,
+	PropHorizontalIcons,
+	PropId,
+	PropLabelWithExpertSlot,
+	PropMsg,
+	PropName,
+	PropOptions,
+	PropRequired,
+	PropShortKey,
+	PropSyncValueBySelector,
+	PropTouched,
+	PropRows,
+} from '../props';
+import type { InputTypeOnDefault, KoliBriHIcons, Option, StencilUnknown, Stringified } from '../types';
+
+type RequiredProps = PropLabelWithExpertSlot & PropOptions;
+type OptionalProps = {
+	msg: Stringified<MsgPropType>;
+	on: InputTypeOnDefault;
+	placeholder: string;
+	value: StencilUnknown[];
+	hideClearButton: boolean;
+} & PropAccessKey &
+	PropDisabled &
+	PropHideMsg &
+	PropHideLabel &
+	PropHint &
+	PropHorizontalIcons &
+	PropName &
+	PropRequired &
+	PropRows &
+	PropSyncValueBySelector &
+	PropShortKey &
+	PropTouched;
+
+type RequiredStates = {
+	options: Option<StencilUnknown>[];
+} & PropId &
+	PropHideMsg &
+	PropLabelWithExpertSlot;
+type OptionalStates = {
+	on: InputTypeOnDefault;
+	placeholder: string;
+	hideClearButton: boolean;
+} & PropAccessKey &
+	PropDisabled &
+	PropHideLabel &
+	KoliBriHIcons &
+	PropHint &
+	PropId &
+	PropName &
+	PropRequired &
+	PropRows &
+	PropMsg &
+	PropShortKey &
+	PropTouched;
+
+export type MultiSelectProps = Generic.Element.Members<RequiredProps, OptionalProps>;
+export type MultiSelectStates = Generic.Element.Members<RequiredStates, OptionalStates>;
+export type MultiSelectWatches = Generic.Element.Watchers<RequiredProps, OptionalProps>;
+export type MultiSelectAPI = Generic.Element.ComponentApi<RequiredProps, OptionalProps, RequiredStates, OptionalStates>;
