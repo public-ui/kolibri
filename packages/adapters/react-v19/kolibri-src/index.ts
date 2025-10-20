1f86d2de082e6f80b7854e039a98f797e41d30a2
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-10-20 19:35:43 +0200

    fix: update import path for StyleReactProps type

--- packages/adapters/react-v19/kolibri-src/index.ts
+++ packages/adapters/react-v19/kolibri-src/index.ts
@@ -1,3 +1,3 @@
 export * from '../src/index';
-export type { StyleReactProps } from '../src/index';
+export type { StyleReactProps } from '../src/react-component-lib/interfaces';
 export { createReactRenderElement } from './create-react-render-element';
