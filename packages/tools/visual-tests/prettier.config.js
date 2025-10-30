7a9693ef57cde993c7918b4142b946e461df797b
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-10-30 06:56:10 +0100

    feat(visual-tests): allow configuring color scheme

--- packages/tools/visual-tests/prettier.config.js
+++
@@ -1,8 +0,0 @@
-import organizeImportsPlugin from 'prettier-plugin-organize-imports';
-
-export default {
-	plugins: [organizeImportsPlugin],
-	printWidth: 160,
-	singleQuote: true,
-	useTabs: true,
-};
