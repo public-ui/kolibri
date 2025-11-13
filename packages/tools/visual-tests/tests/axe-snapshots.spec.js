9d0ea7fa7d0cb9d994febb10a9e1014d97a2a9c1
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-11-13 15:19:49 +0100

    chore(visual-tests): fix package formatting

--- packages/tools/visual-tests/tests/axe-snapshots.spec.js
+++ packages/tools/visual-tests/tests/axe-snapshots.spec.js
@@ -1,5 +1,5 @@
 import { test } from '@playwright/test';
-import { checkA11y, injectAxe } from 'axe-playwright';
+import { checkA11y, injectAxe } from '@axe-core/playwright';
 import process from 'process';
 import { ROUTES } from './sample-app.routes.js';
 
