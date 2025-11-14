93ce054876454a4cba5e3cafe4ded9df3af39893
Peter Laske <37439758+laske185@users.noreply.github.com>
2025-11-14 11:46:34 +0100

    Update sample routes

--- packages/tools/visual-tests/tests/sample-app.routes.js
+++ packages/tools/visual-tests/tests/sample-app.routes.js
@@ -119,7 +119,21 @@ ROUTES.set('button-link/image', {
 		},
 	},
 });
-ROUTES.set('button/basic', {
+ROUTES.set('button/variants', {
+	snapshot: {
+		zoom: {
+			skip: true,
+		},
+	},
+});
+ROUTES.set('button/disabled', {
+	snapshot: {
+		zoom: {
+			skip: true,
+		},
+	},
+});
+ROUTES.set('button/hide-label', {
 	snapshot: {
 		zoom: {
 			skip: true,
@@ -128,31 +142,6 @@ ROUTES.set('button/basic', {
 });
 ROUTES.set('button/icons', {
 	snapshot: {
-		skip: true,
-		zoom: {
-			skip: true,
-		},
-	},
-});
-ROUTES.set('button/width', {
-	snapshot: {
-		skip: true,
-		zoom: {
-			skip: true,
-		},
-	},
-});
-ROUTES.set('button/access-key', {
-	snapshot: {
-		skip: true,
-		zoom: {
-			skip: true,
-		},
-	},
-});
-ROUTES.set('button/baselined', {
-	snapshot: {
-		skip: true,
 		zoom: {
 			skip: true,
 		},
@@ -160,10 +149,6 @@ ROUTES.set('button/baselined', {
 });
 ROUTES.set('button/short-key', {
 	snapshot: {
-		viewportSize: {
-			width: 800,
-			height: 434,
-		},
 		zoom: {
 			skip: true,
 		},
