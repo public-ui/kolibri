1b8fa843a5f5e8286ddfed6139b62102627debd7
BF150 <franziska.bartsch@iso-gruppe.com>
2025-11-25 08:03:51 +0100

    removed$ $ Refs: #8900

--- packages/samples/react/src/components/table/horizontal-scrollbar.tsx
+++ packages/samples/react/src/components/table/horizontal-scrollbar.tsx
@@ -39,11 +39,6 @@ export const TableHorizontalScrollbar: FC = () => {
 				</p>
 			</SampleDescription>
 
-			<KolAlert _variant="card" _type="warning" _label="Deprecation of minWidth">
-				In version 3, the width of the column will be required for the implicit calculation of the minWidth. This behavior is also implemented in version 2, if
-				the _minWidth property is undefined.
-			</KolAlert>
-			<br />
 			<section className="w-full flex flex-col gap-4">
 				<KolHeading _label="Table with scrollbar" _level={2} />
 
