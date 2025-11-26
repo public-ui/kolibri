c5bf8855d78b2997655985d7057ad71862b9f5ba
Peter Laske <37439758+laske185@users.noreply.github.com>
2025-11-26 09:49:38 +0000

    feat: update table sample data

--- packages/samples/react/src/components/table/sticky-header.tsx
+++ packages/samples/react/src/components/table/sticky-header.tsx
@@ -10,14 +10,12 @@ const HEADERS_HORIZONTAL: KoliBriTableHeaders = {
 	horizontal: [
 		[
 			{ label: 'ID', key: 'id', textAlign: 'right' },
-			{ label: 'Common Name', key: 'common_name', textAlign: 'left' },
-			{ label: 'Scientific Name', key: 'scientific_name', textAlign: 'left' },
-			{ label: 'EMail', key: 'email', textAlign: 'left' },
-			{ label: 'IP Address', key: 'ip_address', textAlign: 'center' },
-			{ label: 'User Name', key: 'user_name', textAlign: 'left' },
-			{ label: 'Bitcoin Address', key: 'bitcoin_address', textAlign: 'center' },
-			{ label: 'Street Address', key: 'street_address', textAlign: 'center' },
-			{ label: 'IBAN', key: 'iban', textAlign: 'left' },
+			{ label: 'Common name', key: 'common_name', textAlign: 'left' },
+			{ label: 'Scientific name', key: 'scientific_name', textAlign: 'left' },
+			{ label: 'Conservation status', key: 'conservation_status', textAlign: 'left' },
+			{ label: 'Habitat', key: 'habitat', textAlign: 'left' },
+			{ label: 'Diet', key: 'diet', textAlign: 'left' },
+			{ label: 'Geographic range', key: 'geographic_range', textAlign: 'left' },
 		],
 	],
 };
@@ -27,11 +25,11 @@ export const TableStickyHeader: FC = () => (
 		<SampleDescription>
 			<p>
 				This sample shows KolTableStateful with horizontal and vertical scrollbars. It should be focusable and scrollable with the keyboard. The table headline
-				should be sticky at the top of the table.
+				should be sticky at the top of the table. The data now lists animal species with conservation-focused details.
 			</p>
 		</SampleDescription>
 
 		<KolHeading _level={2} _label="Sticky headers" />
-		<KolTableStateful _label="Sort a date column" _minWidth="1800px" _data={COMPLEX_DATA} _headers={HEADERS_HORIZONTAL} className="block" />
+		<KolTableStateful _label="Animal species overview" _minWidth="1200px" _data={COMPLEX_DATA} _headers={HEADERS_HORIZONTAL} className="block" />
 	</>
 );
