71319ea97efc99293f16fb6a9fab045999c6ceab
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-11-30 09:24:30 +0100

    Update deprecation and license reporting scripts for Angular v21
    - Added deprecation notices for Angular v21 in deprecate-add.sh and deprecate-rm.sh.
    - Updated dist-tags.sh to include Angular v21 for distribution tagging.
    - Included license report generation for Angular v21 in license-reports.sh.

---
+++ packages/adapters/angular/v21/ng-module.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const componentFileContents = fs.readFileSync('./src/components.ts', 'utf-8');
+const componentList = [...componentFileContents.matchAll(/export class ([^ ]+)/g)].map((group) => group[1]);
+const componentListStr = componentList.join(', ');
+
+fs.writeFileSync(
+	'./src/index.ts',
+	`
+/* AutoGen NgModule */
+
+export { ${componentListStr} } from './components';
+export { ReplaceTagDirective } from './angular-component-lib/ReplaceTagDirective';
+export { setTagNameTransformer } from './angular-component-lib/tagNameTransformer';
+`,
+	'utf-8',
+);
