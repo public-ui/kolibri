feb21af8e2ed69686ebd04fe7a15fb02a9c58221
Peter Laske <37439758+laske185@users.noreply.github.com>
2025-12-03 09:35:50 +0100

    feat: update InputTextBackground story to enhance background color and showcase input variants

--- packages/samples/react/src/components/input-text/background-test.tsx
+++ packages/samples/react/src/components/input-text/background-test.tsx
@@ -9,8 +9,12 @@ export const InputTextBackground: FC = () => (
 			<p>This story showcases the component placed on a colored background.</p>
 		</SampleDescription>
 
-		<div className="grid gap-4 bg-blue-100 p-4 rounded">
+		<div className="grid gap-4 bg-blue-400 p-4 rounded">
+			<strong>Default background:</strong>
 			<KolInputText _label="First name" _value="John Doe" />
+
+			<strong>Unset background:</strong>
+			<KolInputText style={{ backgroundColor: 'unset' }} _label="First name" _value="John Doe" />
 		</div>
 	</>
 );
