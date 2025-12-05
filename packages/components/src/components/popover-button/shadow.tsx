322e2f0783b0090d65c2024f031791ea4225d858
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-12-05 08:53:19 +0100

    feat: add inline rendering support for button, link, and popover components
    - Introduced `_inline` property to `KolButton`, `KolLink`, and `KolPopoverButton` components to allow inline rendering without enforcing a minimum height.
    - Updated styles to accommodate inline rendering, including new CSS classes for inline and standalone variants.
    - Modified existing components and tests to utilize the new `_inline` property, replacing the deprecated `_variant` property.
    - Enhanced visual tests to cover inline rendering scenarios for button and link components.
    - Updated documentation and examples to reflect the changes in usage for inline rendering.

--- packages/components/src/components/popover-button/shadow.tsx
+++ packages/components/src/components/popover-button/shadow.tsx
@@ -10,6 +10,7 @@ import type {
 	ButtonVariantPropType,
 	CustomClassPropType,
 	IconsPropType,
+	InlinePropType,
 	LabelWithExpertSlotPropType,
 	PopoverAlignPropType,
 	ShortKeyPropType,
@@ -134,6 +135,11 @@ export class KolPopoverButton implements PopoverButtonProps {
 	 * Defines the icon classnames (e.g. `_icons="fa-solid fa-user"`).
 	 */
 	@Prop() public _icons?: IconsPropType;
+
+	/**
+	 * Defines whether the component is displayed as a standalone block or inline without enforcing a minimum size of 44px.
+	 */
+	@Prop() public _inline?: InlinePropType = false;
 
 	/**
 	 * Defines the internal ID of the primary component element.
--- packages/components/src/schema/props/link-variant.ts
+++ packages/components/src/schema/props/link-variant.ts
@@ -1,24 +1,8 @@
 /* types */
-import type { Generic } from 'adopted-style-sheets';
-import { watchValidator } from '../utils';
 
 const linkVariantPropTypeOptions = ['inline', 'standalone'] as const;
-export type LinkVariantPropType = (typeof linkVariantPropTypeOptions)[number];
 
 /**
- * Defines which variant should be used for presentation.
+ * @deprecated Use the new _inline property instead.
  */
-export type PropLinkVariant = {
-	variant: LinkVariantPropType;
-};
-
-/* validator */
-export const validateLinkVariant = (component: Generic.Element.Component, value?: LinkVariantPropType): void => {
-	watchValidator(
-		component,
-		`_linkVariant`,
-		(value) => typeof value === 'string' && linkVariantPropTypeOptions.includes(value),
-		new Set([`KoliBriLinkVariant {${linkVariantPropTypeOptions.join(', ')}`]),
-		value,
-	);
-};
+export type LinkVariantPropType = (typeof linkVariantPropTypeOptions)[number];
