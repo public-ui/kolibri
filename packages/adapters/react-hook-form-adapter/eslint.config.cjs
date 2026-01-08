d1a85d63c85ff69990bf47931ff540ae3518f2d6
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2026-01-08 20:09:15 +0100

    chore: fix eslint 9 compat config

--- packages/adapters/react-hook-form-adapter/eslint.config.cjs
+++ packages/adapters/react-hook-form-adapter/eslint.config.cjs
@@ -1,8 +1,13 @@
+const js = require('@eslint/js');
 const { FlatCompat } = require('@eslint/eslintrc');
 
 const compat = new FlatCompat({
 	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
 	resolvePluginsRelativeTo: __dirname,
+	allConfig: js.configs.all,
 });
 
-module.exports = [...compat.config(require('./.eslintrc.cjs'))];
+module.exports = [
+	...compat.config(require('./.eslintrc.cjs'))
+]
--- packages/components/eslint.config.cjs
+++ packages/components/eslint.config.cjs
@@ -1,8 +1,16 @@
+const js = require('@eslint/js');
 const { FlatCompat } = require('@eslint/eslintrc');
 
 const compat = new FlatCompat({
 	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
 	resolvePluginsRelativeTo: __dirname,
+	allConfig: js.configs.all,
 });
 
-module.exports = [...compat.config(require('./.eslintrc.js'))];
+module.exports = [
+	{
+		ignores: ['**/assets/**', 'scripts/*.js', 'src/**/*.js', 'src/**/*.html'],
+	},
+	...compat.config(require('./.eslintrc.js'))
+]
