127b8439909d9f7b0b2e4b685c81813da1b091d3
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2026-01-08 19:59:21 +0100

    chore: migrate eslint to v9

---
+++ packages/themes/ecl/eslint.config.cjs
@@ -0,0 +1,8 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	resolvePluginsRelativeTo: __dirname,
+});
+
+module.exports = [...compat.config(require('./.eslintrc.cjs'))];
