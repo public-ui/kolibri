05dbcea8082b230206e1e655ef49616605b961a0
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-01-14 16:20:54 +0100

    obj fix

--- packages/samples/react/src/components/icon/basic.tsx
+++ packages/samples/react/src/components/icon/basic.tsx
@@ -6,7 +6,7 @@ import type { FC } from 'react';
 import { SampleDescription } from '../SampleDescription';
 
 export const IconBasic: FC = () => {
-	const [icons, setIcons] = useState<{}>({});
+	const [icons, setIcons] = useState<object>({});
 
 	useEffect(() => {
 		fetch('/assets/kolicons/kolicons.json')
@@ -25,7 +25,7 @@ export const IconBasic: FC = () => {
 			<div className="grid grid-cols-2 gap-8 p-8">
 				{Object.entries(icons).map(([key]) => {
 					return (
-						<div className="flex gap-4">
+						<div className="flex gap-4" key={key}>
 							<KolIcon _label="" _icons={'kolicon-' + key} />
 							<span>{key}</span>
 						</div>
