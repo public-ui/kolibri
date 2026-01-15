97993ea862061bb728878693beed91036073b4eb
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-01-15 16:18:58 +0100

    added sample

---
+++ packages/samples/react/src/components/link/linked-headline.tsx
@@ -0,0 +1,19 @@
+import type { FC } from 'react';
+import React from 'react';
+
+import { KolHeading, KolLink } from '@public-ui/react-v19';
+import { SampleDescription } from '../SampleDescription';
+
+export const LinkHeadline: FC = () => (
+	<>
+		<SampleDescription>
+			<p>This sample shows a linked headline with anchor.</p>
+		</SampleDescription>
+
+		<div className="grid gap-4">
+			<KolLink _href="#headline2" _label="">
+				<KolHeading _label="I'm a H2-heading" _level={2} slot="expert" id="headline2" />
+			</KolLink>
+		</div>
+	</>
+);
