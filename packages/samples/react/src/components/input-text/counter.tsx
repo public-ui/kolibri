1a3324a4f5a551ec9e1e372219db88797677f139
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-01-21 14:21:48 +0100

    moved sample to own page

---
+++ packages/samples/react/src/components/input-text/counter.tsx
@@ -0,0 +1,16 @@
+import { KolInputText } from '@public-ui/react-v19';
+import type { FC } from 'react';
+import React from 'react';
+import { SampleDescription } from '../SampleDescription';
+
+export const InputTextCounter: FC = () => (
+	<>
+		<SampleDescription>
+			<p>This story showcases an input with a counter for the chars in the field. Maximal length is 30.</p>
+		</SampleDescription>
+
+		<div className="grid gap-4">
+			<KolInputText _label="Name" _icons="kolicon-house" _value="Anderson-Clark" _hasCounter _maxLength={30} />
+		</div>
+	</>
+);
