3fdb362dbfb97e7b3636cb51759761e743887ea2
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2026-01-21 08:41:12 +0100

    refactor(components): use clsx lite implementation

--- packages/components/src/utils/clsx.ts
+++ packages/components/src/utils/clsx.ts
@@ -2,58 +2,13 @@
  * Adapted from clsx (MIT License).
  * Source: https://github.com/lukeed/clsx/blob/master/src/lite.js
  */
-type ClassDictionary = Record<string, boolean>;
-type ClassValue = ClassDictionary | ClassValue[] | number | string | null | undefined | boolean;
-
-// Normalize any supported class input into a space-delimited string.
-const toClassString = (mix: ClassValue): string => {
-	if (typeof mix === 'string' || typeof mix === 'number') {
-		return String(mix);
-	}
-
-	if (!mix || typeof mix !== 'object') {
-		return '';
-	}
-
-	if (Array.isArray(mix)) {
-		let str = '';
-		for (const item of mix) {
-			if (item) {
-				const val = toClassString(item);
-				if (val) {
-					if (str) {
-						str += ' ';
-					}
-					str += val;
-				}
-			}
-		}
-		return str;
-	}
-
-	let str = '';
-	for (const key of Object.keys(mix)) {
-		if (mix[key]) {
-			if (str) {
-				str += ' ';
-			}
-			str += key;
-		}
-	}
-	return str;
-};
+type ClassValue = string | boolean | null | undefined;
 
 const clsx = (...args: ClassValue[]): string => {
 	let str = '';
 	for (const arg of args) {
-		if (arg) {
-			const val = toClassString(arg);
-			if (val) {
-				if (str) {
-					str += ' ';
-				}
-				str += val;
-			}
+		if (arg && typeof arg === 'string') {
+			str += (str && ' ') + arg;
 		}
 	}
 	return str;
