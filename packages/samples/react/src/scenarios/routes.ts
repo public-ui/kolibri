4eadba16e633d20402a16eb32ed49af26d65539a
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-02-03 15:49:36 +0100

    picked from v4

--- packages/samples/react/src/scenarios/routes.ts
+++ packages/samples/react/src/scenarios/routes.ts
@@ -9,6 +9,7 @@ import { InputGroupWithError } from './input-group-with-error';
 import { InputsGetValue } from './inputs-get-value';
 import { PerformanceTest } from './performance-test';
 import { RHFBasic } from './react-hook-form/basic';
+import { RHFDisabled } from './react-hook-form/disabled';
 import { SameHeightOfAllInteractiveElements } from './same-height-of-all-interactive-elements';
 import { SampleFormWithValidation } from './sample-form-with-validation';
 import { StaticForm } from './static-form';
@@ -25,6 +26,7 @@ export const SCENARIO_ROUTES: Routes = {
 		'input-group-with-error': InputGroupWithError,
 		'inputs-get-value': InputsGetValue,
 		'react-hook-form-adapter': RHFBasic,
+		'react-hook-form-adapter-disabled': RHFDisabled,
 		'same-height-of-all-interactive-elements': SameHeightOfAllInteractiveElements,
 		'static-form': StaticForm,
 		'sample-form-with-validation': SampleFormWithValidation,
