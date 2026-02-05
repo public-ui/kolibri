0c53006087246f71722a41e8e36b15b6edb8b0c2
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2026-02-05 19:14:56 +0100

    chore(angular): migrate adapters to v19-v21

--- scripts/license-reports.sh
+++ scripts/license-reports.sh
@@ -1,7 +1,7 @@
 npx license-report --output=html --package=package.json >> license-reports/root.html
-npx license-report --output=html --package=packages/adapters/angular/v15/package.json >> license-reports/adapter-angular-v15.html
-npx license-report --output=html --package=packages/adapters/angular/v16/package.json >> license-reports/adapter-angular-v16.html
-npx license-report --output=html --package=packages/adapters/angular/v17/package.json >> license-reports/adapter-angular-v17.html
+npx license-report --output=html --package=packages/adapters/angular/v19/package.json >> license-reports/adapter-angular-v19.html
+npx license-report --output=html --package=packages/adapters/angular/v20/package.json >> license-reports/adapter-angular-v20.html
+npx license-report --output=html --package=packages/adapters/angular/v21/package.json >> license-reports/adapter-angular-v21.html
 npx license-report --output=html --package=packages/adapters/hydrate/package.json >> license-reports/adapter-hydrate.html
 npx license-report --output=html --package=packages/adapters/preact/package.json >> license-reports/adapter-preact.html
 npx license-report --output=html --package=packages/adapters/react/package.json >> license-reports/adapter-react.html
