653f8b61d21f96eaaa8af9fd2f98bc285aa3a611
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-02-17 15:42:49 +0100

    upload icon

--- packages/samples/react/src/components/input-file/partials/cases.tsx
+++ packages/samples/react/src/components/input-file/partials/cases.tsx
@@ -13,7 +13,7 @@ export const InputFileCases = forwardRef<HTMLKolInputFileElement, Components.Kol
 					_label="Upload file (Black background test)"
 					_icons={{
 						left: {
-							icon: 'kolicon-check',
+							icon: 'kolicon-up',
 						},
 					}}
 					_touched
@@ -27,7 +27,7 @@ export const InputFileCases = forwardRef<HTMLKolInputFileElement, Components.Kol
 				_label="Upload file (Black background test)"
 				_icons={{
 					left: {
-						icon: 'kolicon-check',
+						icon: 'kolicon-up',
 					},
 				}}
 				_touched
