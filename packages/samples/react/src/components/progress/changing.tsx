94a781ce789cf798a0a6e937ffa326dae40fd9fc
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-03-02 13:27:18 +0100

    added sample

---
+++ packages/samples/react/src/components/progress/changing.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+import { KolProgress } from '@public-ui/react-v19';
+
+import type { FC } from 'react';
+import { SampleDescription } from '../SampleDescription';
+
+export const ProgressChanging: FC = () => {
+	const [val, setVal] = useState<number>(5);
+
+	return (
+		<>
+			<SampleDescription>
+				<p>This shows that values can be changed and that they are clamped at max value (here at 10).</p>
+			</SampleDescription>
+
+			<fieldset title="changing" className="flex flex-col gap-4">
+				<div className="flex gap-4">
+					<button onClick={() => setVal(0)}>set 0</button>
+					<button onClick={() => setVal(5)}>set 5</button>
+					<button onClick={() => setVal(13)}>set 13</button>
+				</div>
+
+				<KolProgress _label={`Showing ${val}`} _variant="bar" _max={10} _value={val}></KolProgress>
+				<KolProgress _label={`Showing ${val}`} _variant="cycle" _max={10} _value={val}></KolProgress>
+			</fieldset>
+		</>
+	);
+};
