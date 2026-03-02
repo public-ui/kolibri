3c367d321604a821419683e0fe1a1a974b9faaba
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-03-02 16:23:24 +0100

    up gibts noch gar nicht... margin in component styles entfernt

--- packages/samples/react/src/components/input-file/partials/cases.tsx
+++ packages/samples/react/src/components/input-file/partials/cases.tsx
@@ -13,7 +13,7 @@ export const InputFileCases = forwardRef<HTMLKolInputFileElement, Components.Kol
 					_label="Upload file (Black background test)"
 					_icons={{
 						left: {
-							icon: 'kolicon-up',
+							icon: 'kolicon-check',
 						},
 					}}
 					_touched
@@ -27,7 +27,7 @@ export const InputFileCases = forwardRef<HTMLKolInputFileElement, Components.Kol
 				_label="Upload file (Black background test)"
 				_icons={{
 					left: {
-						icon: 'kolicon-up',
+						icon: 'kolicon-check',
 					},
 				}}
 				_touched
