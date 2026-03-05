207f1e07f08a8591addce915d3ea3fb30ea7cb92
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-03-05 08:42:41 +0100

    removed sample

--- packages/samples/react/src/components/input-checkbox/routes.ts
+++ packages/samples/react/src/components/input-checkbox/routes.ts
@@ -1,7 +1,6 @@
 import type { Routes } from '../../shares/types';
 import { InputCheckboxBasic } from './basic';
 import { InputCheckboxButton } from './button';
-import { InputCheckboxChange } from './change';
 import { InputCheckboxSwitch } from './switch';
 
 export const INPUT_CHECKBOX_ROUTES: Routes = {
@@ -9,6 +8,5 @@ export const INPUT_CHECKBOX_ROUTES: Routes = {
 		basic: InputCheckboxBasic,
 		switch: InputCheckboxSwitch,
 		button: InputCheckboxButton,
-		change: InputCheckboxChange,
 	},
 };
