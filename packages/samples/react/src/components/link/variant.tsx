8d6dc9a9838ea7083ed48c9adfa346e7eca48d29
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-03-06 17:10:52 +0100

    WIP test push

---
+++ packages/samples/react/src/components/link/variant.tsx
@@ -0,0 +1,41 @@
+import type { FC } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { KolLink } from '@public-ui/react-v19';
+import { SampleDescription } from '../SampleDescription';
+
+export const LinkVariant: FC = () => {
+	const [data, setData] = useState<Array<string>>([]);
+
+	useEffect(() => {
+		fetch('/assets/inject-variants.json')
+			.then((response) => response.json())
+			.then((data) => {
+				if (data.hasOwnProperty('linkVariants')) {
+					setData(data.linkVariants);
+				}
+			})
+			.catch((error) => console.error('Error fetching data:', error));
+	}, []);
+
+	return (
+		<>
+			<SampleDescription>
+				<p>
+					This sample shows KolButton with short key without functionality. The short key is purely visual. Its functionality needs to be developed separately.
+				</p>
+			</SampleDescription>
+
+			<div className="grid gap-4">
+				<KolLink _href="#/back-page" _label="Normal link without a variant" />
+				{data.map((element) => {
+					return (
+						<div className="flex gap-4" key={element}>
+							<KolLink _href="#/back-page" _label={`Theme exclusive variant: ${element}`} _variant={element} />
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
+};
