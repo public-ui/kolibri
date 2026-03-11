a1cd9e9d21809c961c16439bdbe5a278ef7edf2c
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-03-11 16:02:37 +0100

    ein formular braucht einen button....

--- packages/samples/react/src/components/form/basic.tsx
+++ packages/samples/react/src/components/form/basic.tsx
@@ -1,20 +1,29 @@
-import { KolForm, KolInputText } from '@public-ui/react-v19';
+import { KolButton, KolForm, KolInputText } from '@public-ui/react-v19';
 import type { FC } from 'react';
 import React from 'react';
 import { SampleDescription } from '../SampleDescription';
 
-export const FormBasic: FC = () => (
-	<>
-		<SampleDescription>
-			<p>KolForm renders a form around the input components provided in a slot. This sample shows a basic form with three input fields.</p>
-		</SampleDescription>
+export const FormBasic: FC = () => {
+	const formEventHAndler = {
+		onSubmit: (event: any) => console.log('submitted:', event),
+	};
 
-		<KolForm className="w-full">
-			<div className="grid gap-2">
-				<KolInputText id="input1" _label="Input 1" />
-				<KolInputText id="input2" _label="Input 2" />
-				<KolInputText id="input3" _label="Input 3" />
-			</div>
-		</KolForm>
-	</>
-);
+	return (
+		<>
+			<SampleDescription>
+				<p>
+					KolForm renders a form around the input components provided in a slot. This sample shows a basic form with three input fields and a submit button.
+				</p>
+			</SampleDescription>
+
+			<KolForm className="w-full" _on={formEventHAndler}>
+				<div className="grid gap-2">
+					<KolInputText id="input1" _label="Input 1" />
+					<KolInputText id="input2" _label="Input 2" />
+					<KolInputText id="input3" _label="Input 3" />
+					<KolButton _label="Submit" _variant="primary" _type="submit" />
+				</div>
+			</KolForm>
+		</>
+	);
+};
