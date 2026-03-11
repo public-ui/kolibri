81910cd41c915cb3564de321b4b4e8baf337897b
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-03-11 10:44:17 +0100

    nü?

--- packages/tools/visual-tests/tests/theme-snapshots.spec.js
+++ packages/tools/visual-tests/tests/theme-snapshots.spec.js
@@ -30,7 +30,7 @@ ROUTES.forEach((options, route) => {
 		const hideMenusParam = `${route.includes('?') ? '&' : '?'}hideMenus`;
 		await page.goto(`/#${route}${hideMenusParam}`);
 		await page.waitForLoadState('networkidle');
-		await page.waitForSelector('.loading', { state: 'disappear' });
+		await page.waitForSelector('.loading', { state: 'hidden' });
 		await page.addStyleTag({
 			content: `
 				* {
