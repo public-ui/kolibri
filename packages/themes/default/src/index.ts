028fd0be21a5fab344b14f373f6ed6496a7f6dd3
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-03-23 08:47:24 +0100

    readded needed spacings

--- packages/themes/default/src/index.ts
+++ packages/themes/default/src/index.ts
@@ -30,6 +30,7 @@ import navCss from './components/nav.scss';
 import paginationCss from './components/pagination.scss';
 import popoverButtonCss from './components/popover-button.scss';
 import progressCss from './components/progress.scss';
+import quoteCss from './components/quote.scss';
 import selectCss from './components/select.scss';
 import singleSelect from './components/single-select.scss';
 import skipNavCss from './components/skip-nav.scss';
@@ -77,6 +78,7 @@ export const DEFAULT = KoliBri.createTheme('default', {
 	'KOL-PAGINATION': paginationCss,
 	'KOL-POPOVER-BUTTON': popoverButtonCss,
 	'KOL-PROGRESS': progressCss,
+	'KOL-QUOTE': quoteCss,
 	'KOL-SELECT': selectCss,
 	'KOL-SINGLE-SELECT': singleSelect,
 	'KOL-SKIP-NAV': skipNavCss,
