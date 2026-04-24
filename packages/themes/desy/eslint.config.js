5669c2c4c1b96a219675a241a640d0c523e1e75c
Martin <6279703+deleonio@users.noreply.github.com>
2026-04-24 13:44:32 +0200

    chore: add desy theme

---
+++ packages/themes/desy/eslint.config.js
@@ -0,0 +1,37 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default [
+	{
+		ignores: ['dist/**', 'node_modules/**', 'assets/**'],
+	},
+	{
+		files: ['src/**/*.{ts,tsx}'],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				project: 'tsconfig.json',
+				sourceType: 'module',
+				tsconfigRootDir: __dirname,
+			},
+		},
+		plugins: {
+			'@typescript-eslint': tsPlugin,
+		},
+		rules: {
+			...js.configs.recommended.rules,
+			...tsPlugin.configs['recommended'].rules,
+			...tsPlugin.configs['recommended-requiring-type-checking'].rules,
+			'@typescript-eslint/no-namespace': 'off',
+			'@typescript-eslint/no-unsafe-assignment': 'warn',
+			'@typescript-eslint/no-unsafe-call': 'warn',
+			'@typescript-eslint/no-unsafe-member-access': 'warn',
+			eqeqeq: 'error',
+		},
+	},
+];
