8813c0c36b7711200504acfb428abc3030c0284c
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2026-05-02 05:31:54 +0200

    feat(pagination): add PaginationSiblingBoundary component to demonstrate sibling and boundary count effects

--- packages/samples/react/src/components/pagination/routes.ts
+++ packages/samples/react/src/components/pagination/routes.ts
@@ -1,10 +1,12 @@
 import type { Routes } from '../../shares/types';
 import { PaginationBasic } from './basic';
 import { PaginationButtonVisibility } from './button-visibility';
+import { PaginationSiblingBoundary } from './sibling-boundary';
 
 export const PAGINATION_ROUTES: Routes = {
 	pagination: {
 		basic: PaginationBasic,
 		'button-visibility': PaginationButtonVisibility,
+		'sibling-boundary': PaginationSiblingBoundary,
 	},
 };
