6cfd35d926b38b38e9e2005da29fd2dc38d4d2de
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2026-05-02 05:31:21 +0200

    feat(pagination): add PaginationButtonVisibility component to demonstrate button visibility settings

--- packages/samples/react/src/components/pagination/routes.ts
+++ packages/samples/react/src/components/pagination/routes.ts
@@ -1,8 +1,10 @@
 import type { Routes } from '../../shares/types';
 import { PaginationBasic } from './basic';
+import { PaginationButtonVisibility } from './button-visibility';
 
 export const PAGINATION_ROUTES: Routes = {
 	pagination: {
 		basic: PaginationBasic,
+		'button-visibility': PaginationButtonVisibility,
 	},
 };
