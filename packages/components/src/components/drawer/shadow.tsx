43caac8ce32eb5adcd6400857f2fdc055b6bfc6b
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2026-05-02 06:46:27 +0200

    fix(components): guard drawer dialog methods in test env

--- packages/components/src/components/drawer/shadow.tsx
+++ packages/components/src/components/drawer/shadow.tsx
@@ -44,7 +44,7 @@ export class KolDrawer implements DrawerAPI {
 			...this.state,
 			_open: true,
 		};
-		this.dialogElement?.showModal();
+		this.dialogElement?.showModal?.();
 	}
 
 	/**
@@ -199,7 +199,7 @@ export class KolDrawer implements DrawerAPI {
 	}
 
 	private handleCloseDialog() {
-		this.dialogElement?.close();
+		this.dialogElement?.close?.();
 		this._on?.onClose?.();
 		if (this.host) {
 			dispatchDomEvent(this.host, KolEvent.close);
