52926f8e865734096eca68baeadd71ca37564de7
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-05-07 17:09:03 +0200

    on toggle in drawer

--- packages/components/src/components/drawer/drawer.e2e.ts
+++ packages/components/src/components/drawer/drawer.e2e.ts
@@ -21,6 +21,26 @@ test.describe('kol-drawer', () => {
 
 			await expect(callbackPromise).resolves.toBeUndefined();
 		});
+
+		test(`should call 'onToggle' callback when drawer is opened/closed`, async ({ page }) => {
+			await page.setContent('<kol-drawer _label="Details" _open>Drawer content</kol-drawer>');
+			const kolDrawer = page.locator('kol-drawer');
+
+			const callbackPromise = kolDrawer.evaluate((element: HTMLKolDrawerElement) => {
+				return new Promise((resolve) => {
+					element._on = {
+						onToggle: (value?: boolean) => {
+							resolve(value);
+						},
+					};
+				});
+			});
+			await page.waitForChanges();
+			await expect(callbackPromise).resolves.toBe(true);
+
+			await page.keyboard.press('Escape');
+			await expect(callbackPromise).resolves.toBe(false);
+		});
 	});
 
 	test.describe('_open property', () => {
