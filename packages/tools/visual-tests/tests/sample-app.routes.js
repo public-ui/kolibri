1653c5ad4aa08ad237a45220a657b4d1e32a9025
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-05-12 10:10:27 +0200

    added to e2e tests

--- packages/tools/visual-tests/tests/sample-app.routes.js
+++ packages/tools/visual-tests/tests/sample-app.routes.js
@@ -1057,6 +1057,16 @@ ROUTES.set('table/sticky-cols', {
 		},
 	},
 });
+ROUTES.set('table/variant', {
+	snapshot: {
+		viewportSize: {
+			width: 1000,
+		},
+		zoom: {
+			skip: true,
+		},
+	},
+});
 ROUTES.set('tabs/basic', {
 	snapshot: {
 		zoom: {
