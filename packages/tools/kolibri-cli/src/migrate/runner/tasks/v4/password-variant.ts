5fc3459a6c05ceadc61cc37f5150114e997ece8e
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-05-19 10:28:03 +0200

    added migration task

---
+++ packages/tools/kolibri-cli/src/migrate/runner/tasks/v4/password-variant.ts
@@ -0,0 +1,36 @@
+import { AbstractTask } from '../../abstract-task';
+import { RemovePropertyNameTask } from '../common/RemovePropertyNameTask';
+import { RenamePropertyNameTask } from '../common/RenamePropertyNameTask';
+import { UpdatePropertyValueTask } from '../common/UpdatePropertyValueTask';
+
+export const RenameVariantPropTask: AbstractTask = RenamePropertyNameTask.getInstance('kol-input-password', '_variant', '_visibilityToggle', '^4');
+
+export const UpdateVisibilityToggleValueTrue: AbstractTask = UpdatePropertyValueTask.getInstance(
+	'kol-input-password',
+	'_visibilityToggle',
+	'visibility-toggle',
+	'true',
+	'^4',
+	[RenameVariantPropTask],
+);
+
+export const UpdateVisibilityToggleValueFalse: AbstractTask = UpdatePropertyValueTask.getInstance(
+	'kol-input-password',
+	'_visibilityToggle',
+	'default',
+	'false',
+	'^4',
+	[RenameVariantPropTask],
+);
+
+export const RemoveIdPropTasks: AbstractTask = RemovePropertyNameTask.getInstance('kol-input-password', '_variant', '^4', [
+	UpdateVisibilityToggleValueTrue,
+	UpdateVisibilityToggleValueFalse,
+]);
+
+export const RenamePasswordVariantToVisibilityToggle: AbstractTask[] = [
+	RenameVariantPropTask,
+	UpdateVisibilityToggleValueTrue,
+	UpdateVisibilityToggleValueFalse,
+	RemoveIdPropTasks,
+];
