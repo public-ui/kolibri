07c5518d983de0f068715ef642506eb5b281ca9d
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-05-20 10:18:03 +0200

    format + lint

--- packages/components/src/components/input-password/shadow.tsx
+++ packages/components/src/components/input-password/shadow.tsx
@@ -123,7 +123,7 @@ export class KolInputPassword implements InputPasswordAPI, FocusableElement {
 
 		return {
 			ref: this.setInputRef,
-type: (this.state._visibilityToggle || this.state._variant === 'visibility-toggle') && this._passwordVisible ? 'text' : 'password',
+			type: (this.state._visibilityToggle || this.state._variant === 'visibility-toggle') && this._passwordVisible ? 'text' : 'password',
 			state: this.state,
 			ariaDescribedBy,
 			...this.controller.onFacade,
@@ -141,7 +141,7 @@ type: (this.state._visibilityToggle || this.state._variant === 'visibility-toggl
 	}
 
 	private getShowPasswordButton(): VNode | null {
-if (this.state._visibilityToggle || this.state._variant === 'visibility-toggle') {
+		if (this.state._visibilityToggle || this.state._variant === 'visibility-toggle') {
 			return (
 				<KolIconButtonFc
 					componentName="button"
