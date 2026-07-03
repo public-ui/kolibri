134a7072b8a02c7cf6e4ab6ed6e3f6509f8363d3
deleonio <deleonio@users.noreply.github.com>
2026-07-03 18:54:00 +0000

    Deploying to gh-pages from @ public-ui/kolibri@81fe06a5bdfab68df849e6692277a3bd955f785a 🚀

---
+++ pr-10470/81fe06a/assets/component-DSKs197E-Bndb_qsE.js
@@ -0,0 +1,1 @@
+import{o as e}from"./index-hQH6VtwJ.js";import{t}from"./component-CcBTaeMx-CZZhRVgZ.js";var n=({label:n,badgeText:r,id:i,refFloating:a})=>e(`div`,{class:`kol-tooltip__floating`,hidden:n.length===0,ref:a},e(`div`,{class:`kol-tooltip__arrow`}),e(t,{class:`kol-tooltip__content`,id:i||void 0,badgeText:r,label:n}));export{n as t};
---
+++ pr-10470/81fe06a/assets/test-component.entry-BXKHLKyu.js
@@ -0,0 +1,1 @@
+import{s as e}from"./index-hQH6VtwJ.js";var t=class{constructor(t){e(this,t)}};export{t as test_component};
