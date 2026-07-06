6a2bc7d10a47842a812089fbb3ede76bd04af6af
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-07-06 13:58:03 +0200

    samples

--- packages/samples/react/src/scenarios/same-height-of-all-interactive-elements.tsx
+++ packages/samples/react/src/scenarios/same-height-of-all-interactive-elements.tsx
@@ -34,28 +34,28 @@ export const SameHeightOfAllInteractiveElements: FC = () => {
 				</p>
 			</SampleDescription>
 			<div className="w-full flex flex-wrap gap-4">
-				<KolButton _icons="fa-solid fa-house" _label="Button" _on={dummyEventHandler} />
-				<KolButton _hideLabel _icons="fa-solid fa-house" _label="Button" _on={dummyEventHandler} />
-				<KolLinkButton _href="#" _icons="fa-solid fa-house" _label="Link-Button" _on={dummyEventHandler} />
-				<KolLinkButton _hideLabel _href="#" _icons="fa-solid fa-house" _label="Link-Button" _on={dummyEventHandler} />
-				<KolCombobox _hideLabel _icons="fa-solid fa-house" _label="Combobox" _suggestions={[]} />
+				<KolButton _icons="kolicon-house" _label="Button" _on={dummyEventHandler} />
+				<KolButton _hideLabel _icons="kolicon-house" _label="Button" _on={dummyEventHandler} />
+				<KolLinkButton _href="#" _icons="kolicon-house" _label="Link-Button" _on={dummyEventHandler} />
+				<KolLinkButton _hideLabel _href="#" _icons="kolicon-house" _label="Link-Button" _on={dummyEventHandler} />
+				<KolCombobox _hideLabel _icons="kolicon-house" _label="Combobox" _suggestions={[]} />
 				<KolInputColor
 					_hideLabel
 					_icons={{
-						left: 'fa-solid fa-house',
+						left: 'kolicon-house',
 					}}
 					_label="Input-Color"
 				/>
-				<KolInputFile _hideLabel _icons="fa-solid fa-house" _label="Input-File" />
-				<KolInputDate _hideLabel _icons="fa-solid fa-house" _label="Input-Date" />
-				<KolInputEmail _hideLabel _icons="fa-solid fa-house" _label="Input-Email" />
-				<KolInputNumber _hideLabel _icons="fa-solid fa-house" _label="Input-Number" />
-				<KolInputPassword _hideLabel _icons="fa-solid fa-house" _label="Input-Password" />
-				<KolInputRange _hideLabel _icons="fa-solid fa-house" _label="Input-Range" />
-				<KolInputText _hideLabel _icons="fa-solid fa-house" _label="Input-Text" />
-				<KolSelect _hideLabel _icons="fa-solid fa-house" _label="Combobox" _options={[]} />
-				<KolSingleSelect _hideLabel _icons="fa-solid fa-house" _label="Combobox" _options={[]} />
-				<KolTextarea _hideLabel _icons="fa-solid fa-house" _label="Combobox" />
+				<KolInputFile _hideLabel _icons="kolicon-house" _label="Input-File" />
+				<KolInputDate _hideLabel _icons="kolicon-house" _label="Input-Date" />
+				<KolInputEmail _hideLabel _icons="kolicon-house" _label="Input-Email" />
+				<KolInputNumber _hideLabel _icons="kolicon-house" _label="Input-Number" />
+				<KolInputPassword _hideLabel _icons="kolicon-house" _label="Input-Password" />
+				<KolInputRange _hideLabel _icons="kolicon-house" _label="Input-Range" />
+				<KolInputText _hideLabel _icons="kolicon-house" _label="Input-Text" />
+				<KolSelect _hideLabel _icons="kolicon-house" _label="Combobox" _options={[]} />
+				<KolSingleSelect _hideLabel _icons="kolicon-house" _label="Combobox" _options={[]} />
+				<KolTextarea _hideLabel _icons="kolicon-house" _label="Combobox" />
 			</div>
 		</>
 	);
