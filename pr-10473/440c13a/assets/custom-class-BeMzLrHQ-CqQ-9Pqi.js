da9bf6e48e4e794e8cc323e6a3810670e44690c9
deleonio <deleonio@users.noreply.github.com>
2026-07-08 12:17:18 +0000

    Deploying to gh-pages from @ public-ui/kolibri@440c13ad0fa99480641536bd89532fa1c4d959e4 🚀

---
+++ pr-10473/440c13a/assets/custom-class-BeMzLrHQ-CqQ-9Pqi.js
@@ -0,0 +1,1 @@
+import{jt as e}from"./index-BFkeVTm_.js";var t=(t,n)=>{e(t,`_customClass`,n,{defaultValue:``})};export{t};
---
+++ pr-10473/440c13a/assets/multiple-wlinUB8P-mw9DWswT.js
@@ -0,0 +1,1 @@
+import{Ot as e}from"./index-BFkeVTm_.js";var t=(t,n,r)=>{e(t,`_multiple`,n,r)};export{t};
