92530397442ef6b507e1b07dbca1c53991163862
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-07-09 13:00:26 +0200

    more than one word as vairant possible

--- packages/components/src/schema/props/variant-class-name.ts
+++ packages/components/src/schema/props/variant-class-name.ts
@@ -1,17 +1,61 @@
 import type { Generic } from 'adopted-style-sheets';
 
-import { watchValidator } from '../utils';
+import { WatchOptions, watchValidator } from '../utils';
+import { isString } from '../validators';
 
-export type VariantClassNamePropType = string;
+export type VariantClassNamePropType = string | string[];
 
 export type PropVariantClassName = {
 	variant: VariantClassNamePropType;
 };
 
 const SAFE_CLASS_NAME_RE = /^[a-zA-Z][a-zA-Z0-9_-]{3,60}$/;
+const isValidArrayOrString = (value: unknown) =>
+	(Array.isArray(value) && value.every(isSafeClassName)) || (isString(value, 1) && (value as string).split(' ').every(isSafeClassName));
 const isSafeClassName = (value: unknown) => typeof value === 'string' && SAFE_CLASS_NAME_RE.test(value);
 const SAFE_CLASS_NAME_ALLOWED = new Set([SAFE_CLASS_NAME_RE.source]);
 
-export const validateVariantClassName = (component: Generic.Element.Component, value?: VariantClassNamePropType): void => {
-	watchValidator(component, '_variant', isSafeClassName, SAFE_CLASS_NAME_ALLOWED, value);
+const beforePatchString = (component: Generic.Element.Component): void => {
+	if (component.nextState?.has('_variant')) {
+		const variants = component.nextState?.get('_variant') as unknown;
+		let nextStateVariants: VariantClassNamePropType = [];
+		if (isString(variants, 1)) {
+			console.log('isString');
+			nextStateVariants = (variants as string).split(' ');
+		} else if (Array.isArray(variants)) {
+			console.log('isarray');
+
+			nextStateVariants = variants;
+		}
+		console.log(nextStateVariants);
+
+		component.nextState?.set('_variant', nextStateVariants);
+	}
+};
+
+export const validateVariantClassName = (component: Generic.Element.Component, value?: VariantClassNamePropType, options: WatchOptions = {}): void => {
+	watchValidator(component, '_variant', isValidArrayOrString, SAFE_CLASS_NAME_ALLOWED, value, {
+		...options,
+		defaultValue: {},
+		hooks: {
+			afterPatch: options.hooks?.afterPatch,
+			beforePatch: (nextValue, nextState, component, key) => {
+				if (typeof options.hooks?.beforePatch === 'function') {
+					options.hooks?.beforePatch(nextValue, nextState, component, key);
+				}
+				beforePatchString(component);
+			},
+		},
+	});
+};
+
+export const classNameFromVariant = (variants: VariantClassNamePropType | undefined, componentName: string): string => {
+	let className = '';
+
+	if (Array.isArray(variants)) {
+		variants.forEach((variant) => {
+			className += 'kol-' + componentName + '--' + variant + ' ';
+		});
+	}
+	return className;
 };
