5d1fa174cba7584fdc31b59b70ae524fec5121dc
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-07-15 12:38:41 +0200

    label as deprecated

--- packages/components/src/components/abbr/component.tsx
+++ packages/components/src/components/abbr/component.tsx
@@ -1,8 +1,9 @@
 import type { JSX } from '@stencil/core';
-import { Component, h, Host } from '@stencil/core';
+import { Component, h, Host, Prop } from '@stencil/core';
 
 import type { AbbrApi } from '../../internal/functional-components/abbr/api';
 import { BaseWebComponent } from '../../internal/functional-components/base-web-component';
+import { LabelPropType } from '../../schema';
 
 /**
  * The **Abbr** component implements the HTML tag `abbr`.
@@ -17,6 +18,12 @@ import { BaseWebComponent } from '../../internal/functional-components/base-web-
 	shadow: true,
 })
 export class KolAbbr extends BaseWebComponent<AbbrApi> {
+	/**
+	 * DEPRECATED!
+	 * Defines the visible or semantic label of the component (e.g. aria-label, label, headline, caption, summary, etc.).
+	 */
+	@Prop() public _label?: LabelPropType;
+
 	public render(): JSX.Element {
 		return (
 			<Host>
