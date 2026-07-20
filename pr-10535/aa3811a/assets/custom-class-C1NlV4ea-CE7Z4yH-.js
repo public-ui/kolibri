51cc74942b394598cff1680b1108c2ba284812e5
BF150 <BF150@users.noreply.github.com>
2026-07-20 09:08:37 +0000

    Deploying to gh-pages from @ public-ui/kolibri@aa3811af3ede0bee3cdc058283717683cf00fb8a 🚀

---
+++ pr-10535/aa3811a/assets/custom-class-C1NlV4ea-CE7Z4yH-.js
@@ -0,0 +1,1 @@
+import{jt as e}from"./index-BHGTSLVg.js";var t=(t,n)=>{e(t,`_customClass`,n,{defaultValue:``})};export{t};
---
+++ pr-10535/aa3811a/assets/hide-label-B9ipEhG8-5OEjnYwM.js
@@ -0,0 +1,1 @@
+import{Ot as e}from"./index-BHGTSLVg.js";var t=(t,n,r={})=>{e(t,`_hideLabel`,n,r)};export{t};
