0be8edb2388f3c0bb3db11c3eaea6cea34fd02e3
deleonio <deleonio@users.noreply.github.com>
2026-07-27 08:50:39 +0000

    Deploying to gh-pages from @ public-ui/kolibri@d1572e095b6b216f572be820b5b0df3c8d1b10f9 🚀

---
+++ pr-10568/d1572e0/assets/read-only-CR_961Fh-Cyf-54Mt.js
@@ -0,0 +1,1 @@
+import{Ot as e}from"./index-DxQSqfSm.js";var t=(t,n)=>{e(t,`_readOnly`,n)};export{t};
---
+++ pr-10568/d1572e0/assets/tooltip-align-ChjYN8Pt-9d_lDkDU.js
@@ -0,0 +1,1 @@
+import"./index-DxQSqfSm.js";import{r as e}from"./align-DpNc6EKs-OLRSFzEn.js";var t=(t,n)=>{e(t,`_tooltipAlign`,n)};export{t};
