4217d61431c1e59ce70ecf9bcf992868f491fe11
deleonio <deleonio@users.noreply.github.com>
2026-07-31 12:29:58 +0000

    Deploying to gh-pages from @ public-ui/kolibri@dcee6c1f537d5b6a8c9eff18bb4ffa5ffe91690a 🚀

---
+++ pr-10602/dcee6c1/assets/label-RX8TPr4R-CApmXHNy.js
@@ -0,0 +1,1 @@
+import{n as e,o as t}from"./normalizers-Dmgd1cXp-AdFtTixz.js";var n=e(`label`,``,t,e=>e===``||e.length>=2&&e.length<=80);export{n as t};
---
+++ pr-10602/dcee6c1/assets/tooltip-align-ChjYN8Pt-DS7j3272.js
@@ -0,0 +1,1 @@
+import"./index-psqCdCH3.js";import{r as e}from"./align-DpNc6EKs-CTdDhu7J.js";var t=(t,n)=>{e(t,`_tooltipAlign`,n)};export{t};
