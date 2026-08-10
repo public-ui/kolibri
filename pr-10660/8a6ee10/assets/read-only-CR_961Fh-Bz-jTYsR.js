51af54c4c068c7f526ad22cef7ac9b1125e14e08
BF150 <BF150@users.noreply.github.com>
2026-08-10 06:52:23 +0000

    Deploying to gh-pages from @ public-ui/kolibri@8a6ee10bd1dbf18144f570f513e2d49146898037 🚀

---
+++ pr-10660/8a6ee10/assets/read-only-CR_961Fh-Bz-jTYsR.js
@@ -0,0 +1,1 @@
+import{Tt as e}from"./index-CZJYj5VG.js";var t=(t,n)=>{e(t,`_readOnly`,n)};export{t};
---
+++ pr-10660/8a6ee10/assets/tooltip-align-ChjYN8Pt-RmzOewmw.js
@@ -0,0 +1,1 @@
+import"./index-CZJYj5VG.js";import{r as e}from"./align-DpNc6EKs-Dw-yedA4.js";var t=(t,n)=>{e(t,`_tooltipAlign`,n)};export{t};
