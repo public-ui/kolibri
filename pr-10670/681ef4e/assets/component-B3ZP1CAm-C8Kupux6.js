00767baa79233e9c852ffa3cca07363fc4f1205c
ItzGerSte <ItzGerSte@users.noreply.github.com>
2026-08-11 12:47:49 +0000

    Deploying to gh-pages from @ public-ui/kolibri@681ef4edbc8102fd8904b8572615c14d8314fb56 🚀

---
+++ pr-10670/681ef4e/assets/component-B3ZP1CAm-C8Kupux6.js
@@ -0,0 +1,1 @@
+import{o as e}from"./index-CXnsTMYC.js";import{t}from"./component-BYL32eGP-Co-APgC3.js";var n=({label:n,badgeText:r,id:i,refFloating:a})=>e(`div`,{class:`kol-tooltip__floating`,hidden:n.length===0,ref:a},e(`div`,{class:`kol-tooltip__arrow`}),e(t,{class:`kol-tooltip__content`,badgeText:r,id:i,label:n}));export{n as t};
---
+++ pr-10670/681ef4e/assets/read-only-Vt3H3FN6-BhPSqjXJ.js
@@ -0,0 +1,1 @@
+import{Tt as e}from"./index-CXnsTMYC.js";var t=(t,n)=>{e(t,`_readOnly`,n)};export{t};
