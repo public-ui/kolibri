2179b34fb7c2de0b4d262f5a6d138b7b8b6e4063
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-08-12 10:41:49 +0200

    reduced sample

--- packages/samples/react/src/components/card/linked.tsx
+++ packages/samples/react/src/components/card/linked.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { KolCard } from '@public-ui/react-v19';
 
@@ -6,29 +6,14 @@ import type { FC } from 'react';
 import { SampleDescription } from '../SampleDescription';
 
 export const CardLinked: FC = () => {
-	const cardRef = React.useRef<HTMLKolCardElement | null>(null);
-
-	useEffect(() => {
-		// Ensure the popover is closed on initial render
-		if (cardRef.current) {
-			cardRef.current.focus();
-		}
-	}, []);
-
 	return (
 		<>
 			<SampleDescription>
-				<p>KolCard shows a card with title and slot content. The second sample features a close button.</p>
+				<p>This sample shows KolCards with links. The headline is wrapped with an a-tag. The whole card is clickable.</p>
 			</SampleDescription>
 
 			<div className="w-full grid grid-cols-2 gap-4">
-				<KolCard
-					_label="Card with title and content"
-					_href="#/back-page"
-					ref={cardRef}
-					onBlur={() => console.log('outer blur')}
-					onFocus={() => console.log('outer focus')}
-				>
+				<KolCard _label="Card with title and content" _href="#/back-page">
 					<p>This card has a link.</p>
 				</KolCard>
 
