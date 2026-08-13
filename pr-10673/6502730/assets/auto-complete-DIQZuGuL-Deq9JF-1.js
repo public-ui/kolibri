63b291cfab8438926e7579335d1bffa2f9543c24
ItzGerSte <ItzGerSte@users.noreply.github.com>
2026-08-13 07:27:04 +0000

    Deploying to gh-pages from @ public-ui/kolibri@650273070cfc6d33151b45f503fc8d41be37045d 🚀

---
+++ pr-10673/6502730/assets/auto-complete-DIQZuGuL-Deq9JF-1.js
@@ -0,0 +1,1 @@
+import{kt as e}from"./index-CwtYmmu9.js";var t=[`on`,`off`],n=(n,r)=>{e(n,`_autoComplete`,e=>t.includes(e)||typeof e==`string`&&e.length>0,new Set(t),r,{defaultValue:`off`})};export{n as t};
---
+++ pr-10673/6502730/assets/tooltip-align-B40m4aqW-CPXKNQFn.js
@@ -0,0 +1,1 @@
+import"./index-CwtYmmu9.js";import{r as e}from"./align-CCcqVV8A-DX0tPgZs.js";var t=(t,n)=>{e(t,`_tooltipAlign`,n)};export{t};
