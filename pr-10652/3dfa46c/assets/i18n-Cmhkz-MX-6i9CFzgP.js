6aaf3ce672af24fef1d02ea3bb99697402238b9f
deleonio <deleonio@users.noreply.github.com>
2026-08-14 13:10:19 +0000

    Deploying to gh-pages from @ public-ui/kolibri@3dfa46ce02c7ad1afa08a85500899c65e9db1c6d 🚀

---
+++ pr-10652/3dfa46c/assets/i18n-Cmhkz-MX-6i9CFzgP.js
@@ -0,0 +1,1 @@
+import{at as e,f as t,p as n}from"./index-DwiBCCs4.js";var r=(e,r)=>(t()??n(`de`)).translate(e,r);e()&&(r=e=>e);export{r as t};
---
+++ pr-10652/3dfa46c/assets/label-with-expert-slot-BtB4_gwz-DGOv9Vqh.js
@@ -0,0 +1,1 @@
+import{n as e,s as t}from"./normalizers-DwKzbMGB-DNBq4gpu.js";var n=e(`label`,``,t);export{n as t};
