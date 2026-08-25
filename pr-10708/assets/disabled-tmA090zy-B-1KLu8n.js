200218c2aa587beda9a1b4118c8047b0888c84a7
deleonio <deleonio@users.noreply.github.com>
2026-08-25 13:45:25 +0000

    Deploying to gh-pages from @ public-ui/kolibri@6f5f985babab8ea08c99d3cc64aa61b6ed1534dc 🚀

---
+++ pr-10708/assets/disabled-tmA090zy-B-1KLu8n.js
@@ -0,0 +1,1 @@
+import{H as e,Tt as t}from"./index-Dbc2-hox.js";var n=(n,r)=>{t(n,`_disabled`,r,{hooks:{afterPatch:t=>{t===!0&&e()}}})};export{n as t};
---
+++ pr-10708/assets/suggestions-CdFtI3g5-BSU2ylr4.js
@@ -0,0 +1,1 @@
+import{Et as e,V as t}from"./index-Dbc2-hox.js";var n=(n,r)=>{e(n,`_suggestions`,e=>typeof e==`string`||typeof e==`number`,r,void 0,{hooks:{afterPatch:e=>{Array.isArray(e)&&e.length&&t(`Property suggestions: Options have accessibility issues in how browsers implemented them and should not be used for now.`)}}})};export{n as t};
