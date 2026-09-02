8cb8b3e67b027753af26fc14e214c193668db4d6
ItzGerSte <ItzGerSte@users.noreply.github.com>
2026-09-02 12:30:02 +0000

    Deploying to gh-pages from @ public-ui/kolibri@59800b6d974ad052015ef12a68de548e6f4a1f24 🚀

---
+++ pr-10754/assets/open-BRQrMgjX-BwBrE_Ul.js
@@ -0,0 +1,1 @@
+import{Tt as e}from"./index-ECijqoS-.js";var t=(t,n,r)=>{e(t,`_open`,n,r)};export{t};
---
+++ pr-10754/assets/validation-BZPzOg5I-DNS7q0P_.js
@@ -0,0 +1,1 @@
+import{Et as e,wt as t}from"./index-ECijqoS-.js";var n=(n,r,i)=>{e(r,`_links`,e=>typeof e==`object`&&(typeof e._href==`string`||typeof e._label==`string`),i),t(n,r.state._links.length)};export{n as t};
