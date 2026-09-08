4069eb9f89631d35017facadb4f33c9a35ff451a
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-09-08 08:59:11 +0200

    migrate fix

--- packages/tools/kolibri-cli/src/migrate/runner/tasks/v4/table-stateless-headers.ts
+++ packages/tools/kolibri-cli/src/migrate/runner/tasks/v4/table-stateless-headers.ts
@@ -2,8 +2,8 @@ import { AbstractTask } from '../../abstract-task';
 import { RemovePropertyNameTask } from '../common/RemovePropertyNameTask';
 import { RenamePropertyNameTask } from '../common/RenamePropertyNameTask';
 
-const RenameHeaderCellsPropTask: AbstractTask = RenamePropertyNameTask.getInstance('kol-table-stateless', '_headerCells', '_headers', '^4');
+const RenameHeaderCellsPropTask: AbstractTask = RenamePropertyNameTask.getInstance('kol-table-stateless', '_header-cells', '_headers', '^4');
 
-const RemoveHeaderCellsPropTask: AbstractTask = RemovePropertyNameTask.getInstance('kol-table-stateless', '_headerCells', '^4', [RenameHeaderCellsPropTask]);
+const RemoveHeaderCellsPropTask: AbstractTask = RemovePropertyNameTask.getInstance('kol-table-stateless', '_header-cells', '^4', [RenameHeaderCellsPropTask]);
 
 export const RenameTableStatelessHeaderCellsToHeaders: AbstractTask[] = [RenameHeaderCellsPropTask, RemoveHeaderCellsPropTask];
