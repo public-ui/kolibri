c16783e63211b7d57c4d19f20dba6ac73f7495f8
deleonio <deleonio@users.noreply.github.com>
2026-09-09 05:42:32 +0000

    Deploying to gh-pages from @ public-ui/kolibri@3217424dbe11655f77d4fce0e78a74ac81124219 🚀

---
+++ pr-10800/assets/component-BGR8kQ7--Betdf-u4.js
@@ -0,0 +1,1 @@
+import{o as e}from"./index-DdYxVYpp.js";import{t}from"./component-D8uE-jGD-BJexak7u.js";var n=({label:n,badgeText:r,id:i,refFloating:a})=>e(`div`,{class:`kol-tooltip__floating`,hidden:n.length===0,ref:a},e(`div`,{class:`kol-tooltip__arrow`}),e(t,{class:`kol-tooltip__content`,badgeText:r,id:i,label:n}));export{n as t};
