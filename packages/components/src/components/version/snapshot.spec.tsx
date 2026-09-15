e668b2edd8c1287d2d5750bf0b59883b0932c00a
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2026-09-15 21:18:57 +0200

    refactor(version): co-locate snapshot test

---
+++ packages/components/src/components/version/snapshot.spec.tsx
@@ -0,0 +1,6 @@
+import { KolVersionTag } from '../../core/component-names';
+import type { VersionProps } from '../../schema';
+import { executeSnapshotTests } from '../../utils/testing';
+import { KolVersion } from './component';
+
+executeSnapshotTests<VersionProps>(KolVersionTag, [KolVersion], [{ _label: '1.0.0' }]);
