7952904db592b25362c199ea71952d9b192989d4
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2026-09-15 21:21:35 +0200

    refactor(version): drop legacy snapshot test in favor of the co-located suite

--- packages/components/src/components/version/test/snapshot.spec.tsx
+++ packages/components/src/components/version/test/snapshot.spec.tsx
@@ -1,7 +1,2 @@
-import { KolVersionTag } from '../../../core/component-names';
-import type { VersionProps } from '../../../schema';
-import { executeSnapshotTests } from '../../../utils/testing';
-
-import { KolVersion } from '../shadow';
-
-executeSnapshotTests<VersionProps>(KolVersionTag, [KolVersion], [{ _label: '1.0.0' }]);
+// Tests are co-located at packages/components/src/components/version/snapshot.spec.tsx.
+export {};
