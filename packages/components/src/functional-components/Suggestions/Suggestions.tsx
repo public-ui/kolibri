2e631bf6456ecbd15723cc9a688b80971b92a917
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-09-22 12:56:07 +0200

    geht doch mit value

--- packages/components/src/functional-components/Suggestions/Suggestions.tsx
+++ packages/components/src/functional-components/Suggestions/Suggestions.tsx
@@ -16,7 +16,7 @@ const SuggestionsFc: FC<SuggestionsProps> = ({ id, suggestions, ...other }) => {
 	return (
 		<datalist id={createRelatedUniqueId(id, 'list')} {...other}>
 			{suggestions.map((option: W3CInputValue) => (
-				<option>{option}</option>
+				<option value={option} />
 			))}
 		</datalist>
 	);
