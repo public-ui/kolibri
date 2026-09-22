ef252b62852b9dafab479bd730db04ef68faea88
deleonio <deleonio@users.noreply.github.com>
2026-09-22 11:48:23 +0000

    Deploying to gh-pages from @ public-ui/kolibri@7c95561b13ed37d5bb217f6158939496a73a9229 🚀

---
+++ pr-10969/assets/open-v80i9O0s-C-uC1Gox.js
@@ -0,0 +1,1 @@
+import{Ct as e}from"./index-h2kE_xJ4.js";var t=(t,n,r)=>{e(t,`_open`,n,r)};export{t};
---
+++ pr-10969/assets/placeholder-BgFXMdY3-C4yFJh70.js
@@ -0,0 +1,1 @@
+import{Et as e}from"./index-h2kE_xJ4.js";var t=(t,n,r={})=>{e(t,`_placeholder`,n,r)};export{t};
