7dbd6fde935ddb0269976ddeda16e7f9610aa28f
Franziska Bartsch <franziska.bartsch@iso-gruppe.com>
2026-09-23 09:37:28 +0200

    accesskey is always undefined for the range part removed suggestions from range part

--- packages/components/src/components/input-range/shadow.tsx
+++ packages/components/src/components/input-range/shadow.tsx
@@ -33,7 +33,7 @@ import KolFormFieldStateWrapperFc, { type FormFieldStateWrapperProps } from '../
 import KolInputContainerFc from '../../functional-component-wrappers/InputContainerStateWrapper/InputContainerStateWrapper';
 import KolInputStateWrapperFc, { type InputStateWrapperProps } from '../../functional-component-wrappers/InputStateWrapper/InputStateWrapper';
 import KolSuggestionsFc from '../../functional-components/Suggestions';
-import { createRelatedUniqueId, createUniqueId } from '../../utils/dev.utils';
+import { createUniqueId } from '../../utils/dev.utils';
 import { createCtaRef, delegateClick, delegateFocus } from '../../utils/element-interaction';
 import { propagateSubmitEventToForm } from '../form/controller';
 import { InputRangeController } from './controller';
@@ -187,11 +187,11 @@ export class KolInputRange implements ClickableElement, FocusableElement, InputR
 		return {
 			...this.getGenericInputProps(),
 			name: this.state._name ? `${this.state._name}-range` : undefined,
-			list: this.hasSuggestions ? createRelatedUniqueId(this.state._id, 'list') : undefined,
 			type: 'range',
 			tabIndex: -1,
 			id: undefined,
 			'aria-hidden': 'true',
+			accessKey: undefined,
 			ref: this.setInputRangeRef,
 		};
 	}
